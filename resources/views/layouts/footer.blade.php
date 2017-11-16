f673548410ea9f77a36fa65d42e6341fea2744e7
Jeremy Payne <=>
2017-11-16 13:25:45 +0100

    UI - Add social in footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,9 +1,26 @@
 <!-- Main Footer -->
 <footer class="main-footer">
-    <!-- To the right -->
-    <div class="pull-right hidden-xs">
-        Made with <a href="//laravel.com">Laravel</a>
+    <div class="row">
+        <div class="col-md-4 hidden-xs">
+            <strong>Copyright © 2017 <a href="#">{{ config('app.name') }}</a> - <a href="{{ route('releases.index') }}">{{ config('app.version') }}</a>.</strong> All rights reserved.
+        </div>
+        <div class="col-md-4 text-center">
+            <a href="mailto:#">hello@kanka.io</a>
+        </div>
+        <div class="col-md-4 text-right footer-social hidden-xs">
+            <a href="//twitter.com/kankaio" title="Twitter">
+                <i class="fa fa-twitter-square"></i>
+            </a>
+            <a href="//www.facebook.com/kanka.io.ch/" title="Facebook">
+                <i class="fa fa-facebook-official"></i>
+            </a>
+            <a href="//www.reddit.com/r/kanka/" title="Reddit">
+                <i class="fa fa-reddit-square"></i>
+            </a>
+            <a href="//www.patreon.com/kankaio" title="Patreon">
+                <i class="fa fa-gratipay"></i>
+            </a>
+        </div>
     </div>
     <!-- Default to the left -->
-    <strong>Copyright © 2017 <a href="#">{{ config('app.name') }}</a> - <a href="{{ route('releases.index') }}">{{ config('app.version') }}</a>.</strong> All rights reserved.
 </footer>
