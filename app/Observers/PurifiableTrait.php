356d51575afa282e031508152e35e685af7db339
Jeremy Payne <jay@jpayne.ch>
2017-12-03 13:43:06 +0100

    Relations - fix deleting entities where relations still existed

--- app/Observers/PurifiableTrait.php
+++ app/Observers/PurifiableTrait.php
@@ -10,8 +10,9 @@ trait PurifiableTrait
     {
         $config = [
             'HTML.Allowed' =>
-                'big,small,h1,h2,h3,h4,h5,h6,img[src],div,ins,del,pre,a,strong,em,s,u,ul,ol,,li,'
-                . 'blockquote,table,tr,td,hr,'
+                'big,small,h1,h2,h3,h4,h5,h6,img[src],div,ins,del,pre,a,strong,em,s,u,ul,ol,li,'
+                . 'blockquote,sup,sub,'
+                . 'table,tbody,thead,tfoot,tr,td,hr,'
         ];
         return Purify::clean($text, $config);
     }
