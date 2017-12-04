24da2d0779daf2fdc66b5216216c108ea5041462
Jeremy Payne <jay@jpayne.ch>
2017-12-04 10:29:47 +0100

    Rich text - fix return

--- app/Observers/PurifiableTrait.php
+++ app/Observers/PurifiableTrait.php
@@ -10,7 +10,7 @@ trait PurifiableTrait
     {
         $config = [
             'HTML.Allowed' =>
-                'big,small,h1,h2,h3,h4,h5,h6,img[src],div,ins,del,pre,a,strong,em,s,u,ul,ol,li,'
+                'big,small,h1,h2,h3,h4,h5,h6,img[src],div,ins,del,pre,a,strong,em,s,u,ul,ol,li,p,'
                 . 'blockquote,sup,sub,'
                 . 'table,tbody,thead,tfoot,tr,td,hr,'
         ];
