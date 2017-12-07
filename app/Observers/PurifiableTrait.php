6355f55248c38925ce8ba34adc8ad27472f79c27
Jeremy Payne <jay@jpayne.ch>
2017-12-07 20:56:24 +0100

    Rich Text - keep links and image styling

--- app/Observers/PurifiableTrait.php
+++ app/Observers/PurifiableTrait.php
@@ -10,10 +10,11 @@ trait PurifiableTrait
     {
         $config = [
             'HTML.Allowed' =>
-                'big,small,h1,h2,h3,h4,h5,h6,img[src],div,ins,del,pre,a,strong,em,s,u,ul,ol,li,p,'
+                'big,small,h1,h2,h3,h4,h5,h6,img[src|style|alt],div,ins,del,pre,a[href|target],strong,em,s,u,ul,ol,li,p,'
                 . 'blockquote,sup,sub,'
                 . 'table,tbody,thead,tfoot,tr,td,hr,'
         ];
+
         return Purify::clean($text, $config);
     }
 }
