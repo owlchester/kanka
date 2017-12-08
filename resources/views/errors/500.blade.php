67eb489c6a945b0305836a708a0f9a411d7ce097
Jeremy Payne <jay@jpayne.ch>
2017-12-08 09:10:04 +0100

    Custom 500 error page

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,13 @@
+@extends('errors::layout')
+
+@section('title', 'Error')
+
+@section('message')
+
+    <p>Whoops, looks like something went wrong.</p>
+
+    <p>A report with the error was sent to us, but sometimes it helps if we can know a little bit more about what you were doing.</p>
+
+    <p>If you need further assistance, please contact us at hello@kanka.io or on the <a href="//reddit.com/r/kanka">r/kanka</a> subreddit.</p>
+
+@endsection
