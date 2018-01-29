5710bf6017149cfa49a656f292dc0bb077945a30
Jeremy Payne <jay@jpayne.ch>
2018-01-29 10:04:14 +0100

    Translate the app to American #37 Fix some translations

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -118,6 +118,36 @@ return [
     |
     */
 
-    'attributes' => [],
+    'attributes' => [
+        'name' => 'name',
+        'username' => "username",
+        'email' => 'email address',
+        'first_name' => 'first name',
+        'last_name' => 'last name',
+        'password' => 'password',
+        'password_confirmation' => 'confirm your password',
+        'city' => 'city',
+        'country' => 'country',
+        'address' => 'address',
+        'phone' => 'phone',
+        'mobile' => 'mobile',
+        'age' => 'age',
+        'sex' => 'sex',
+        'gender' => 'gender',
+        'day' => 'day',
+        'month' => 'month',
+        'year' => 'year',
+        'hour' => 'hour',
+        'minute' => 'minute',
+        'second' => 'second',
+        'title' => 'title',
+        'content' => 'content',
+        'description' => 'description',
+        'excerpt' => 'excerpt',
+        'date' => 'date',
+        'time' => 'time',
+        'available' => 'available',
+        'size' => 'size',
+    ],
 
 ];
