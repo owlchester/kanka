dd0342718f910e233f64ec70e8485c1d5f22e278
Jeremy Payne <jay@jpayne.ch>
2018-02-15 23:26:57 +0100

    Translate the app to Brazilian Portuguese #41

---
+++ resources/lang/pt-BR/attribute_templates.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+    'create'        => [
+        'success'   => 'Modelo de Atributo \':name\' criado.',
+        'title'     => 'Criar um novo Modelo de Atributo',
+    ],
+    'destroy'       => [
+        'success'   => 'Modelo de Atributo \':name\' removido.',
+    ],
+    'edit'          => [
+        'success'   => 'Modelo de Atributo \':name\' atualizado.',
+        'title'     => 'Editar Modelo de Atributo :name',
+    ],
+    'fields'        => [
+        'attributes'    => 'Atributos',
+        'name'          => 'Nome',
+    ],
+    'index'         => [
+        'add'           => 'Novo Modelo de Atributo',
+        'description'   => 'Gerencie os Modelos de Atributos de :name',
+        'header'        => 'Modelos de Atributos de :name',
+        'title'         => 'Modelos de Atributos',
+    ],
+    'placeholders'  => [
+        'name'  => 'Nome do Modelo de Atributo',
+    ],
+    'show'          => [
+        'description'   => 'Uma visão detalhada de um Modelo de Atributo',
+        'tabs'          => [
+            'attributes'    => 'Atributos',
+        ],
+        'title'         => 'Modelo de Atributo :name',
+    ],
+];
---
+++ resources/lang/pt-BR/teams.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'index' => [
+        'core'          => 'Principal',
+        'description'   => 'Essas pessoas fazem o Kanka possível',
+        'title'         => 'Time',
+        'translations'  => 'Traduções',
+    ],
+];
