ce770e451d34419a5464d85bb31d0faf2c9a7d49
Jeremy Payne <jay@jpayne.ch>
2018-03-21 18:39:28 +0100

    Map: Being able to upload a map of your campaign, and create links on it to various entities #20

--- app/Services/LocationService.php
+++ app/Services/LocationService.php
@@ -68,18 +68,20 @@ class LocationService
             $existing[$model->id] = $model;
         }
 
-        foreach ($data['map_point'] as $key => $point) {
-            if (isset($existing[$key])) {
-                unset($existing[$key]);
-                continue;
+        if (!empty($data['map_point'])) {
+            foreach ($data['map_point'] as $key => $point) {
+                if (isset($existing[$key])) {
+                    unset($existing[$key]);
+                    continue;
+                }
+                list($x, $y, $target) = explode('-', $point);
+                $new = MapPoint::create([
+                    'location_id' => $location->id,
+                    'target_id' => $target,
+                    'axis_x' => $x,
+                    'axis_y' => $y
+                ]);
             }
-            list($x, $y, $target) = explode('-', $point);
-            $new = MapPoint::create([
-                'location_id' => $location->id,
-                'target_id' => $target,
-                'axis_x' => $x,
-                'axis_y' => $y
-            ]);
         }
 
         // Delete old
