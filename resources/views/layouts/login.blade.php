8839f0c04e4dcc2bc9e11244a7ddfaab4931fd64
Jeremy Payne <jay@jpayne.ch>
2018-03-26 10:50:37 +0200

    Fix http assets on login page

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -20,7 +20,7 @@
     <!-- Font Awesome Icons -->
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
     <!-- Ionicons -->
-    <link href="http://code.ionicframework.com/ionicons/2.0.0/css/ionicons.min.css" rel="stylesheet" type="text/css" />
+    <link href="https://code.ionicframework.com/ionicons/2.0.0/css/ionicons.min.css" rel="stylesheet" type="text/css" />
     <link rel="icon" type="image/png" href="/favicon.ico">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
