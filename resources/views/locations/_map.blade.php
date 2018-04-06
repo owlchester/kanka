2e308df90cadad1f961f64f2e621f6c9804329c4
Jeremy Payne <jay@jpayne.ch>
2018-04-06 12:32:07 +0200

    Map: Being able to upload a map of your campaign, and create links on it to various entities #20 Better css

--- resources/views/locations/_map.blade.php
+++ resources/views/locations/_map.blade.php
@@ -7,7 +7,9 @@
     <div class="map">
         <img src="/storage/{{ $model->map }}" alt="{{ $model->name }}" />
         @foreach ($model->mapPoints()->with('location')->get() as $point)
-            <a class="point" style="top: {{ $point->axis_y }}px; left: {{ $point->axis_x }}px"  href="{{ route('locations.show', [$point->target, (!empty($point->target->map) ? '#tab_map' : null)]) }}" title="{{ $point->target->name }}"></a>
+            <a class="point" style="top: {{ $point->axis_y }}px; left: {{ $point->axis_x }}px"  href="{{ route('locations.show', [$point->target, (!empty($point->target->map) ? '#tab_map' : null)]) }}" title="{{ $point->target->name }}">
+                <i class="fa fa-map-marker"></i>
+            </a>
         @endforeach
     </div>
 @else
