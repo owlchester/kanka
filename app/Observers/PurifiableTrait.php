5e92e635c6955fd9d7475b5c20989fae7150b0f8
Jeremy Payne <jay@jpayne.ch>
2018-04-16 10:45:07 +0200

    Table styling #68

--- app/Observers/PurifiableTrait.php
+++ app/Observers/PurifiableTrait.php
@@ -12,7 +12,7 @@ trait PurifiableTrait
             'HTML.Allowed' =>
                 'big,small,h1,h2,h3,h4,h5,h6,img[src|style|alt|width|height],div,ins,del,pre,a[href|target],strong,em,s,u,ul,ol,li,p,'
                 . 'blockquote,sup,sub,'
-                . 'table,tbody,thead,tfoot,tr,td,hr,'
+                . 'table[style|border|cellpadding|cellspacing],tbody,thead,tfoot,tr[style],td[style],hr,caption'
         ];
 
         return Purify::clean($text, $config);
