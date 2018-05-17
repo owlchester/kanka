85d7332b7ddcc6f55ffd799c645786b4ab683664
Jeremy Payne <jay@jpayne.ch>
2018-05-17 14:52:42 +0200

    Upload all user files to AWS storage #3

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,13 @@
+@extends('errors::layout')
+
+@section('title', 'Update underway')
+
+@section('message')
+
+    <p>An update is underway!</p>
+
+    <p>Sorry for the inconvenience. These usually only take a few minutes.</p>
+
+    <p>To find out more, come over to the <a href="https://discord.gg/rhsyZJ4">Discord</a>.</p>
+
+@endsection
