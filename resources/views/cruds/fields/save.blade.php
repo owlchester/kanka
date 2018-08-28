d4470a4aea398d4fe8190fb6f77ecc4f2473c90a
Jeremy Payne <jay@jpayne.ch>
2018-08-28 10:29:05 +0200

    Save Actions: More Workflows #168

--- resources/views/cruds/fields/save.blade.php
+++ resources/views/cruds/fields/save.blade.php
@@ -1,4 +1,4 @@
-<div class="col-md-<?=(isset($saveColLength) ? $saveColLength : 12)?>">
+<div class="col-md-<?=(isset($saveColLength) ? $saveColLength : 12)?> margin-bottom">
     <div class="form-group">
         <div class="btn-group">
             <button class="btn btn-success" id="form-submit-main">{{ trans('crud.save') }}</button>
