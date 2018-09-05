52184f57631226a380935900d0a0c3325c9e807e
Jeremy Payne <jay@jpayne.ch>
2018-09-05 15:09:39 +0200

    Merge History and Description #142 Fix some forms

--- resources/views/items/_form.blade.php
+++ resources/views/items/_form.blade.php
@@ -60,20 +60,6 @@
                 <a href="{{ route('helpers.link') }}" target="_blank">{{ trans('crud.linking_help') }}</a>
             </div>
         </div>
-
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                <h4>{{ trans('crud.panels.entry') }}</h4>
-            </div>
-            <div class="panel-body">
-                <div class="form-group">
-                    {!! Form::textarea('entry', $formService->prefill('entry', $source), ['class' => 'form-control html-editor', 'id' => 'entry']) !!}
-                </div>
-            </div>
-            <div class="panel-footer">
-                <a href="{{ route('helpers.link') }}" target="_blank">{{ trans('crud.linking_help') }}</a>
-            </div>
-        </div>
     </div>
 </div>
 
