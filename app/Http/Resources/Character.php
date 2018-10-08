db4dfa8fc6cb84d4d4a069a617f9d3ac1c9ffee7
Jeremy Payne <jay@jpayne.ch>
2018-10-08 09:17:55 +0200

    APIs #181 Add character traits

--- app/Http/Resources/Character.php
+++ app/Http/Resources/Character.php
@@ -18,12 +18,13 @@ class Character extends EntityResource
             'title' => $this->title,
             'age' => $this->age,
             'sex' => $this->sex,
-            'race' => $this->race,
+            'race_id' => $this->race_id,
             'type' => $this->type,
 
             'family_id' => $this->family_id,
 
             'is_dead' => (bool) $this->is_dead,
+            'traits' => new CharacterTraitCollection($this->characterTraits),
         ]);
     }
 }
