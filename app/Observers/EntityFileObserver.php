cb9c64cee52fc5b625fe81aae051bcf605fa3926
J. Payne <j.payne@taktwerk.ch>
2019-01-18 16:13:02 +0100

    Entity Notes: Update Entity Last Modified - files

--- app/Observers/EntityFileObserver.php
+++ app/Observers/EntityFileObserver.php
@@ -24,8 +24,25 @@ class EntityFileObserver
     /**
      * @param EntityFile $entityFile
      */
+    public function saved(EntityFile $entityFile)
+    {
+        // When adding or changing an entity note to an entity, we want to update the
+        // last updated date to reflect changes in the dashboard.
+        $entityFile->entity->child->touch();
+    }
+
+    /**
+     * @param EntityFile $entityFile
+     */
     public function deleted(EntityFile $entityFile)
     {
         ImageService::cleanup($entityFile, 'path');
+
+        // When deleting an entity note, we want to update the entity's last update
+        // for the dashboard. Careful of this when deleting an entity, we could be
+        // entering a non-ending loop.
+        if ($entityFile->entity) {
+            $entityFile->entity->child->touch();
+        }
     }
 }
