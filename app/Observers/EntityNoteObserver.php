6ee95eb278da77569b6185f338e6877f7e693c68
J. Payne <j.payne@taktwerk.ch>
2019-01-18 16:10:30 +0100

    Entity Notes: Update Entity Last Modified

--- app/Observers/EntityNoteObserver.php
+++ app/Observers/EntityNoteObserver.php
@@ -38,4 +38,27 @@ class EntityNoteObserver
             $entityNote->is_private = false;
         }
     }
+
+    /**
+     * @param EntityNote $entityNote
+     */
+    public function saved(EntityNote $entityNote)
+    {
+        // When adding or changing an entity note to an entity, we want to update the
+        // last updated date to reflect changes in the dashboard.
+        $entityNote->entity->child->touch();
+    }
+
+    /**
+     * @param EntityNote $entityNote
+     */
+    public function deleted(EntityNote $entityNote)
+    {
+        // When deleting an entity note, we want to update the entity's last update
+        // for the dashboard. Careful of this when deleting an entity, we could be
+        // entering a non-ending loop.
+        if ($entityNote->entity) {
+            $entityNote->entity->child->touch();
+        }
+    }
 }
