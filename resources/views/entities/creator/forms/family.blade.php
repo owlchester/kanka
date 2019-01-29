29f4716bd3c6ffb64e55d0c9f56620ab1e5b27da
Jeremy Payne <jay@jpayne.ch>
2019-01-29 12:07:07 +0100

    Quick Creator: Family parent family

--- resources/views/entities/creator/forms/family.blade.php
+++ resources/views/entities/creator/forms/family.blade.php
@@ -1,1 +1,12 @@
+
+<div class="form-group">
+    {!! Form::select2(
+        'family_id',
+        (isset($model) && $model->family ? $model->family : $formService->prefillSelect('family', $source)),
+        App\Models\Family::class,
+        true,
+        'families.fields.family'
+    ) !!}
+</div>
+
 @include('cruds.fields.location')
