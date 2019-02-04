dd7d0fe0de8ddbbcd1be50564954650e36a2669f
Jeremy Payne <jay@jpayne.ch>
2019-02-04 13:48:19 +0100

    Translations: FR

--- resources/lang/fr/helpers.php
+++ resources/lang/fr/helpers.php
@@ -13,5 +13,6 @@ return [
         'description'   => 'Un lien vers une entité peut être facilement inséré en utilisant \'@\' dans le text. \'#\' peut être utilisé pour avoir une liste de mois depuis les calendriers de la campagne.',
         'title'         => 'Liens vers d\'autres éléments et raccourcis',
     ],
+    'public' => 'Une vidéo sur Youtube explique comment fonctionne les campagnes publiques.',
     'title'         => 'Aides',
 ];
