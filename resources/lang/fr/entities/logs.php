a1e2129d74a0b6a1963fd471b71a8249ac9c1027
Jeremy Payne <jay@jpayne.ch>
2019-03-15 10:33:05 +0100

    Translations: FR

---
+++ resources/lang/fr/entities/logs.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'actions'   => [
+        'create'    => 'Créé',
+        'delete'    => 'Supprimé',
+        'update'    => 'Mis à jour',
+    ],
+    'fields'    => [
+        'action'    => 'Action',
+        'date'      => 'Date',
+    ],
+    'show'      => [
+        'title' => 'Journaux de l\'entité :name',
+    ],
+];
---
+++ resources/lang/fr/entities/mentions.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'entity_note'   => ':name (Note d\'entité)',
+    'fields'        => [
+        'entity'    => 'Entité',
+    ],
+    'helper'        => 'Cette liste affiche les entités qui mentionnent cette entité dans leur description.',
+    'mentioned_in'  => 'Mentioné dans :count entités.',
+    'show'          => [
+        'title' => 'Mention d\'entité :name',
+    ],
+];
