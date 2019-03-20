c6b44f86b02a0e8ec8444d864143216b01e0565c
J. Payne <j.payne@taktwerk.ch>
2019-03-20 21:17:16 +0100

    Export

--- resources/views/cruds/export.blade.php
+++ resources/views/cruds/export.blade.php
@@ -11,7 +11,7 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 
     <!-- Styles -->
-    <link href="{{ asset('/css/export.css') }}" rel="stylesheet">
+    <link href="{{ url(mix('/css/export.css')) }}" rel="stylesheet">
     <style>
         @font-face {
             font-family: 'Open Sans';
