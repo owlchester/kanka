53783c30f4bc287a4575e6c98c1b480ca9a3635c
J. Payne <j.payne@taktwerk.ch>
2019-04-06 09:07:29 +0200

    Location Map: Private

--- app/Http/Resources/Location.php
+++ app/Http/Resources/Location.php
@@ -17,6 +17,7 @@ class Location extends EntityResource
         return $this->entity([
             'type' => $this->type,
             'map' => $this->map,
+            'is_map_private' => $this->is_map_private,
             'parent_location_id' => $this->parent_location_id,
         ]);
     }
