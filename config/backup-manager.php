e43eece7dca79b22241cf4b6c755e8dd5ac029f6
Jeremy Payne <jay@jpayne.ch>
2019-05-03 14:42:15 +0200

    DB Backup Manager

---
+++ config/backup-manager.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'local' => [
+        'type' => 'Local',
+        'root' => storage_path('app'),
+    ],
+    's3' => [
+        'type' => 'AwsS3',
+        'key'    => env('AWS_ACCESS_KEY_ID'),
+        'secret' => env('AWS_SECRET_ACCESS_KEY'),
+        'region' => env('AWS_DEFAULT_REGION'),
+        'bucket' => env('AWS_BUCKET_BACKUP'),
+        'root'   => '',
+    ],
+];
