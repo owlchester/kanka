bfa892828de9aa6fea914a9d331607d4a542def1
Jay Payne <jeremy@ilestis.ch>
2019-07-18 16:56:18 +0200

    FR: Spelling

--- resources/lang/fr/notifications.php
+++ resources/lang/fr/notifications.php
@@ -2,7 +2,7 @@
 
 return [
     'campaign'          => [
-        'export'        => 'Un export de la campagne est disponnible. <a href=":link">Télécharger</a>. Ce lien sera disponnible durant 30 minutes.',
+        'export'        => 'Un export de la campagne est disponible. <a href=":link">Télécharger</a>. Ce lien sera disponnible durant 30 minutes.',
         'export_error'  => 'Une erreure est survenue lors de l\'export de la campagne. Prière de nous contacter si ce problème persiste.',
         'join'          => ':user a rejoind la campagne :campaign.',
         'leave'         => ':user a quitté la campagne :campaign.',
