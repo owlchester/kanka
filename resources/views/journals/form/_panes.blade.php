ab83d6a1cd61affb102be1f4e4a085b558749acf
Jeremy Payne <jay@jpayne.ch>
2019-09-11 08:04:24 +0200

    Quest: calendar date when no calendar

--- resources/views/journals/form/_panes.blade.php
+++ resources/views/journals/form/_panes.blade.php
@@ -1,3 +1,5 @@
-<div class="tab-pane {{ (request()->get('tab') == 'form-calendar' ? ' active' : '') }}" id="form-calendar">
-    @include('cruds.forms._calendar', ['source' => $source])
-</div>
+@if ($campaign->enabled('calendars'))
+    <div class="tab-pane {{ (request()->get('tab') == 'form-calendar' ? ' active' : '') }}" id="form-calendar">
+        @include('cruds.forms._calendar', ['source' => $source])
+    </div>
+@endif
--- resources/views/quests/form/_tabs.blade.php
+++ resources/views/quests/form/_tabs.blade.php
@@ -1,5 +1,7 @@
+@if ($campaign->enabled('calendars'))
 <li class="{{ (request()->get('tab') == 'form-calendar' ? ' active' : '') }}">
     <a href="#form-calendar" title="{{ trans('crud.panels.calendar_date') }}" data-toggle="tooltip">
         {{ trans('crud.panels.calendar_date') }}
     </a>
 </li>
+@endif
