e1eff9b980ea3aaadb02df10fe7c187bb5745690
J. Payne <j.payne@taktwerk.ch>
2019-11-04 23:48:20 +0100

    Bulk Update

--- resources/views/cruds/fields/tags.blade.php
+++ resources/views/cruds/fields/tags.blade.php
@@ -1,4 +1,8 @@
 @if ($campaign->enabled('tags'))
+    @if (isset($bulk) && $bulk)
+        <div class="row">
+            <div class="col-md-8">
+    @endif
     <div class="form-group">
         {!! Form::tags(
             'tag_id',
@@ -10,12 +14,16 @@
     </div>
 
     @if (isset($bulk) && $bulk)
-        <div class="form-group">
-            <label for="bulk-tagging">{{ __('crud.bulk.edit.tagging') }}</label>
-            <select name="bulk-tagging" class="form-control">
-                <option value="add">{{ __('crud.bulk.edit.tags.add') }}</option>
-                <option value="remove">{{ __('crud.bulk.edit.tags.remove') }}</option>
-            </select>
+            </div>
+            <div class="col-md-4">
+                <div class="form-group">
+                    <label for="bulk-tagging">{{ __('crud.bulk.edit.tagging') }}</label>
+                    <select name="bulk-tagging" class="form-control">
+                        <option value="add">{{ __('crud.bulk.edit.tags.add') }}</option>
+                        <option value="remove">{{ __('crud.bulk.edit.tags.remove') }}</option>
+                    </select>
+                </div>
+            </div>
         </div>
     @endif
 @endif
