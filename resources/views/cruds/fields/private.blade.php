5616859beaa28b4880397d49a0014b3bee0575b1
J. Payne <j.payne@taktwerk.ch>
2019-12-18 19:06:00 +0100

    Entity Copy: Private not kept if activated

--- resources/views/cruds/fields/private.blade.php
+++ resources/views/cruds/fields/private.blade.php
@@ -2,9 +2,9 @@
     <hr />
     <div class="form-group">
         {!! Form::hidden('is_private', 0) !!}
-        <label>{!! Form::checkbox('is_private', 1, empty($model) ? CampaignLocalization::getCampaign()->entity_visibility : $model->is_private) !!}
-            {{ trans('crud.fields.is_private') }}
+        <label>{!! Form::checkbox('is_private', 1, empty($model) ? (!empty($source) ? $source->is_private : CampaignLocalization::getCampaign()->entity_visibility) : $model->is_private) !!}
+            {{ __('crud.fields.is_private') }}
         </label>
-        <p class="help-block">{{ trans('crud.hints.is_private') }}</p>
+        <p class="help-block">{{ __('crud.hints.is_private') }}</p>
     </div>
 @endif
