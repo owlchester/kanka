d3ad676ce5f9374389f73c5ad82cd55a09b863e4
Jay Payne <jeremy@ilestis.ch>
2020-02-04 16:48:31 +0100

    Front: FAQ redesign

--- resources/lang/en/faq.php
+++ resources/lang/en/faq.php
@@ -1,6 +1,13 @@
 <?php
 
 return [
+    'sections' => [
+        'general' => 'General',
+        'permissions' => 'Permissions',
+        'community' => 'Community',
+        'other' => 'Other',
+        'pricing' => 'Pricing',
+    ],
     'attribute-templates'   => [
         'answer'    => <<<'TEXT'
 The best way we can explain Attribute Templates is with an example. Let's imagine that your world has lots of Locations, and on many of those locations, you want to remember to create a custom Attribute for "Population", "Climate", "Crime Level". 
@@ -12,9 +19,25 @@ TEXT
 ,
         'question'  => 'Attribute Templates, what are they?',
     ],
+    'backup' =>  [
+        'answer' => 'Once a day, you can export all of your campaign\'s data as a ZIP file. In the app, click on "Campaign" on the left menu, and click on "Export". This will create an export that is available for 30 minutes. You can\'t upload this export to Kanka, it is only intended for your own peace of mind or if you no longer plan to use the app.',
+        'question' => 'How can I backup or export my campaign?',
+    ],
+    'bugs' => [
+        'answer' => 'Simply join our :discord server and report your bug in the #error-and-bugs channel.',
+        'question' => 'How can I report a bug?',
+    ],
     'conversations'         => [
         'answer'    => 'Conversations can be set up as talks between Characters or between Campaign Members. If for example you wish to document an important talk between NPCs and the PCs, you can do so using this module. You can also use them for play-by-post campaigns.',
         'question'  => 'What are Conversations?',
+    ],
+    'custom' => [
+        'answer' => 'Kanka comes with a set of predefined entity types that interact with each other. Allowing custom entity types would require rebuilding the app from scratch and defeat the purpose of a tool with predefined types to help out people worldbuild rather than figure out how to organise things. Furthermore, Kanka is flexible with Tags that can represent most custom entity type scenarios.',
+        'question' => 'Can I create custom entity types?'
+    ],
+    'delete-campaign' => [
+        'answer' => 'Go to your campaign dashboard, and click on "Campaign" on the left menu. A "Delete" campaign button will appear if you are the only admin in the campaign. Deleting a campaign is a permanent action that will delete all the data stored on our servers, including images.',
+        'question' => 'How can I delete a campaign?',
     ],
     'entity-notes'          => [
         'answer'    => 'All entities have an \'Entity Notes\' tab that are little snippets of text that can be set to only visible by you (great when co-dming), only for members of the admin role, or visible to all. You can also give your players permission to create and edit entity notes on entities without having to allow them editing a whole entity.',
@@ -37,7 +60,7 @@ TEXT
         'question'  => 'Will the app stay free?',
     ],
     'help'                  => [
-        'answer'    => 'Firstly, thank you for wanting to help out! We are always interested in people who can help out with translations, testing new features, or who can help out new users. We also love when people promote Kanka to reach new users in places we hadn\'t thought of. Your best course of action is to join us on the :discord where a channel is dedicated to helping out. We also love our patrons on Patreon if you wish to support us and get access to some perks!',
+        'answer'    => 'Firstly, thank you for wanting to help out! We are always interested in people who can help out with translations, testing new features, or who can help out new users. We also love when people promote Kanka to reach new users in places we hadn\'t thought of. Your best course of action is to join us on the :discord where a channel is dedicated to helping out. We also love our patrons on :patreon if you wish to support us and get access to some perks!',
         'question'  => 'I want to help! What can I do?',
     ],
     'map'                   => [
@@ -48,25 +71,29 @@ TEXT
         'question'  => 'Can I upload maps to Kanka?',
     ],
     'mobile'                => [
-        'answer'    => 'There currently is no dedicated mobile app for Kanka, but most of the app works on a mobile device. One limitation is the mentions tool not working in the text editor. If the support on Patreon permits it, I hope to pay someone to build a mobile app one day, but don\'t forsee that happening any time soon.',
+        'answer'    => 'There currently is no dedicated mobile app for Kanka, but most of the app works on a mobile device. One limitation is the mentions tool not working in the text editor. We hope that the support on :patreon allows us to pay someone to build a mobile app one day, but don\'t foresee that happening in the near future.',
         'question'  => 'Is there a mobile app? Is one planned?',
     ],
     'multiworld'            => [
-        'answer'    => 'No you don\'t! You can create as many "campaigns" in the app as you want, and have them represent worlds, settings, or whatever you want. Once you have several campaigns, you can easily switch between them',
-        'question'  => 'I am building several worlds in different settings, do I need a different account for each world?',
+        'answer'    => 'You can be a part of as many campaigns as you want, including those you\'ve created. To switch or create a new campaign, go to your campaign dashboard and in the top right you can click on your current campaign to display the campaign switcher interface.',
+        'question'  => 'Can I have more than one campaign?',
     ],
     'permissions'           => [
         'answer'    => 'Absolutely, this is why we built Kanka! You can invite all your players to your campaigns, and give them roles and permissions. We built the system to be extremely flexible (you can both use an opt-in and opt-out configuration) to cover as many needs and situations as possible.',
-        'question'  => 'I want to use Kanka to build my RPG world, but want my players to have access to some of the entities and edit their characters. Is this possible?',
+        'question'  => 'Can I limit the information my players see in my campaign?',
     ],
     'plans'                 => [
         'answer'    => <<<'TEXT'
 The long term plans for Kanka are to build a versatile worldbuilding and campaign management tool that is system agnostic with system specific content managed by the community in the form of "Community Templates". A longer goal is to build tools that integrate with other platforms like Virtual Table Top apps to link those with the worlds of Kanka.
         
-As for the second part, most hobby projects end up in burnout and the creator abandoning them. The Patreon is set up with the goal of me reducing my working hours to devote more time to Kanka without sacrificing my family's financial security, as well as covering the server costs. The project is also open source and can be picked up by the community if something were to ever happen to me. Each campaign's data can also be exported by the campaign admins once a day in case you are concerned about ever losing all your content.
+As for the second part, most hobby projects end up in burnout and the creator abandoning them. The :patreon is set up with the goal of us being able to work full time on Kanka without sacrificing our families' financial security, as well as covering the server costs. The project is also open source and can be picked up by the community if something were to ever happen to us.
 TEXT
 ,
-        'question'  => 'What are the long term plans? What if Ilestis gets bored of working on Kanka?',
+        'question'  => 'What are the long term plans?',
+    ],
+    'public-campaigns' => [
+        'answer' => 'You can browse the :public-campaigns page to see how others use Kanka for their campaigns.',
+        'question' => 'How do others use Kanka?'
     ],
     'show'                  => [
         'return'    => 'Return to the FAQ',
@@ -78,7 +105,7 @@ TEXT
         'question'  => 'My campaign permissions are set up, how can I test them?',
     ],
     'visibility'            => [
-        'answer'    => 'Only the people that you invite to your campaign can see and interact with that you have created. Your data is private and always in your control.',
+        'answer'    => 'Only the people that you invite to your campaign can see and interact with that you have created. Your data is private and always in your control. You can also set your campaign to public to allow unregistered users to view it.',
         'question'  => 'Can anyone see my world?',
     ],
 ];
