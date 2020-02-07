e9fe56c1957f4a57501b78b3a3301a6076eb777a
Jay Payne <jeremy@ilestis.ch>
2020-02-07 16:08:45 +0100

    Translations

---
+++ resources/lang/fr/front/community-votes.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'actions'       => [
+        'pricing_info'  => 'Soutiens Kanka pour participer aux votes communautaires.',
+        'return'        => 'Retour aux votes communautaires.',
+        'show'          => 'Afficher les résultats',
+        'vote'          => 'Voter',
+    ],
+    'description'   => 'Les utilisateurs qui supportent Kanka aident à façonné l\'évolution de l\'application en participant fréquement à des votes communautaires.',
+    'index'         => [
+        'past'      => 'Votes clos',
+        'voting'    => 'Votes activs',
+    ],
+    'latest'        => [
+        'title' => 'Votes récents',
+    ],
+    'show'          => [
+        'restricted'    => 'La participation aux votes communautaire est limitées à ceux qui soutiennent Kanka.',
+        'title'         => 'Vote Communautaire - :name',
+        'voted_lasted'  => 'Le vote s\'est déroulé de :from GMT jusqu\'à :until GTM.',
+        'voting_until'  => 'Vote ouvert jusqu\'à :until GMT.',
+    ],
+    'title'         => 'Votes Communautaires',
+];
---
+++ resources/lang/fr/front/news.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'actions'       => [
+        'read'      => 'Lire l\'article',
+        'return'    => 'Retour aux articles',
+    ],
+    'description'   => 'Rester à jour sur les fréquentes évolutions de Kanka.',
+    'latest'        => [
+        'title' => 'Dernières nouvelles',
+    ],
+    'show'          => [
+        'title' => 'Nouvelle :name',
+    ],
+    'title'         => 'Nouvelles',
+];
