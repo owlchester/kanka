3c37656eb9dd1a2d7db631499ab5b5e7ab500709
J. Payne <j.payne@taktwerk.ch>
2020-02-09 14:48:27 +0100

    Pinned Attributes: center sections

--- resources/views/entities/components/attributes.blade.php
+++ resources/views/entities/components/attributes.blade.php
@@ -7,7 +7,7 @@ $attributes = $model->entity->starredAttributes;
 ?>
 @if (count($attributes) > 0)
     @foreach ($attributes as $attribute)
-        <li class="list-group-item">
+        <li class="list-group-item @if ($attribute->isSection()) text-center @endif">
             @if ($attribute->isCheckbox())
                 @if ($attribute->value)
                     <i class="fa fa-check pull-right"></i>
