786e6021c04e9202b678cd507652672667700808
J. Payne <j.payne@taktwerk.ch>
2020-02-09 15:46:49 +0100

    API: Dice Rolls

--- app/Http/Resources/EntityResource.php
+++ app/Http/Resources/EntityResource.php
@@ -18,7 +18,7 @@ class EntityResource extends JsonResource
         $merged = [
             'id' => $this->id,
             'name' => $this->name,
-            'entry' => $this->entry,
+            'entry' => $this->getEntityType() == 'dice_roll' ? null : $this->entry,
             'image' => $this->image,
             'image_full' => $this->getImageUrl(),
             'image_thumb' => $this->getImageUrl(true),
