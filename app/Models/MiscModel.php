f0d2bd76ad593e69e075ffd8d7cd899e4d012f91
J. Payne <j.payne@taktwerk.ch>
2020-03-22 12:20:02 +0100

    Abilities: Don't allow entity abilities on abilities

--- app/Models/MiscModel.php
+++ app/Models/MiscModel.php
@@ -294,7 +294,7 @@ abstract class MiscModel extends Model
 
         // Each entity can have abilities
         $campaign = CampaignLocalization::getCampaign();
-        if ($campaign->enabled('abilities') && $campaign->boosted()) {
+        if ($campaign->enabled('abilities') && $campaign->boosted() && $this->entityTypeId() != config('entities.ids.ability')) {
             $items['abilities'] = [
                 'name' => 'crud.tabs.abilities',
                 'route' => 'entities.entity_abilities.index',
