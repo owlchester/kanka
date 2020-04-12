95825329422f36601a14f2e25040401671573511
J. Payne <j.payne@taktwerk.ch>
2020-04-12 16:34:11 +0200

    Subscription - css

--- resources/views/settings/subscription/billing.blade.php
+++ resources/views/settings/subscription/billing.blade.php
@@ -24,27 +24,30 @@
                         ]) !!}
                     </p>
 
-                    <strong>
-                        {{ __('settings.subscription.billing.saved' )}}
-                    </strong>
-                    <div id="billing">
-                        <billing-management
-                                api_token="{{ $stripeApiToken }}"
-                        ></billing-management>
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <strong>
+                                {{ __('settings.subscription.billing.saved' )}}
+                            </strong>
+                            <div id="billing">
+                                <billing-management
+                                        api_token="{{ $stripeApiToken }}"
+                                ></billing-management>
+                            </div>
+
+
+                            {!! Form::model(auth()->user(), ['method' => 'PATCH', 'route' => ['settings.billing.save']]) !!}
+                            <div class="form-group">
+                                <label>{{ trans('settings.subscription.fields.currency') }}</label>
+                                {!! Form::select('currency', ['' => trans('settings.subscription.currencies.usd'), 'eur' => trans('settings.subscription.currencies.eur')], null, ['class' => 'form-control']) !!}
+                            </div>
+                            <button class="btn btn-primary margin-bottom">
+                                {{ trans('settings.subscription.actions.update_currency') }}
+                            </button>
+                            {!! Form::close() !!}
+
+                        </div>
                     </div>
-
-
-
-                    {!! Form::model(auth()->user(), ['method' => 'PATCH', 'route' => ['settings.billing.save']]) !!}
-                    <div class="form-group">
-                        <label>{{ trans('settings.subscription.fields.currency') }}</label>
-                        {!! Form::select('currency', ['' => trans('settings.subscription.currencies.usd'), 'eur' => trans('settings.subscription.currencies.eur')], null, ['class' => 'form-control']) !!}
-                    </div>
-
-                    <button class="btn btn-primary margin-bottom">
-                        {{ trans('settings.subscription.actions.update_currency') }}
-                    </button>
-                    {!! Form::close() !!}
                 </div>
             </div>
         </div>
