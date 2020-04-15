5023e8ce456aa5eed3f523889f60e186d72107be
J. Payne <j.payne@taktwerk.ch>
2020-04-15 22:38:56 +0200

    Translations: HU

---
+++ resources/lang/hu/campaigns/recovery.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'actions'   => [
+        'recover'   => 'Visszaállítás',
+    ],
+    'error'     => 'Az entitások visszaállítása közben hiba lépett fel.',
+    'fields'    => [
+        'deleted'   => 'Törölt',
+    ],
+    'helper'    => 'A törölt entitások legfeljebb :count napig állíthatóak vissza, ezen a felületen keresztül.',
+    'success'   => '{1} :count entitás visszaállítva.|[2,*] :entitás visszaállítva.',
+    'title'     => 'Entitás Visszaállítás :campaign számára',
+];
