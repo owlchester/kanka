30bb2e11b8dbb3210dbe97cee08cb18679f4f7c1
Jay Payne <jeremy@ilestis.ch>
2020-04-17 16:15:41 +0200

    CalendarEvent und Exception handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -13,7 +13,7 @@ class Handler extends ExceptionHandler
      * @var array
      */
     protected $dontReport = [
-        //
+        \League\OAuth2\Server\Exception\OAuthServerException::class
     ];
 
     /**
