fcd271f22d3fccc35db2e2663f2732c45118cad3
J. Payne <j.payne@taktwerk.ch>
2020-05-23 18:36:53 +0200

    Translations

--- resources/lang/en/footer.php
+++ resources/lang/en/footer.php
@@ -5,4 +5,5 @@ return [
     'patreon'           => 'Become a Patreon',
     'subscribe'         => 'Subscribe to Kanka',
     'translator_call'   => 'Kanka is translated in other languages thanks to our amazing community. If you want to help translate Kanka into your language, contact us over on the :discord!',
+    'server_time' => 'Server time: :time',
 ];
