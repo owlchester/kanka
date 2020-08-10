b78f8b52a8b266bc1155b2a7e2dccfe1c0a11868
J. Payne <j.payne@taktwerk.ch>
2020-08-10 14:55:07 +0200

    Entity Tags: show tooltip

--- resources/views/entities/components/tags.blade.php
+++ resources/views/entities/components/tags.blade.php
@@ -1,11 +1,12 @@
-<?php /** @var \App\Models\Tag $section */?>
+<?php /** @var \App\Models\Tag $tag */?>
 @if ($campaign->enabled('tags') && $model->entity->tags()->count() > 0)
     <li class="list-group-item">
         <b>{{ trans('crud.fields.tags') }}</b>
         <p>
-            @foreach ($model->entity->tags as $section)
-                <a href="{{ route('tags.show', $section) }}">
-                    {!! $section->html() !!}
+            @foreach ($model->entity->tags()->with('entity')->get() as $tag)
+                <a href="{{ route('tags.show', $tag) }}" data-toggle="tooltip-ajax" data-id="{{ $tag->entity->id }}"
+                   data-url="{{ route('entities.tooltip', $tag->entity->id) }}">
+                    {!! $tag->html() !!}
                 </a>
             @endforeach
         </p>
