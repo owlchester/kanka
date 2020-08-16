09b2d45a7132cb13229b3ff5c3a8d5f29d771d95
J. Payne <j.payne@taktwerk.ch>
2020-08-16 22:31:22 +0200

    Timeline bugs QandA

--- resources/views/timelines/form/_entry.blade.php
+++ resources/views/timelines/form/_entry.blade.php
@@ -4,17 +4,6 @@
         @include('cruds.fields.name', ['trans' => 'timelines'])
         @include('cruds.fields.type', ['base' => \App\Models\Timeline::class, 'trans' => 'timelines'])
 
-        <div class="form-group">
-        {!! Form::hidden('revert_order', 0) !!}
-            <label>{!! Form::checkbox('revert_order') !!}
-                {{ __('timelines.fields.reverse_order') }}
-            </label>
-
-            <p class="help-block">
-                {!! __('timelines.helpers.reverse_order') !!}
-            </p>
-        </div>
-
         @include('cruds.fields.tags')
 
         @include('cruds.fields.private')
