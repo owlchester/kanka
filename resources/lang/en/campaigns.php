b7e5e63db774bc6584f0e512212660e0b197f449
J. Payne <j.payne@taktwerk.ch>
2020-08-22 09:35:17 +0200

    Fixes

--- resources/lang/en/campaigns.php
+++ resources/lang/en/campaigns.php
@@ -291,7 +291,7 @@ TEXT
         'edit'          => [
             'success'   => 'Campaign settings updated.',
         ],
-        'helper'        => 'All modules of a campaign can be enabled or disabled at will. Disabling a module will simply hide interface elements related to it, and pre-existing entities will be hidden but still exist in the background, in case you change your mind. These change effect all users of a campaign, including Admin users.',
+        'helper'        => 'All modules of a campaign can be enabled or disabled at will. Disabling a module will simply hide interface elements related to it, and pre-existing entities will be hidden but still exist in the background, in case you change your mind. These changes affect all users of a campaign, including Admin users.',
         'helpers'       => [
             'abilities'     => 'Create abilities, be it feats, spells, or powers that can be assigned to entities.',
             'calendars'     => 'A place to define the calendars of your world.',
@@ -304,7 +304,7 @@ TEXT
             'journals'      => 'Observations written by characters, or session prep for the dungeon master.',
             'locations'     => 'Planets, planes, continents, rivers, states, settlements, temples, taverns.',
             'maps'          => 'Upload maps with layers and markers pointing to other entities in the campaign.',
-            'menu_links'    => 'Custom menu links in the side bar.',
+            'menu_links'    => 'Set up links to entities or lists of the campaign in the sidebar.',
             'notes'         => 'Lore, religions, history, magic, races.',
             'organisations' => 'Cults, military units, factions, guilds.',
             'quests'        => 'To keep track of various quests with characters and locations.',
--- resources/lang/en/menu_links.php
+++ resources/lang/en/menu_links.php
@@ -2,17 +2,15 @@
 
 return [
     'create'        => [
-        'description'   => 'Create a new menu link',
-        'success'       => 'Menu Link \':name\' created.',
-        'title'         => 'New Menu Link',
+        'success'       => 'Quick Link \':name\' created.',
+        'title'         => 'New Quick Link',
     ],
     'destroy'       => [
-        'success'   => 'Menu Link \':name\' removed.',
+        'success'   => 'Menu Quick \':name\' removed.',
     ],
     'edit'          => [
-        'description'   => 'Edit a menu item.',
-        'success'       => 'Menu Link \':name\' updated.',
-        'title'         => 'Menu Link :name',
+        'success'       => 'Quick Link \':name\' updated.',
+        'title'         => 'Quick Link :name',
     ],
     'fields'        => [
         'entity'    => 'Entity',
@@ -24,28 +22,25 @@ return [
         'type'      => 'Entity Type',
     ],
     'helpers'       => [
-        'entity'    => 'Set up this menu link to go directly to an entity. The :tab field controls which of the tabs is focused. The :menu field controls which subpage of the entity is opened.',
+        'entity'    => 'Set up this quick link to go directly to an entity. The :tab field controls which of the tabs is focused. The :menu field controls which subpage of the entity is opened.',
         'position'  => 'Use this field to control in which ascending order the links appear in the menu.',
-        'type'      => 'Set up this menu link to go directly to a list of entities. To filter the results, copy parts of the url on the filtered entity list after the :? sign into the :filter field.',
+        'type'      => 'Set up this quick link to go directly to a list of entities. To filter the results, copy parts of the url on the filtered entity list after the :? sign into the :filter field.',
     ],
     'index'         => [
-        'add'           => 'New Menu Link',
-        'description'   => 'Manage the menu links of :name.',
-        'header'        => 'Menu Link of :name',
-        'title'         => 'Menu Links',
+        'add'           => 'New Quick Link',
+        'title'         => 'Quick Links',
     ],
     'placeholders'  => [
         'entity'    => 'Choose an entity',
         'filters'   => 'location_id=15&type=city',
         'menu'      => 'Menu subpage (use the last text of the url)',
-        'name'      => 'Name of the menu link',
+        'name'      => 'Name of the quick link',
         'tab'       => 'entry, relations, notes',
     ],
     'show'          => [
-        'description'   => 'A detailed view of a menu link',
         'tabs'          => [
             'information'   => 'Information',
         ],
-        'title'         => 'Menu Link :name',
+        'title'         => 'Quick Link :name',
     ],
 ];
