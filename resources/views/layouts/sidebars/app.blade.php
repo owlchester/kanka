4f2169cda592a17f226a10f317e931f75aee3cbe
J. Payne <j.payne@taktwerk.ch>
2020-08-23 13:03:18 +0200

    Campaign Switcher in Settings

--- resources/views/layouts/sidebars/app.blade.php
+++ resources/views/layouts/sidebars/app.blade.php
@@ -25,50 +25,8 @@ $defaultIndex = auth()->check() && auth()->user()->defaultNested ? 'tree' : 'ind
             </div>
         </section>
 
-        <section class="campaign-switcher collapse" id="campaign-switcher">
-            <div class="section">{{ __('sidebar.campaign_switcher.created_campaigns') }}</div>
+        @include('layouts.sidebars.campaign-switcher')
 
-            @if (Auth::check())
-            <ul>
-                @foreach (\App\Facades\UserCache::campaigns() as $userCampaign)
-                    @if ($userCampaign->id != $currentCampaign->id && !\App\Facades\Identity::isImpersonating())
-                        <li>
-                            @if ($userCampaign->image)
-                                <div class="background-image" style="background-image: url({{ Img::crop(208, 48)->url($userCampaign->image) }})"></div>
-                                <div class="background-gradient"></div>
-                            @endif
-                            <a href="{{ url(App::getLocale() . '/' . $userCampaign->getMiddlewareLink()) }}">
-                                {!! $userCampaign->name !!}
-                            </a>
-                        </li>
-                    @endif
-                @endforeach
-                @can('create', \App\Models\Campaign::class)
-                    <li class="bordered">
-                        <a href="{{ !Auth::user()->hasCampaigns() ? route('start') : route('campaigns.create') }}">
-                            <i class="fa fa-plus"></i> {{ __('sidebar.campaign_switcher.new_campaign') }}
-                        </a>
-                    </li>
-                @endcan
-            </ul>
-
-            @if (\App\Facades\UserCache::follows()->count() > 0)
-                <div class="section">{{ __('sidebar.campaign_switcher.public_campaigns') }}</div>
-                <ul>
-                    @foreach (\App\Facades\UserCache::follows() as $userCampaign)
-                        @if ($userCampaign->id != $currentCampaign->id && !\App\Facades\Identity::isImpersonating())
-                            <li>
-                                <a href="{{ url(App::getLocale() . '/' . $userCampaign->getMiddlewareLink()) }}">
-                                    {!! $userCampaign->name !!}
-                                </a>
-                            </li>
-                        @endif
-                    @endforeach
-                </ul>
-            @endif
-            @endif
-
-        </section>
         <section class="sidebar" style="height: auto">
             <ul class="sidebar-menu tree" data-widget="tree">
                 <li class="{{ $sidebar->active('dashboard') }}">
