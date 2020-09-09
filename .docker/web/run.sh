fae9c9d6292e2c54b264ee82f6b0fc12cfb14097
graham2071 <bidon.gamelle@gmail.com>
2020-09-09 16:23:20 +0200

    Update docker build
    The aim of this change to provide a build of kanka docker image and compose file to run kanka without the need to install anything (except docker). The docker build was outdated.
    Changes done:
    - include the app in the web image along with apache: this will permit to deploy the image to a docker registry, and users shall be able to run kafka without checking out sources
    - add redis to compose file
    - remove php artisan package:discover. This is because it requires a DB to run, but it is done during install step, when the DB is not available. This seems to be a bug in artisan.
    - remove all vagrant stuff
    - update README

--- .docker/web/run.sh
+++ .docker/web/run.sh
@@ -8,20 +8,23 @@ step() {
     printf "$1\n"
 }
 
-step "composer install"
-cd /var/www/html && composer install
+step "Wait for database up"
+wait-for database:3306
+
+# step "composer install"
+# cd /var/www/html && composer install
 
 step "Generating Key"
 cd /var/www/html && php artisan key:generate
 
-step "Creating storage link"
-cd /var/www/html && php artisan storage:link
+# step "Creating storage link"
+# cd /var/www/html && php artisan storage:link
 
 step "Installing voyager"
 cd /var/www/html && php artisan voyager:install
 
-step "Migrating the database"
-cd /var/www/html && php artisan migrate --force
+# step "Migrating the database"
+# cd /var/www/html && php artisan migrate --force
 
 # step "Seeding permissions"
 # cd /var/www/html && php artisan db:seed --class=BouncerSeeder
