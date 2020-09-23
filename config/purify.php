d424d38afb5788c50e6679227579bebbc09e78be
Jay Payne <jeremy@ilestis.ch>
2020-09-23 15:36:29 +0200

    Purify: allow margin-left and right

--- config/purify.php
+++ config/purify.php
@@ -111,7 +111,7 @@ return [
 
         'CSS.AllowedProperties' => 'font,font-size,font-weight,font-style,font-family,text-decoration,padding-left,' .
             'color,background-color,text-align,width,height,border,border-collapse,max-width,max-height,' .
-            'border-style,border-color,' .
+            'border-style,border-color,margin-left,margin-right,margin,padding,' .
             'list-style-type',
 
         /*
