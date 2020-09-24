3be51d018cf5977bf0b3a280178c962f205a1d42
Jay Payne <jeremy@ilestis.ch>
2020-09-24 19:15:17 +0200

    Quests: quest tab more useful info

--- resources/views/quests/_quests.blade.php
+++ resources/views/quests/_quests.blade.php
@@ -1,9 +1,13 @@
+@inject('dateRenderer', 'App\Renderers\DateRenderer')
+
 <p>{{ trans('quests.hints.quests') }}</p>
 <table id="quest-quests" class="table table-hover">
     <tbody><tr>
         <th class="avatar"><br /></th>
         <th>{{ trans('quests.fields.name') }}</th>
         <th>{{ trans('quests.fields.type') }}</th>
+        <th>{{ trans('quests.fields.date') }}</th>
+        <th>{{ trans('quests.fields.is_completed') }}</th>
         <th>&nbsp;</th>
     </tr>
     @foreach ($r = $model->quests()->has('quest')->orderBy('name', 'ASC')->paginate() as $model)
@@ -16,6 +20,17 @@
             </td>
             <td>
                 {{ $model->type }}
+            </td>
+            <td>
+                {{ $dateRenderer->render($model->date) }}
+            </td>
+            <td>
+                @if ($model->is_completed)
+                    <i class="fa fa-check-circle" title="{{ __('quests.fields.is_completed') }}"></i>
+                @endif
+            </td>
+            <td>
+
             </td>
         </tr>
     @endforeach
