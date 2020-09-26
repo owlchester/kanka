077d91c969b5d138efe8bf071f7db8561030d8d1
Jay Payne <jeremy@ilestis.ch>
2020-09-26 09:59:17 +0200

    Purify: allow code tag

--- config/purify.php
+++ config/purify.php
@@ -76,6 +76,7 @@ return [
             . 'p[class|style|id|dir],span[class|style|id|dir],'
             . 'table[class|summary|style|border|cellpadding|cellspacing|id],tbody,thead,tfoot,tr[class|style|id],td[class|style|abbr|colspan],th[class|style|abbr|colspan],'
             . 'acronym[title],abbr[title],'
+            . 'code[style|class|id],'
             . 'font[color|style],'
             . 'summary[class|style|id],details[class|style|id|open],'
             . 'iframe[src|width|height|style|class|scrolling|id]', // only use this with HTML.SafeIframe
