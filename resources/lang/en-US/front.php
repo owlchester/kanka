5d9365c63ce530295c606990833a562cbfb26286
Jay Payne <jeremy@ilestis.ch>
2020-09-29 11:52:33 +0200

    Translations

--- resources/lang/en-US/front.php
+++ resources/lang/en-US/front.php
@@ -8,7 +8,7 @@ return [
     ],
     'home'      => [],
     'master'    => [
-        'description'   => 'Kanka is an easy and flexible tool to worldbuild and manage your tabletop RPG campaign online. Create and organize people, places, organizations, events and much more.',
+        'description'   => 'Kanka is a community driven worldbuilding and tabletop RPG campaign management tool perfect for worldbuilders and game masters alike. We help you create and organise your campaigns and worlds with our @mentions system and a whole range of features such as calendars, interactive maps, timelines, organizations, families, and as many characters as you can come up with!',
         'title'         => 'Kanka',
     ],
 ];
