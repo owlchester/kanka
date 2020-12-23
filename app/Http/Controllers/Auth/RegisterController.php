b65a08ee533eeb1e71c4f7ab39c7219ea3ffdd98
Jay Payne <jeremy@ilestis.ch>
2020-12-23 14:04:46 -0600

    Spam: stop spam accounts

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Rules\AccountName;
 use App\Services\ReferralService;
 use App\User;
 use App\Http\Controllers\Controller;
@@ -60,7 +61,7 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => 'required|string|max:255|min:2',
+            'name' => ['required', 'string', 'max:255', 'min:2', new AccountName],
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6',
             'tos' => 'required'
---
+++ app/Rules/AccountName.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+use Illuminate\Support\Str;
+
+class AccountName implements Rule
+{
+    /**
+     * Create a new rule instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        return !Str::contains($value, ['<', '>', 'https']);
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return 'Invalid account name.';
+    }
+}
