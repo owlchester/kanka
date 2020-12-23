1ba94d5da36cb80133b20858dc6b1014f338f78e
Jay Payne <jeremy@ilestis.ch>
2020-12-23 13:19:16 -0600

    SEO: CLS

--- resources/views/front/master.blade.php
+++ resources/views/front/master.blade.php
@@ -15,7 +15,7 @@
             </div>
             <div class="col-lg-5 text-center">
                 <div class="youtube-placeholder" data-yt-url="https://www.youtube.com/embed/ZWVf7JAWKPg">
-                    <img src="https://kanka-app-assets.s3.amazonaws.com/images/front/play-youtube.jpg" async loading="lazy" class="play-youtube-video" alt="Youtube video" title="What is Kanka?" >
+                    <img src="https://kanka-app-assets.s3.amazonaws.com/images/front/play-youtube.jpg" async loading="lazy" class="play-youtube-video" alt="Youtube video" title="What is Kanka?" width="445" height="253" >
                 </div>
             </div>
         </div>
