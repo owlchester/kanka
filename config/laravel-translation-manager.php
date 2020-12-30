d0f132765be01772eca9f58538bfd9d6806915fc
Jay Payne <jeremy@ilestis.ch>
2020-12-30 11:46:04 -0600

    Swedish

--- config/laravel-translation-manager.php
+++ config/laravel-translation-manager.php
@@ -34,7 +34,8 @@ return array(
         'tr',
         'cz',
         'gl',
-        'nb'
+        'nb',
+        'sv',
     ],
     /**
      * Specify locales that you want to show in the web interface, if empty or not provided then all locales in the database
