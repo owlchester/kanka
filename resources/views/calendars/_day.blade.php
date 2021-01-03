e8b3e573bb4bc650b4fc58ca7fa096bfc3c242d0
Jay Payne <jeremy@ilestis.ch>
2021-01-03 13:56:24 -0600

    Calendar Reminder: custom colour

--- resources/views/calendars/_day.blade.php
+++ resources/views/calendars/_day.blade.php
@@ -44,7 +44,7 @@
                 @if (!empty($day['events']))
                     @foreach ($day['events'] as $event)
                         <?php /** @var \App\Models\EntityEvent $event */?>
-                        <div class="label calendar-event-block {{ $event->getLabelColour() }}" style="background-color: {{ $event->getLabelBackgroundColour() }}"
+                        <div class="label calendar-event-block {{ $event->getLabelColour() }}" style="background-color: {{ $event->getLabelBackgroundColour() }};"
                             @if ($canEdit)
                                 data-toggle="ajax-modal" data-target="#entity-modal" data-url="{{ route('entities.entity_events.edit', ($event->calendar_id !== $model->id ? [$event->entity->id, $event->id, 'from' => $model->calendar_id, 'next' => 'calendar.' . $model->id] : [$event->entity->id, $event->id])) }}"
                             @else
