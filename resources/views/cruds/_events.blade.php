3b0544e1a0aa1c3062bf58edd143392f2131afdf
Jay Payne <jeremy@ilestis.ch>
2021-01-15 15:33:56 -0600

    Calendars: custom reminder colours

--- resources/views/cruds/_events.blade.php
+++ resources/views/cruds/_events.blade.php
@@ -86,3 +86,15 @@ $r = $model
     </tbody></table>
 
 {{ $r->fragment('tab_calendars')->links() }}
+
+
+@section('scripts')
+    @parent
+    <script src="/vendor/spectrum/spectrum.js" defer></script>
+@endsection
+
+
+@section('styles')
+    @parent
+    <link href="/vendor/spectrum/spectrum.css" rel="stylesheet">
+@endsection
