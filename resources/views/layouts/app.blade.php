9556ffaa73bee7b041679defb794c3e1fff3ba8d
Jay Payne <jeremy@ilestis.ch>
2021-01-29 00:17:10 -0600

    Ads fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -132,9 +132,6 @@ $campaign = CampaignLocalization::getCampaign(); ?>
                 @include('partials.success')
 
 @if(!empty(config('tracking.adsense')) && (auth()->guest() || auth()->user()->showAds()) && !isset($skipBannerAd) && (!isset($sidebar) || $sidebar != 'settings'))
-                <script>
-                    (adsbygoogle = window.adsbygoogle || []).push({});
-                </script>
                 <p class="text-center text-muted">
                     {!! __('misc.ads.remove_v2', [
     'supporting' => link_to_route('settings.subscription', __('misc.ads.supporting'), [], ['target' => '_blank']),
