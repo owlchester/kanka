76342ad45f8e242481ed3854e5f618d466145ffb
Jay Payne <jeremy@ilestis.ch>
2021-02-24 21:41:02 -0600

    Reddit tracking for ads

--- resources/views/layouts/_tracking.blade.php
+++ resources/views/layouts/_tracking.blade.php
@@ -49,6 +49,13 @@
             value: 1,
             currency: 'USD',
         });
+        rdt('track', 'SignUp');
+    </script>
+@endif
+@if (!empty(config('tracking.reddit')))
+    <!-- Reddit Pixel -->
+    <script>
+        !function(w,d){if(!w.rdt){var p=w.rdt=function(){p.sendEvent?p.sendEvent.apply(p,arguments):p.callQueue.push(arguments)};p.callQueue=[];var t=d.createElement("script");t.src="https://www.redditstatic.com/ads/pixel.js",t.async=!0;var s=d.getElementsByTagName("script")[0];s.parentNode.insertBefore(t,s)}}(window,document);rdt('init','{{ config('tracking.reddit') }}');rdt('track', 'PageVisit');
     </script>
 @endif
 
