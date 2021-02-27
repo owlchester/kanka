281c69c0aebeda7577dc443ae213ea8eedc987c6
Salvatos <contact_abn@hotmail.com>
2021-02-26 23:38:02 -0500

    Update calendars.php
    it’s vs. its

--- resources/lang/en/calendars.php
+++ resources/lang/en/calendars.php
@@ -96,7 +96,7 @@ return [
     ],
     'helpers'       => [
         'month_type'    => 'Intercalary months don\'t use week days, but still influence moons and seasons.',
-        'nested'        => 'Displaying calendars that have no parent calendar first. Click on a calendar to explore it\'s sub calendars.',
+        'nested'        => 'Displaying calendars that have no parent calendar first. Click on a calendar to explore its sub calendars.',
         'start_offset'  => 'By default, the calendar starts on the first weekday of year 0. Changing this field influences where the calendar\'s first day is placed.',
     ],
     'hints'         => [
