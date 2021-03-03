e7dcf8a38e9801c0b2efb5d8369b9a15797a5f9f
Jay Payne <jeremy@ilestis.ch>
2021-03-03 11:09:12 -0600

    Njord spelling

--- resources/lang/en/front/features/calendars.php
+++ resources/lang/en/front/features/calendars.php
@@ -9,7 +9,7 @@ return [
     'second' => 'But it doesn\'t stop there. You can create moons, seasons, name your weeks, name your years, define the weather on any given day, and more.',
     'third' => 'Calendars comes with the options to set intercalary months, leap years, weekly and monthly resets. They can also be set to automatically advance the date by a day when the server clocks hit midnight. ',
     'fourth' => 'Once the calendar is set up, you can add reminders to them. Reminders are what links the entities of a campaign to a calendar. If a reminder links to an entity that the user can\'t see, the reminder won\'t be shown to them. Reminders can be recurring each month or each year. A reminder that has a length longer than two months will stop displaying in the calendar after the initial two months. We recommend creating two reminders for that scenario: one when it starts and one when it ends.',
-    'fifth' => 'Display a calendar directly on your campaign dashboards by including the calendar widget. This will also show recently past and upcoming events. Please note that the recurring events are less likely to show in the list of upcoming events.',
+    'fifth' => 'You can display a calendar directly on your campaign dashboards by including the calendar widget. This will also show recently finished and upcoming events. Please note that the recurring events are less likely to show in the list of upcoming events.',
 
     'example' => 'Explore an example of a calendar',
 ];
