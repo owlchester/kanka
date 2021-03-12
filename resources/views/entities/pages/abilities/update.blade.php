76c55ff6c0e6fc41e5ca69a7c124c27b13db5ea9
Jay Payne <jeremy@ilestis.ch>
2021-03-12 11:27:39 -0600

    Entity Abilities: parse note field for attributes and mentions

--- resources/views/entities/pages/abilities/update.blade.php
+++ resources/views/entities/pages/abilities/update.blade.php
@@ -37,9 +37,15 @@
             </div>
 
             <div class="form-group">
+                <div class="pull-right hidden-xs">
+                    <i class="fa fa-question-circle" title="{!! __('entities/abilities.helpers.note', [
+    'code' => '<code>[character:4096]</code>',
+    'attr' => '<code>{Strengh}</code>'
+]) !!}" data-toggle="tooltip" data-html="true" data-placement="left"></i>
+                </div>
                 <label>{{ __('entities/abilities.fields.note') }}</label>
                 {!! Form::textarea('note', null, ['class' => 'form-control', 'rows' => 4]) !!}
-                <p class="help-block">{!! __('entities/abilities.helpers.note', [
+                <p class="help-block hidden-sm hidden-md hidden-lg hidden-xl">{!! __('entities/abilities.helpers.note', [
     'code' => '<code>[character:4096]</code>',
     'attr' => '<code>{Strengh}</code>'
 ]) !!}</p>
