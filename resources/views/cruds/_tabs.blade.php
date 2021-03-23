844ccadd8a2ca30cbd9d606a28920713db3a406a
Jay Payne <jeremy@ilestis.ch>
2021-03-23 16:57:49 -0600

    Entity view: add data-tab attribute to tab li elements

--- resources/views/cruds/_tabs.blade.php
+++ resources/views/cruds/_tabs.blade.php
@@ -1,13 +1,13 @@
 
 @if (!isset($calendars) && $campaign->enabled('calendars'))
-    <li class="{{ (request()->get('tab') == 'calendars' ? ' active' : '') }}">
+    <li class="{{ (request()->get('tab') == 'calendars' ? ' active' : '') }}" data-tab="reminders">
         <a href="#calendars" title="{{ trans('crud.tabs.reminders') }}" data-toggle="tooltip">
             <i class="ra ra-moon-sun"></i> <span class="hidden-sm hidden-xs">{{ trans('crud.tabs.reminders') }}</span>
         </a>
     </li>
 @endif
 
-<li class="{{ (request()->get('tab') == 'notes' ? ' active' : '') }}">
+<li class="{{ (request()->get('tab') == 'notes' ? ' active' : '') }}" data-tab="secrets">
     <a href="#notes" title="{{ trans('crud.tabs.notes') }}" data-toggle="tooltip">
         <i class="fa fa-file"></i> <span class="hidden-sm hidden-xs">{{ trans('crud.tabs.notes') }}</span>
     </a>
@@ -15,7 +15,7 @@
 
 @if (!isset($disableAttributes))
 @can('attributes', $model->entity)
-<li class="{{ (request()->get('tab') == 'attribute' ? ' active' : '') }}">
+<li class="{{ (request()->get('tab') == 'attribute' ? ' active' : '') }}" data-tab="attributes">
     <a href="#attribute" title="{{ trans('crud.tabs.attributes') }}" data-toggle="tooltip">
         <i class="fa fa-th-list"></i> <span class="hidden-sm hidden-xs">{{ trans('crud.tabs.attributes') }}</span>
     </a>
