5bd397e47923e019181cbcfc53390c34fa97a651
Jay Payne <jeremy@ilestis.ch>
2021-04-26 10:25:58 -0600

    Typos

--- resources/lang/en/front/features/permissions.php
+++ resources/lang/en/front/features/permissions.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'description'   => 'Permissions in Kanka can be fine tuned to control exactly what a user can and can\'t to. You can be as open or restrictive as you want, depending on your needs.',
+    'description'   => 'Permissions in Kanka can be fine tuned to control exactly what a user can and can\'t do. You can be as open or restrictive as you want, depending on your needs.',
     'fifth'         => 'Most entities have a "privacy" checkbox option which will supersede all other permissions. When checked, only members of the campaign\'s admin role will see the entity.',
     'first'         => 'Kanka\'s permissions is split into several concepts: role permissions and entity permissions. By default, each campaign comes with an Admin, a Public and a Player role. Members of the admin role can see and do everything in a campaign. The public role is used if the campaign is public and a user isn\'t part of it and by default the role has no permissions. Lastly, the player role also comes with no permissions by default, and is used for anyone who is part of the campaign. When you invite a new member to your campaign, you define what role they join as. You can create more roles, and move people around, as well as have members in several roles.',
     'fourth'        => 'To check the permissions of a user, go to your campaign\'s members page and click on the "View as" button. This button is only available to campaign admins, and can only be used on non campaign admins. When creating or editing an entity using this feature, the info will be saved in the entity\'s logs.',
