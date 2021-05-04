dd1535f2871d1981bdbd19fbc1582795c8ff47af
Jay Payne <jeremy@ilestis.ch>
2021-05-04 15:11:10 -0600

    Campaigns: allow disabling the inventories module

--- app/Models/MiscModel.php
+++ app/Models/MiscModel.php
@@ -371,13 +371,15 @@ abstract class MiscModel extends Model
         }
 
         // Each entity can have an inventory
-        $items['inventory'] = [
-            'name' => 'crud.tabs.inventory',
-            'route' => 'entities.inventory',
-            'count' => $this->entity->inventories()->has('item')->count(),
-            'entity' => true,
-            'icon' => 'ra ra-round-bottom-flask',
-        ];
+        if ($campaign->enabled('inventories')) {
+            $items['inventory'] = [
+                'name' => 'crud.tabs.inventory',
+                'route' => 'entities.inventory',
+                'count' => $this->entity->inventories()->has('item')->count(),
+                'entity' => true,
+                'icon' => 'ra ra-round-bottom-flask',
+            ];
+        }
 
         // Each entity can have abilities
         if ($campaign->enabled('abilities') && $this->entityTypeId() != config('entities.ids.ability')) {
@@ -548,11 +550,14 @@ abstract class MiscModel extends Model
                     <i class="fa fa-users" aria-hidden="true"></i> ' . __('crud.tabs.relations') . '
                 </a>
             </li>';
-            $actions[] = '<li>
+
+            if ($campaign->enabled('inventories')) {
+                $actions[] = '<li>
                 <a href="' . route('entities.inventory', $this->entity) . '" class="dropdown-item">
                     <i class="ra ra-round-bottom-flask" aria-hidden="true"></i> ' . __('crud.tabs.inventory') . '
                 </a>
             </li>';
+            }
 
             if ($campaign->enabled('abilities') && $this->entityTypeId() != config('entities.ids.ability')) {
                 $actions[] = '<li>
