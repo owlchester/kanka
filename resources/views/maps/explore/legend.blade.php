53efed6ae0f46d2c54f668a95fa4d27eb7b73e58
Jay Payne <jeremy@ilestis.ch>
2021-05-24 14:45:53 -0600

    Map Legend typo

--- resources/views/maps/explore/legend.blade.php
+++ resources/views/maps/explore/legend.blade.php
@@ -9,12 +9,12 @@
     @foreach ($map->legendMarkers(false) as $marker)
         <li>
             @if(isset($marker['markers']))
-                <a href="#" class="map-legend-marker map-legend-group" data-toggle="collapse" data-target="#map-legend-group-{{ $marker['id'] }}">{!! $marker['name'] !!} w</a>
+                <a href="#" class="map-legend-marker map-legend-group" data-toggle="collapse" data-target="#map-legend-group-{{ $marker['id'] }}">{!! $marker['name'] !!}</a>
 
                 <ul class="collapse in" id="map-legend-group-{{ $marker['id'] }}">
                     @foreach ($marker['markers'] as $mk)
                         <li>
-                            <a href="#" class="map-legend-marker" data-lng="{{ $mk['longitude'] }}" data-lat="{{ $mk['latitude'] }}" data-id="marker{{ $mk['id'] }}">{!! $mk['name'] !!} a</a>
+                            <a href="#" class="map-legend-marker" data-lng="{{ $mk['longitude'] }}" data-lat="{{ $mk['latitude'] }}" data-id="marker{{ $mk['id'] }}">{!! $mk['name'] !!}</a>
                         </li>
                     @endforeach
                 </ul>
