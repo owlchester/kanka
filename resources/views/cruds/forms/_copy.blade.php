c31a0747839642954a93be772cc74959334e2bd1
TheCakeIsNotALie <yannisp09@gmail.com>
2021-07-07 13:14:27 +0200

    Changed copy_source_notes default to always true

--- resources/views/cruds/forms/_copy.blade.php
+++ resources/views/cruds/forms/_copy.blade.php
@@ -1,17 +1,17 @@
 <p class="help-block">{{ __('crud.helpers.copy_options') }}</p>
 <div class="form-group">
     {!! Form::hidden('copy_source_notes', null) !!}
-    <label>{!! Form::checkbox('copy_source_notes', 1, request()->filled('template')) !!}
+    <label>{!! Form::checkbox('copy_source_notes', 1, true) !!}
         {{ __('crud.fields.copy_notes') }}
     </label>
 </div>
 @if ($campaign->campaign()->boosted())
-<div class="form-group">
-    {!! Form::hidden('copy_source_links', null) !!}
-    <label>{!! Form::checkbox('copy_source_links', 1, request()->filled('template')) !!}
-        {{ __('crud.fields.copy_links') }}
-    </label>
-</div>
+    <div class="form-group">
+        {!! Form::hidden('copy_source_links', null) !!}
+        <label>{!! Form::checkbox('copy_source_links', 1, request()->filled('template')) !!}
+            {{ __('crud.fields.copy_links') }}
+        </label>
+    </div>
 @endif
 <div class="form-group">
     {!! Form::hidden('copy_source_abilities', null) !!}
@@ -26,7 +26,8 @@
     </label>
 </div>
 
-@if(view()->exists($name . '.form._copy'))
+@if (view()->exists($name . '.form._copy'))
     @include($name . '.form._copy')
 @endif
-<input type="hidden" name="copy_source_id" value="{{ !empty($source) ? (!empty($source->entity) ? $source->entity->id : $source->id) : old('copy_source_id') }}">
+<input type="hidden" name="copy_source_id"
+    value="{{ !empty($source) ? (!empty($source->entity) ? $source->entity->id : $source->id) : old('copy_source_id') }}">
