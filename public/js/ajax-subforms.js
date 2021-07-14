64db2344a837d281d4e4abe61f2c113384a86a02
TheCakeIsNotALie <yannisp09@gmail.com>
2021-07-14 13:43:43 +0200

    Added images names in gallery

--- public/js/ajax-subforms.js
+++ public/js/ajax-subforms.js
@@ -1,1 +1,226 @@
-!function(t){var e={};function n(r){if(e[r])return e[r].exports;var o=e[r]={i:r,l:!1,exports:{}};return t[r].call(o.exports,o,o.exports,n),o.l=!0,o.exports}n.m=t,n.c=e,n.d=function(t,e,r){n.o(t,e)||Object.defineProperty(t,e,{enumerable:!0,get:r})},n.r=function(t){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(t,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(t,"__esModule",{value:!0})},n.t=function(t,e){if(1&e&&(t=n(t)),8&e)return t;if(4&e&&"object"==typeof t&&t&&t.__esModule)return t;var r=Object.create(null);if(n.r(r),Object.defineProperty(r,"default",{enumerable:!0,value:t}),2&e&&"string"!=typeof t)for(var o in t)n.d(r,o,function(e){return t[e]}.bind(null,o));return r},n.n=function(t){var e=t&&t.__esModule?function(){return t.default}:function(){return t};return n.d(e,"a",e),e},n.o=function(t,e){return Object.prototype.hasOwnProperty.call(t,e)},n.p="/",n(n.s=2)}({2:function(t,e,n){t.exports=n("P87i")},P87i:function(t,e){function n(){var t=$(".ajax-subform");0!==t.length&&(t.off("submit"),t.on("submit",(function(t){if($(this).attr("is-valid"))return!0;window.entityFormHasUnsavedChanges=!1,t.preventDefault();var e=$(this);e.find(".submit-group").hide(),e.find(".submit-animation").show(),$.ajaxSetup({headers:{"X-CSRF-TOKEN":$('meta[name="csrf-token"]').attr("content")}});var n=new FormData(this);$.ajax({url:$(this).attr("action"),method:$(this).attr("method"),data:n,cache:!1,contentType:!1,processData:!1}).done((function(t){e.attr("is-valid",!0),e.submit()})).fail((function(t){e.find(".input-error").removeClass("input-error"),e.find(".text-danger").remove(),503===t.status&&($("#entity-form-503-error").show(),r(e)),403===t.status&&($("#entity-form-403-error").show(),r(e));var n=t.responseJSON.errors,o=Object.keys(n);o.forEach((function(t){$('[name="'+t+'"]').length>0?e.find('[name="'+t+'"]').addClass("input-error").parent().append('<div class="text-danger">'+n[t][0]+"</div>"):!1}));var i=Object.keys(n)[0],a=e.find('[name="'+i+'"]');a.length>0&&a.focus(),r(e)}))})))}function r(t){t.find(".submit-group").show(),t.find(".submit-animation").hide()}$(document).ready((function(){n(),$(document).on("shown.bs.modal shown.bs.popover",(function(){n()}))}))}});
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 2);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./resources/assets/js/ajax-subforms.js":
+/*!**********************************************!*\
+  !*** ./resources/assets/js/ajax-subforms.js ***!
+  \**********************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+/**
+ * Contains standard ajax process for forms.
+ *
+ * Wartning /!\ : This will replace submit events on the form itself!
+ * Usage: <form> tag must have class : 'ajax-subforms'
+ *        Submit button or button group must be enclosed in a <div> that has the class : 'submit-group'
+ */
+$(document).ready(function () {
+  initSubforms();
+  $(document).on('shown.bs.modal shown.bs.popover', function () {
+    initSubforms();
+  });
+});
+
+function initSubforms() {
+  //console.info('Init Ajax Subforms');
+  var subForms = $('.ajax-subform');
+
+  if (subForms.length === 0) {
+    //console.info('not ajax subforms');
+    return;
+  } //remove current submit event just in case it isn't clear
+
+
+  subForms.off('submit');
+  subForms.on('submit', function (e) {
+    //console.log('Ajax subform submitted', $(this));
+    //Get the validity status of the form
+    var formIsValid = $(this).attr('is-valid'); //console.log("Form validity", formIsValid);
+
+    if (formIsValid) {
+      //console.log("Ajax subform already validated, sending", $(this));
+      //do nothing and send form
+      return true;
+    } //else form is not confirmed valid
+    //disable global alert when redirection occurs
+
+
+    window.entityFormHasUnsavedChanges = false;
+    e.preventDefault(); //show button animation
+
+    var currentAjaxForm = $(this);
+    currentAjaxForm.find('.submit-group').hide();
+    currentAjaxForm.find('.submit-animation').show(); // Allow ajax requests to use the X_CSRF_TOKEN for deletes
+
+    $.ajaxSetup({
+      headers: {
+        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+      }
+    }); //send request to server
+
+    var formData = new FormData(this);
+    $.ajax({
+      url: $(this).attr('action'),
+      method: $(this).attr('method'),
+      data: formData,
+      cache: false,
+      contentType: false,
+      processData: false
+    }).done(function (res) {
+      // If the validation succeeded, confirm its validity
+      currentAjaxForm.attr('is-valid', true); // resubmit the form
+
+      currentAjaxForm.submit();
+    }).fail(function (err) {
+      //console.log('error', err);
+      // Reset any error fields
+      currentAjaxForm.find('.input-error').removeClass('input-error');
+      currentAjaxForm.find('.text-danger').remove(); // /?\ how do the 503/403 error ids work ?
+      // If we have a 503 error status, let's assume it's from cloudflare and help the user
+      // properly save their data.
+
+      if (err.status === 503) {
+        $('#entity-form-503-error').show();
+        resetSubformSubmitAnimation(currentAjaxForm);
+      } // If it's 403, the session is gone
+
+
+      if (err.status === 403) {
+        $('#entity-form-403-error').show();
+        resetSubformSubmitAnimation(currentAjaxForm);
+      } // Loop through the errors to add the class and error message
+
+
+      var errors = err.responseJSON.errors;
+      var errorKeys = Object.keys(errors);
+      var foundAllErrors = true;
+      errorKeys.forEach(function (i) {
+        var errorSelector = $('[name="' + i + '"]'); //console.log('error field', '[name="' + i + '"]');
+
+        if (errorSelector.length > 0) {
+          currentAjaxForm.find('[name="' + i + '"]').addClass('input-error').parent().append('<div class="text-danger">' + errors[i][0] + '</div>');
+        } else {
+          foundAllErrors = false;
+        }
+      });
+      var firstItem = Object.keys(errors)[0];
+      var firstItemDom = currentAjaxForm.find('[name="' + firstItem + '"]'); // If we can actually find the first element, switch to it and the correct tab.
+
+      if (firstItemDom.length > 0) {
+        firstItemDom.focus();
+      } // Reset submit buttons
+
+
+      resetSubformSubmitAnimation(currentAjaxForm);
+    });
+  });
+}
+
+function resetSubformSubmitAnimation(form) {
+  //console.log("Resetting ajax subform animation");
+  //reset animation
+  form.find('.submit-group').show();
+  form.find('.submit-animation').hide();
+}
+
+/***/ }),
+
+/***/ 2:
+/*!****************************************************!*\
+  !*** multi ./resources/assets/js/ajax-subforms.js ***!
+  \****************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! /mnt/c/Users/yanni/Desktop/kanka/miscellany/resources/assets/js/ajax-subforms.js */"./resources/assets/js/ajax-subforms.js");
+
+
+/***/ })
+
+/******/ });
--- public/js/entity.js
+++ public/js/entity.js
@@ -1,1 +1,161 @@
-!function(t){var e={};function n(r){if(e[r])return e[r].exports;var a=e[r]={i:r,l:!1,exports:{}};return t[r].call(a.exports,a,a.exports,n),a.l=!0,a.exports}n.m=t,n.c=e,n.d=function(t,e,r){n.o(t,e)||Object.defineProperty(t,e,{enumerable:!0,get:r})},n.r=function(t){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(t,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(t,"__esModule",{value:!0})},n.t=function(t,e){if(1&e&&(t=n(t)),8&e)return t;if(4&e&&"object"==typeof t&&t&&t.__esModule)return t;var r=Object.create(null);if(n.r(r),Object.defineProperty(r,"default",{enumerable:!0,value:t}),2&e&&"string"!=typeof t)for(var a in t)n.d(r,a,function(e){return t[e]}.bind(null,a));return r},n.n=function(t){var e=t&&t.__esModule?function(){return t.default}:function(){return t};return n.d(e,"a",e),e},n.o=function(t,e){return Object.prototype.hasOwnProperty.call(t,e)},n.p="/",n(n.s=8)}({8:function(t,e,n){t.exports=n("TO7d")},TO7d:function(t,e){$(document).ready((function(){$(".entity-private-toggle").click((function(){$(this).addClass("disabled"),$(this).children("i.fa").removeClass().addClass("fa fa-spin fa-spinner"),$.post({url:$(this).data("url"),data:{},context:this}).done((function(t){if(t.success){var e=$(this).children("i.fa"),n=t.status?$(e).data("off"):$(e).data("on"),r=t.status?$(e).data("title-off"):$(e).data("title-on");e.removeClass().addClass("fa").addClass("fa-"+n).attr("title",r),$(this).removeClass("disabled")}}))}))}))}});
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 8);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./resources/assets/js/entity.js":
+/*!***************************************!*\
+  !*** ./resources/assets/js/entity.js ***!
+  \***************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+var entityFileUi, entityFileModal;
+var entityFileDrop, entityFileProgress, entityFileMax;
+var openingEntityFileModal = false;
+$(document).ready(function () {
+  registerPrivacyToggle();
+});
+/**
+ *
+ * @param data
+ * @returns {string}
+ */
+
+function buildErrors(data) {
+  var errors = '';
+
+  for (var key in data) {
+    // skip loop if the property is from prototype
+    if (!data.hasOwnProperty(key)) continue;
+    errors += data[key] + "\n";
+  }
+
+  return errors;
+}
+/**
+ * Toggle the privacy of an entity
+ */
+
+
+function registerPrivacyToggle() {
+  $('.entity-private-toggle').click(function () {
+    $(this).addClass('disabled');
+    var child = $(this).children('i.fa');
+    child.removeClass().addClass('fa fa-spin fa-spinner');
+    $.post({
+      url: $(this).data('url'),
+      data: {},
+      context: this
+    }).done(function (res) {
+      if (!res.success) {
+        return;
+      }
+
+      var child = $(this).children('i.fa');
+      var cssClass = res.status ? $(child).data('off') : $(child).data('on');
+      var title = res.status ? $(child).data('title-off') : $(child).data('title-on');
+      child.removeClass().addClass('fa').addClass('fa-' + cssClass).attr('title', title);
+      $(this).removeClass('disabled');
+    });
+  });
+}
+
+/***/ }),
+
+/***/ 8:
+/*!******************************************!*\
+  !*** multi ./resources/assets/js/entity ***!
+  \******************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! /mnt/c/Users/yanni/Desktop/kanka/miscellany/resources/assets/js/entity */"./resources/assets/js/entity.js");
+
+
+/***/ })
+
+/******/ });
