758ff246fed617aafa704e74bc75081d9f6490ed
Jay Payne <jeremy@ilestis.ch>
2021-07-14 13:14:04 +0200

    Attributes: correct order revert

--- resources/views/entities/pages/attributes/render.blade.php
+++ resources/views/entities/pages/attributes/render.blade.php
@@ -18,6 +18,6 @@ if ($layout) {
     @include('cruds.attributes.marketplace_template', ['plugin' => $marketplaceTemplate])
 @else
     @include('entities.pages.attributes._attributes', [
-        'attributes' => $entity->entityAttributes
+        'attributes' => $entity->attributes()->with('entity')->order(request()->get('order'), 'default_order')->get()
     ])
 @endif
