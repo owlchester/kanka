1039abd8a09e53b608c7bedaf23de99463df7ae1
Jay Payne <jeremy@ilestis.ch>
2021-07-16 09:41:51 +0200

    Remove tooltips on tabs

--- resources/views/calendars/form/_tabs.blade.php
+++ resources/views/calendars/form/_tabs.blade.php
@@ -1,23 +1,23 @@
 <li class="{{ (request()->get('tab') == 'form-calendar' ? ' active' : '') }}">
-    <a href="#form-calendar" title="{{ trans('crud.fields.calendar') }}" data-toggle="tooltip">
+    <a href="#form-calendar" title="{{ trans('crud.fields.calendar') }}">
         <i class="ra ra-moon-sun visible-xs"></i> <span class="hidden-xs">{{ trans('crud.fields.calendar') }}</span>
     </a>
 </li>
 <li class="{{ (request()->get('tab') == 'form-months' ? ' active' : '') }}">
-    <a href="#form-months" title="{{ trans('calendars.panels.months') }}" data-toggle="tooltip">
+    <a href="#form-months" title="{{ trans('calendars.panels.months') }}">
         {{ trans('calendars.panels.months') }}
     </a>
 <li class="{{ (request()->get('tab') == 'form-weeks' ? ' active' : '') }}">
-    <a href="#form-weeks" title="{{ trans('calendars.panels.weeks') }}" data-toggle="tooltip">
+    <a href="#form-weeks" title="{{ trans('calendars.panels.weeks') }}">
         {{ trans('calendars.panels.weeks') }}
     </a>
 </li>
 <li class="{{ (request()->get('tab') == 'form-moons' ? ' active' : '') }}">
-    <a href="#form-moons" title="{{ trans('calendars.fields.moons') }}" data-toggle="tooltip">
+    <a href="#form-moons" title="{{ trans('calendars.fields.moons') }}">
         <i class="far fa-moon visible-xs"></i> <span class="hidden-xs">{{ trans('calendars.fields.moons') }}</span>
     </a>
 </li><li class="{{ (request()->get('tab') == 'form-seasons' ? ' active' : '') }}">
-    <a href="#form-seasons" title="{{ trans('calendars.fields.seasons') }}" data-toggle="tooltip">
+    <a href="#form-seasons" title="{{ trans('calendars.fields.seasons') }}">
         {{ trans('calendars.fields.seasons') }}
     </a>
 </li>
