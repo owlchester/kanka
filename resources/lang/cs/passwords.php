beee96b5a528fd13af0751eea288b6f24ec46efd
Jay Payne <jeremy@ilestis.ch>
2021-07-21 14:49:29 +0200

    CZ translations prepare

--- resources/lang/cs/passwords.php
+++ resources/lang/cs/passwords.php
@@ -1,17 +1,17 @@
 <?php
 
-return [
-    /*
-    |--------------------------------------------------------------------------
-    | Password Reminder Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines are the default lines which match reasons
-    | that are given by the password broker for a password update attempt
-    | has failed, such as for an invalid token or invalid new password.
-    |
-    */
+/*
+|--------------------------------------------------------------------------
+| Password Reset Language Lines
+|--------------------------------------------------------------------------
+|
+| The following language lines are the default lines which match reasons
+| that are given by the password broker for a password update attempt
+| has failed, such as for an invalid token or invalid new password.
+|
+*/
 
+return [
     'reset'     => 'Heslo bylo obnoveno!',
     'sent'      => 'E-mail s instrukcemi k obnovení hesla byl odeslán!',
     'throttled' => 'Počkejte prosím a zkuste to znovu.',
