8b49cc127d61728f324f7c7fd00eaaeb295428bb
Jay Payne <jeremy@ilestis.ch>
2021-08-19 11:19:47 +0200

    Role member form: correct key

--- resources/views/campaigns/roles/users/_form.blade.php
+++ resources/views/campaigns/roles/users/_form.blade.php
@@ -2,7 +2,7 @@
 <div class="row">
     <div class="col-md-12">
         <div class="form-group required">
-            <label>{{ trans('campaigns.roles.fields.name') }}</label>
+            <label>{{ trans('campaigns.members.fields.name') }}</label>
             {!! Form::select('user_id', $campaign->membersList($role->users->pluck('user_id')->toArray()), null, ['class' => 'form-control']) !!}
         </div>
     </div>
