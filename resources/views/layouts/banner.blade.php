e9aee1bf31dc12bb5047f2200ab5d21e4d88e3a2
Jay Payne <jeremy@ilestis.ch>
2021-10-18 11:18:42 -0500

    Fix promo code

--- resources/views/layouts/banner.blade.php
+++ resources/views/layouts/banner.blade.php
@@ -2,7 +2,7 @@
     <div class="bg-primary banner-notification">
         <span>
             <a href="{{ route('settings.subscription') }}">
-                {!! __('banners.kanka4years', ['code' => '<code>KANKA4YEARS</code>']) !!}
+                {!! __('banners.kanka4years', ['code' => '<code>KANKA4YEAR</code>']) !!}
             </a>
 
             <button type="button" class="close" data-dismiss="alert" aria-hidden="true" id="banner-notification-dismiss" data-url="{{ route('settings.banner', ['code' => '4yearpromo']) }}">×</button>
