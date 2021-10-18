f15d86cf82cf296b2c5246a003d8aa730b881371
Jay Payne <jeremy@ilestis.ch>
2021-10-18 10:16:41 -0500

    Promo: themes & translation

---
+++ resources/lang/en/banners.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'kanka4years' => 'Use promo code :code to get 20% off your first yearly subscription!',
+];
--- resources/views/layouts/banner.blade.php
+++ resources/views/layouts/banner.blade.php
@@ -1,9 +1,11 @@
 @if (auth()->check() && auth()->user()->subscribed('kanka') && !auth()->user()->settings->get('banner_4yearpromo'))
     <div class="bg-orange banner-notification">
-                <span>
-                    Use promo code <code>KANKA4YEARS</code> to get 20% off your first yearly <a href="{{ route('settings.subscription') }}">subscription</a>!
+        <span>
+            <a href="{{ route('settings.subscription') }}">
+                {!! __('banners.kanka4years', ['code' => '<code>KANKA4YEARS</code>']) !!}
+            </a>
 
-                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true" id="banner-notification-dismiss" data-url="{{ route('settings.banner', ['code' => '4yearpromo']) }}">×</button>
-                </span>
+            <button type="button" class="close" data-dismiss="alert" aria-hidden="true" id="banner-notification-dismiss" data-url="{{ route('settings.banner', ['code' => '4yearpromo']) }}">×</button>
+        </span>
     </div>
 @endif
