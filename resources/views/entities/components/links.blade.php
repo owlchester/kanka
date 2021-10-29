f90d19b3829ba14c207a953c6c20e03ea603e524
Jay Payne <jeremy@ilestis.ch>
2021-10-29 18:43:57 -0500

    Entity Links: class on specific entity links

--- resources/views/entities/components/links.blade.php
+++ resources/views/entities/components/links.blade.php
@@ -8,7 +8,7 @@
         <ul class="list-inline export-hidden">
             @foreach ($model->entity->links()->ordered()->get() as $link)
                 <li data-target="{{ $link->id }}">
-                    <a href="{{ route('entities.entity_links.go', ['entity' => $model->entity->id, 'entity_link' => $link->id]) }}" title="{!! $link->name !!}" target="_blank" rel="noreferrer nofollow">
+                    <a href="{{ route('entities.entity_links.go', ['entity' => $model->entity->id, 'entity_link' => $link->id]) }}" title="{!! $link->name !!}" target="_blank" rel="noreferrer nofollow" class="entity-link">
                         <i class="{{ $link->iconName() }} margin-r-5"></i> {!! $link->name !!}
                     </a>
                 </li>
