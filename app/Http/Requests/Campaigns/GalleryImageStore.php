348f404ca425266f83e4478f616f2177f9acbdff
Jay Payne <jeremy@ilestis.ch>
2021-12-14 11:00:18 +0100

    Gallery file upload test

--- app/Http/Requests/Campaigns/GalleryImageStore.php
+++ app/Http/Requests/Campaigns/GalleryImageStore.php
@@ -27,7 +27,7 @@ class GalleryImageStore extends FormRequest
     public function rules()
     {
         $rules = [
-            'file' => 'required|mimes:jpeg,png,jpg,gif,webp|max:' . auth()->user()->maxUploadSize(),
+            'file' => 'required|mimes:jpeg,jpg,gif,png,webp,opentype,ttf,woff,woff2|max:' . auth()->user()->maxUploadSize(),
             'folder_id' => [
                 'nullable',
                 Rule::exists('images', 'id')->where(function ($query) {
@@ -35,6 +35,7 @@ class GalleryImageStore extends FormRequest
                 }),
             ],
         ];
+
         return $rules;
     }
 }
