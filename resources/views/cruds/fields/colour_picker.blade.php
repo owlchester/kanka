a75aded95b1d9a814a645d90d7ed0eedafe39de3
Jay Payne <jeremy@ilestis.ch>
2022-02-22 16:17:29 -0600

    Colour picker editable on firefox

--- resources/views/cruds/fields/colour_picker.blade.php
+++ resources/views/cruds/fields/colour_picker.blade.php
@@ -1,4 +1,15 @@
+@php
+$fieldOptions = [
+    'class' => 'form-control spectrum',
+    'maxlength' => 7
+];
+if (isset($dropdownParent)) {
+    $fieldOptions['data-append-to'] = $dropdownParent;
+}
+
+@endphp
+
 <div class="form-group">
     <label>{{ __('crud.fields.colour') }}</label>
-    {!! Form::text('colour', null, ['class' => 'form-control spectrum', 'maxlength' => 7] ) !!}
+    {!! Form::text('colour', null, $fieldOptions ) !!}
 </div>
