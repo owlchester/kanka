183bd405dbee1424fc45cf843b12c070910a2297
Jay Payne <jeremy@ilestis.ch>
2022-03-24 11:07:02 -0600

    Cancel/Fail emails: include how long was as sub

--- resources/views/emails/subscriptions/failed/html.blade.php
+++ resources/views/emails/subscriptions/failed/html.blade.php
@@ -8,5 +8,9 @@
     <p>
         Failed payment too many times for user <a href="{{ route('admin.users.show', $user) }}">{{ $user->name }}</a> (#{{ $user->id }}) <a href="mailto:{{ $user->email }}">{{ $user->email }}</a>.
     </p>
+    <p>
+        <strong>Subscribed since:</strong><br />
+        {{ $user->subscription('kanka')->created_at->isoFormat('MMMM D, Y') }}
+    </p>
 </body>
 </html>
