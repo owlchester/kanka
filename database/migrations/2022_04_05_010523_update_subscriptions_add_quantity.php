a83a0654364b7c29d1d1759c4c85a10bfa0b97f4
Jay Payne <jeremy@ilestis.ch>
2022-04-04 20:05:53 -0500

    Update cashier to 11

---
+++ database/migrations/2022_04_05_010523_update_subscriptions_add_quantity.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateSubscriptionsAddQuantity extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('subscriptions', function (Blueprint $table) {
+            $table->string('stripe_plan')->nullable()->change();
+            $table->integer('quantity')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
