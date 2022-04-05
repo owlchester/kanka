dcb35a066fa6233b4e59e963cf70e60d1481d54d
Jay Payne <jeremy@ilestis.ch>
2022-04-04 20:09:17 -0500

    Update cashier to 12

---
+++ database/migrations/2022_04_05_010744_update_subscriptions_add_metered_billing.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateSubscriptionsAddMeteredBilling extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('subscription_items', function (Blueprint $table) {
+            $table->integer('quantity')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
