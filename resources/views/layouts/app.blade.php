b956bbcee6a6e13336bbb1e3cb3eda2e752ab79a
Jay Payne <jeremy@ilestis.ch>
2022-04-15 11:25:58 -0500

    Maintenance work notification

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -90,6 +90,7 @@ $seoTitle = isset($seoTitle) ? $seoTitle : (isset($title) ? $title : null);
         @yield('fullpage-form')
 
         <div class="content-wrapper" id="{{ isset($contentId) ? $contentId : "main-content" }}">
+            @include('layouts.banner')
 
             @if(!view()->hasSection('content-header'))
             <section class="content-header">
