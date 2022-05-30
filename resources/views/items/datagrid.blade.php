4a360f81527a8498640d781b78400f9f5ec6d318
Jay Payne <jeremy@ilestis.ch>
2022-05-30 10:14:28 -0500

    Item datagrid: remove loc/creator image

--- resources/views/items/datagrid.blade.php
+++ resources/views/items/datagrid.blade.php
@@ -16,22 +16,10 @@
         'size',
         // Location
         [
-            'type' => 'avatar',
-            'parent' => 'location',
-            'parent_route' => 'locations',
-            'visible' => $campaign->enabled('locations'),
-        ],
-        [
             'type' => 'location',
             'visible' => $campaign->enabled('locations'),
         ],
         // Character
-        [
-            'type' => 'avatar',
-            'parent' => 'character',
-            'parent_route' => 'characters',
-            'visible' => $campaign->enabled('characters'),
-        ],
         [
             'type' => 'character',
             'visible' => $campaign->enabled('characters'),
