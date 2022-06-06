734b45bc86bffc3490535efab7dc8a66d8901139
ilestis <jay.payne.ch@gmail.com>
2022-06-06 11:39:08 -0500

    Map: small tweaks

--- resources/views/maps/markers/details.blade.php
+++ resources/views/maps/markers/details.blade.php
@@ -24,26 +24,27 @@
         @endif
 
 
-        @if($marker->entity->typeId() == config('entities.ids.map'))
+        @if($marker->entity->typeId() === config('entities.ids.map'))
             <div class="marker-map-link text-center">
                 <a href="{{ $marker->entity->url('explore') }}" target="_blank" class="btn btn-primary">
-                    <i class="fa-solid fa-map"></i> {{ __('maps.actions.explore') }}
+                    <i class="fa-solid fa-map" aria-hidden="true"></i> {{ __('maps.actions.explore') }}
                 </a>
+            </div>
+        @endif
+
+        @if($marker->entity->typeID() === config('entities.ids.location') && !$marker->entity->child->maps->isEmpty())
+            <div class="marker-explore-links text-center">
+                @foreach ($marker->entity->child->maps as $map)
+                    <a href="{{ route('maps.explore', $map) }}" class="btn btn-block btn-primary" target="_blank">
+                        <i class="fa-solid fa-map" aria-hidden="true"></i>
+                        {{ __('maps.actions.explore') }} {!! $map->name !!}
+                    </a>
+                @endforeach
             </div>
         @endif
     @endif
 
-    @if($marker->entity && $marker->entity->typeID() === config('entities.ids.location') && !$marker->entity->child->maps->isEmpty())   
-        <div class="marker-actions text-center">    
-            <div class="profile-maps">
-                @foreach ($marker->entity->child->maps as $map)                
-                    <a href="{{ route('maps.explore', $map) }}" class="btn btn-block btn-primary" target="_blank">
-                        <i class="fa-solid fa-map" aria-hidden="true"></i> {!!__('maps.actions.explore'),' ', $map->name !!}
-                    </a>
-                @endforeach
-            </div>
-        </div>
-    @endif
+
 
     <div class="marker-actions text-center">
         @can('update', $marker->map)
