600447c591efc94606fe84aa598d810fb035eea9
ilestis <jay.payne.ch@gmail.com>
2022-06-13 13:28:09 -0500

    Merge features into develop

---
+++ public/js/settings.js
@@ -0,0 +1,19 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!*****************************************!*\
+  !*** ./resources/assets/js/settings.js ***!
+  \*****************************************/
+/** Included on various settings subpages **/
+$(document).ready(function () {
+  registerBoosters();
+});
+
+function registerBoosters() {
+  var focusModal = $('#focus-modal');
+
+  if (focusModal.length === 1) {
+    focusModal.modal('show');
+  }
+}
+/******/ })()
+;
