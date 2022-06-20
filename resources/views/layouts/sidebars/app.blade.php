019a08c3fc1ce8d207a54b06a3f671222b26e355
ilestis <jay.payne.ch@gmail.com>
2022-06-20 08:50:00 -0500

    Quick Create button A/B

--- resources/views/layouts/sidebars/app.blade.php
+++ resources/views/layouts/sidebars/app.blade.php
@@ -95,7 +95,8 @@ $defaultIndex = ($currentCampaign && $currentCampaign->defaultToNested()) || aut
         <section class="sidebar-creator">
             <a href="#" data-url="{{ route('entity-creator.selection') }}" data-toggle="ajax-modal" data-target="#entity-modal" class="quick-creator-button flex items-center justify-center px-2">
                 <i class="flex-none  fa-solid fa-plus" aria-hidden="true"></i>
-                <span class="flex-grow"  data-toggle="tooltip" title="{{ __('entities.creator.tooltip') }}">{{ __('sidebar.new-entity') }}</span>
+                <span class="flex-grow ab-testing-a" data-toggle="tooltip" title="{{ __('entities.creator.tooltip') }}">{{ __('sidebar.new-entity') }}</span>
+                <span class="flex-grow ab-testing-b" data-toggle="tooltip" title="{{ __('entities.creator.tooltip') }}">{{ __('crud.actions.new') }}</span>
                 <span class="flex-none keyboard-shortcut pull-right" data-toggle="tooltip" title="{!! __('crud.keyboard-shortcut', ['code' => '<code>N</code>']) !!}" data-html="true">N</span>
             </a>
         </section>
