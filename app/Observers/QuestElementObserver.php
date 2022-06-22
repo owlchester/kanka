7ff78887e5543b46e92bc10a8c1e7ddea5a69894
ilestis <jay.payne.ch@gmail.com>
2022-06-22 08:07:24 -0500

    Quest Elements: fix error

--- app/Observers/QuestElementObserver.php
+++ app/Observers/QuestElementObserver.php
@@ -4,6 +4,8 @@ namespace App\Observers;
 
 use App\Facades\Mentions;
 use App\Models\QuestElement;
+use App\Models\Visibility;
+use App\Traits\VisibilityIDTrait;
 
 class QuestElementObserver
 {
@@ -21,8 +23,8 @@ class QuestElementObserver
         $questElement->role = $this->purify($questElement->role);
         $questElement->name = $this->purify($questElement->name);
 
-        if (empty($questElement->visibility)) {
-            $questElement->visibility = 'all';
+        if (empty($questElement->visibility_id)) {
+            $questElement->visibility_id = Visibility::VISIBILITY_ALL;
         }
     }
 
