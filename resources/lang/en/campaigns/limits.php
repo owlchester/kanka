43c5f28155f53fa7cce5ce231650a7941a01a26e
ilestis <jay.payne.ch@gmail.com>
2022-07-07 08:45:33 -0500

    Base config for campaign limits

--- resources/lang/en/campaigns/limits.php
+++ resources/lang/en/campaigns/limits.php
@@ -2,6 +2,9 @@
 
 return [
     'title' => 'Limit reached',
-    'quick-links' => 'The campaign has reached the maximum number of menu links. Remove existing quick links before adding another one, or boost the campaign to enable unlimited quick links.',
-    'entities' => 'The campaign has reached the maximum number of entities. Remove existing entities before adding another one, or boost the campaign to enable unlimited entities.',
+    'quick-links' => 'The campaign has reached the maximum number of menu links it can have. Remove existing quick links before adding another one, or boost the campaign to have unlimited quick links.',
+    'entities' => 'The campaign has reached the maximum number of entities it can have. Remove existing entities before adding another one, or boost the campaign to have unlimited entities.',
+    'members' => 'The campaign has reached the maximum number of members it can have. Boost the campaign to enable unlimited members, or remove existing members to allow new ones to join.',
+    'roles' => 'The campaign has reached the maximum number of roles it can have. Remove existing roles before adding another one, or boost the campaign to have unlimited roles.',
+
 ];
