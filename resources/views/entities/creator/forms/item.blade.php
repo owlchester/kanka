40c8b09fdbda8119cd639d7e1d01043d37a0fbf2
ilestis <jay.payne.ch@gmail.com>
2022-07-08 14:00:36 -0500

    Parent item issues

--- resources/views/entities/creator/forms/item.blade.php
+++ resources/views/entities/creator/forms/item.blade.php
@@ -1,10 +1,12 @@
 @include('cruds.fields.type', ['base' => \App\Models\Item::class, 'trans' => 'items'])
 
+@include('cruds.fields.item', ['parent' => true])
+
 <div class="row">
     <div class="col-sm-6">
-@include('cruds.fields.location')
+        @include('cruds.fields.location')
     </div>
     <div class="col-sm-6">
-@include('cruds.fields.character')
+        @include('cruds.fields.character')
     </div>
 </div>
---
+++ resources/views/entities/headers/_item.blade.php
@@ -0,0 +1,14 @@
+<?php /**
+ * @var \App\Models\Item $model
+ * @var \App\Services\CampaignService $campaign
+ */
+?>
+@if ($model->item)
+    <div class="entity-header-sub pull-left">
+        <span title="{{ __('items.fields.item') }}" data-toggle="tooltip">
+        <i class="ra ra-gem-pendant"></i>
+        {!! $model->item->tooltipedLink() !!}
+        </span>
+    </div>
+@endif
+
