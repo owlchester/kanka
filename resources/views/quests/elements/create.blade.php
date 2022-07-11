5fd28bb19925fe3b18f19550f5ec0f3d5d433bfc
ilestis <jay.payne.ch@gmail.com>
2022-07-11 16:17:51 -0500

    Quest Elements: add footer layout

--- resources/views/quests/elements/create.blade.php
+++ resources/views/quests/elements/create.blade.php
@@ -13,26 +13,29 @@
 @section('content')
     <div class="row">
         <div class="col-md-12">
+            {!! Form::open([
+                'route' => ['quests.quest_elements.store', $quest->id],
+                'method'=>'POST',
+                'data-shortcut' => 1,
+            ]) !!}
             <div class="panel panel-default">
                 <div class="panel-body">
                     @include('partials.errors')
 
-                    {!! Form::open([
-                        'route' => ['quests.quest_elements.store', $quest->id],
-                        'method'=>'POST',
-                        'data-shortcut' => 1,
-                    ]) !!}
                     @include('quests.elements._form')
+                </div>
+                <div class="panel-footer">
+                    @include('partials.footer_cancel')
 
-                    <div class="form-group">
-                        <button class="btn btn-success">{{ __('crud.save') }}</button>
-
-                        @include('partials.or_cancel')
+                    <div class="pull-right">
+                        <button class="btn btn-success">
+                            <i class="fa-solid fa-save" aria-hidden="true"></i>
+                            {{ __('crud.create') }}
+                        </button>
                     </div>
-
-                    {!! Form::close() !!}
                 </div>
             </div>
+            {!! Form::close() !!}
         </div>
     </div>
 @endsection
--- resources/views/quests/elements/update.blade.php
+++ resources/views/quests/elements/update.blade.php
@@ -12,25 +12,28 @@
 @section('content')
     <div class="row">
         <div class="col-md-12 col-md-offset">
+            {!! Form::model($model, [
+                'method' => 'PATCH',
+                'route' => ['quests.quest_elements.update', $quest, $model->id],
+                'data-shortcut' => 1,
+            ]) !!}
             <div class="panel panel-default">
                 <div class="panel-body">
                     @include('partials.errors')
-
-                    {!! Form::model($model, [
-                        'method' => 'PATCH',
-                        'route' => ['quests.quest_elements.update', $quest, $model->id],
-                        'data-shortcut' => 1,
-                    ]) !!}
                     @include('quests.elements._form')
+                </div>
+                <div class="panel-footer">
+                    @include('partials.footer_cancel')
 
-                    <div class="form-group">
-                        <button class="btn btn-success">{{ trans('crud.save') }}</button>
-                        @include('partials.or_cancel')
+                    <div class="pull-right">
+                        <button class="btn btn-success">
+                            <i class="fa-solid fa-save" aria-hidden="true"></i>
+                            {{ __('crud.save') }}
+                        </button>
                     </div>
-
-                    {!! Form::close() !!}
                 </div>
             </div>
+            {!! Form::close() !!}
         </div>
     </div>
 @endsection
