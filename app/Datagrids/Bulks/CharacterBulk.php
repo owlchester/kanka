226d699e064f29c84f888262e08c3d465d49b3fd
ilestis <jay.payne.ch@gmail.com>
2022-07-12 15:06:52 -0500

    Character Bulk: move org field

--- app/Datagrids/Bulks/CharacterBulk.php
+++ app/Datagrids/Bulks/CharacterBulk.php
@@ -14,9 +14,9 @@ class CharacterBulk extends Bulk
         'sex',
         'dead_choice',
         'age',
+        'organisations',
         'tags',
         'private_choice',
-        'organisations',
     ];
 
     protected $mappings = [
