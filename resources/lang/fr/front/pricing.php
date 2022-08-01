2857690f75e4a93fa3400922fe9250e9c34ee80e
ilestis <jay.payne.ch@gmail.com>
2022-08-01 08:20:56 -0500

    Translations

---
+++ resources/lang/fr/front/pricing.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'cards' => [
+        'description'   => 'Tu peux t\'abonner avec la majorité des cartes de crédit et débit comme Visa et Mastercard, ainsi que Giropay, Sofort, et PayPal pour les abonnements annuels.',
+    ],
+];
