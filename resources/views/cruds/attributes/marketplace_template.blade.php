80403b88b289df61caf188cac437c030d811de35
ilestis <jay.payne.ch@gmail.com>
2022-09-27 12:59:26 -0500

    Character sheets: allow valid import rules

--- resources/views/cruds/attributes/marketplace_template.blade.php
+++ resources/views/cruds/attributes/marketplace_template.blade.php
@@ -23,12 +23,13 @@
 @section('styles')
     @parent
     <style>
+        {!! $plugin->version->css !!}
+
         /** Entity attributes **/
         :root {
-@foreach ((isset($entity) ? $entity->allAttributes : $model->entity->allAttributes) as $attribute) @if ($attribute->isText()) @continue @endif
-            --attribute-{{ \Illuminate\Support\Str::slug($attribute->name) }}: {{ trim(preg_replace('/\s+/', ' ', $attribute->value)) }};
-@endforeach
-        }
-        {!! $plugin->version->css !!}
+            @foreach ((isset($entity) ? $entity->allAttributes : $model->entity->allAttributes) as $attribute) @if ($attribute->isText()) @continue @endif
+--attribute-{{ \Illuminate\Support\Str::slug($attribute->name) }}: {{ trim(preg_replace('/\s+/', ' ', $attribute->value)) }};
+        @endforeach
+}
     </style>
 @endsection
