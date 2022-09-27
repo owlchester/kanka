27776bad19dbde7c5195e131e62d058aa2c70ddf
ilestis <jay.payne.ch@gmail.com>
2022-09-27 13:00:17 -0500

    Character sheets: allow valid import rules

--- resources/views/cruds/attributes/marketplace_template.blade.php
+++ resources/views/cruds/attributes/marketplace_template.blade.php
@@ -27,7 +27,7 @@
 
         /** Entity attributes **/
         :root {
-            @foreach ((isset($entity) ? $entity->allAttributes : $model->entity->allAttributes) as $attribute) @if ($attribute->isText()) @continue @endif
+        @foreach ((isset($entity) ? $entity->allAttributes : $model->entity->allAttributes) as $attribute) @if ($attribute->isText()) @continue @endif
 --attribute-{{ \Illuminate\Support\Str::slug($attribute->name) }}: {{ trim(preg_replace('/\s+/', ' ', $attribute->value)) }};
         @endforeach
 }
