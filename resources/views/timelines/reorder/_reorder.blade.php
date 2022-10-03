1fbb5f70dddc28164f4440f32ec8a1be1ac1e66b
ilestis <jay.payne.ch@gmail.com>
2022-10-03 16:37:54 -0500

    Timeline: unified reorder interface

--- resources/views/timelines/reorder/_reorder.blade.php
+++ resources/views/timelines/reorder/_reorder.blade.php
@@ -3,6 +3,7 @@
         'route' => ['timelines.reorder-save', $timeline],
         'method' => 'POST',
     ]) !!}
+
 <div class="box box-solid box-entity-story-reorder">
     <div class="box-header">
         <h3 class="box-title">
