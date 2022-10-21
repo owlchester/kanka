f2e7b1c121b3dfcd7ea57092ae63218f1e30b862
ilestis <jay.payne.ch@gmail.com>
2022-10-21 15:29:35 -0500

    Email tweaks

--- resources/views/emails/export.blade.php
+++
@@ -1,3 +0,0 @@
-<p>{{ trans('campaigns.export.email.subject', ['name' => $user->name, 'campaign' => $invite->campaign->name]) }}</p>
-
-<p>{!! trans('campaigns.export.email.link', ['link' => route('campaigns.join', $invite->token), 'name' => e($user->name)]) !!}</p>
