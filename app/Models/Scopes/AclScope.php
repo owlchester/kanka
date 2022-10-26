5579189640e0ebe14eb681deaea43efcb0154e90
ilestis <jay.payne.ch@gmail.com>
2022-10-26 16:47:08 -0500

    Debugging on prod

--- app/Models/Scopes/AclScope.php
+++ app/Models/Scopes/AclScope.php
@@ -141,6 +141,14 @@ class AclScope implements Scope
             ;
         }
 
+        if (request()->has('_debug_perm')) {
+            echo "<h3>Accepted:</h3>";
+            echo implode(",<br />", Permissions::allowedModels());
+
+            echo "<h3>Denied:</h3>";
+            echo implode(",<br />", Permissions::deniedModels());
+        }
+
         return $query
             ->whereIn($table . '.' . $primaryKey, Permissions::allowedModels())
             ->whereNotIn($table . '.' . $primaryKey, Permissions::deniedModels())
