746bde04e63220fcd9d3350aa06e84b6f5524fea
ilestis <jay.payne.ch@gmail.com>
2022-11-01 12:18:28 -0600

    Landing: smaller hero

--- lang/en/characters.php
+++ lang/en/characters.php
@@ -1,4 +1,4 @@
-<?php
+$<?php
 
 return [
     'actions'       => [
@@ -81,6 +81,7 @@ return [
         'age'               => 'Age',
         'appearance_entry'  => 'Description',
         'appearance_name'   => 'Hair, Eyes, Skin, Height',
+        'name'              => 'Name of the character',
         'personality_entry' => 'Details',
         'personality_name'  => 'Goals, Mannerisms, Fears, Bonds',
         'physical'          => 'Physical',
