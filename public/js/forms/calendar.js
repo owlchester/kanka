0665dd5ef6883023725ef9792f63a92e99994885
ilestis <jay.payne.ch@gmail.com>
2022-11-28 08:04:32 -0600

    Assets 1.36

--- public/js/forms/calendar.js
+++ public/js/forms/calendar.js
@@ -1,14 +1,2 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!***********************************************!*\
-  !*** ./resources/assets/js/forms/calendar.js ***!
-  \***********************************************/
-$(document).ready(function () {
-  /** Leap year toggler **/
-  $('input[name="has_leap_year"]').on('click', function () {
-    $('#calendar-leap-year').toggle();
-  });
-});
-/******/ })()
-;
+$(document).ready((function(){$('input[name="has_leap_year"]').on("click",(function(){$("#calendar-leap-year").toggle()}))}));
 //# sourceMappingURL=calendar.js.map
