857f664694eaf5752e2a6a541641f7a851f17b2c
ilestis <jay.payne.ch@gmail.com>
2022-11-28 17:13:20 -0600

    Fixes to the new translatable character sheets

--- lang/fr/attributes/templates.php
+++ lang/fr/attributes/templates.php
@@ -4,7 +4,7 @@ return [
     'errors'    => [
         'marketplace'   => [
             'hint'      => 'Erreur',
-            'rendering' => 'Une erreur est survenue lors de l\'affichage du plugin. Prière de contacter le ou la créateur/euse du plugin.',
+            'rendering' => 'Une erreur est survenue lors de l\'affichage du plugin. Prière de contacter le ou la créateur/rice du plugin.',
         ],
     ],
     'helpers'   => [],
