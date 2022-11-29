f70448c08779a677c44c01482065ba81bee3e254
ilestis <jay.payne.ch@gmail.com>
2022-11-29 15:15:12 -0600

    Summernote tweaks

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,2 +1,30 @@
-(()=>{var e;function n(n){$(n).change((function(){var n={};n[this.name]=this.checked?1:0,$.post(e,n)}))}$(document).ready((function(){1===$("#newsletter-api").length&&(e=$("#newsletter-api").val(),n($('input[name="mail_release"]')),n($('input[name="mail_newsletter"]')),n($('input[name="mail_vote"]')))}))})();
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!****************************************!*\
+  !*** ./resources/assets/js/profile.js ***!
+  \****************************************/
+var api;
+$(document).ready(function () {
+  if ($('#newsletter-api').length === 1) {
+    init();
+  }
+});
+
+function init() {
+  api = $('#newsletter-api').val();
+  handle($('input[name="mail_release"]'));
+  handle($('input[name="mail_newsletter"]'));
+  handle($('input[name="mail_vote"]'));
+}
+
+function handle(element) {
+  $(element).change(function () {
+    var name = this.name;
+    var data = {};
+    data[name] = this.checked ? 1 : 0;
+    $.post(api, data);
+  });
+}
+/******/ })()
+;
 //# sourceMappingURL=profile.js.map
