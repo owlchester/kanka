0f609e091fc9cb768961ff997d981ec7a52ac65e
spitfire305 <itza305@gmail.com>
2022-12-05 16:37:42 -0600

    Minor code changes.

--- resources/views/entities/pages/inventory/_form.blade.php
+++ resources/views/entities/pages/inventory/_form.blade.php
@@ -34,7 +34,7 @@
     <div class="col-md-6">
         <div class="form-group required">
             <label>{{ __('entities/inventories.fields.amount') }}</label>
-            {!! Form::number('amount', (empty($inventory) ? 1 : null), ['class' => 'form-control', 'maxlength' => 191]) !!}
+            {!! Form::number('amount', (empty($inventory) ? 1 : null), ['class' => 'form-control', 'max' => 1000000000, 'min' => 0]) !!}
         </div>
     </div>
     <div class="col-md-6">
