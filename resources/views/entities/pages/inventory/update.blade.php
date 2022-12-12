a8ce55a705a53b4863c3cf55ebb9a8f58f9055ca
ilestis <jay.payne.ch@gmail.com>
2022-12-12 12:05:30 -0600

    Inventory: wrong ajax form

--- resources/views/entities/pages/inventory/update.blade.php
+++ resources/views/entities/pages/inventory/update.blade.php
@@ -11,23 +11,10 @@
 @section('content')
     {!! Form::model($inventory, ['route' => ['entities.inventories.update', $entity->id, $inventory], 'method' => 'PATCH', 'data-shortcut' => 1]) !!}
 
-    @if (request()->ajax())
-        @include('modals.modal', [
-            'title' => __('entities/inventories.update.title', ['name' => $entity->name]),
-            'content' => 'entities.pages.inventory._form',
-        ])
-    @else
-        <div class="box box-solid">
-            <div class="box-body">
-                @include('partials.errors')
-                @include('entities.pages.inventory._form')
-            </div>
-            <div class="box-footer">
-                <button class="btn btn-success">{{ __('crud.save') }}</button>
-                @includeWhen(!request()->ajax(), 'partials.or_cancel')
-            </div>
-        </div>
-    @endif
+    @include('partials.forms.form', [
+        'title' => __('entities/inventories.update.title', ['name' => $entity->name]),
+        'content' => 'entities.pages.inventory._form',
+    ])
 
     {!! Form::hidden('entity_id', $entity->id) !!}
     {!! Form::close() !!}
