d89cd9a591320ff8a4d3c568fcd791f82fb34c80
ilestis <jay.payne.ch@gmail.com>
2023-01-12 14:43:22 -0600

    Cleaner code for withChildren filter

--- resources/views/cruds/datagrids/filters/_array.blade.php
+++ resources/views/cruds/datagrids/filters/_array.blade.php
@@ -12,20 +12,21 @@
     </div>
     <div class="col-xs-4">
         @php
-        $options = [
-            '' => __('crud.filters.options.include'),
-            'exclude' => __('crud.filters.options.exclude'),
-            'none' => __('crud.filters.options.none'),
-        ];
-        if (isset($field['withChildren']) && $field['withChildren'] === true ) {
-            array_splice($options, 1, 0, ['children' => __('crud.filters.options.children')]);
-        }
+            $options = [
+                '' => __('crud.filters.options.include'),
+                'children' => __('crud.filters.options.children'),
+                'exclude' => __('crud.filters.options.exclude'),
+                'none' => __('crud.filters.options.none'),
+            ];
+            if (!isset($field['withChildren']) || $field['withChildren'] !== true) {
+                unset($options['children']);
+            }
         @endphp
-            {!! Form::select(
-                $field['field'] . '_option',
-                $options,
-                $filterService->single($field['field'] . '_option'), [
-                    'class' => 'form-control entity-list-option',
-            ]) !!}
+        {!! Form::select(
+            $field['field'] . '_option',
+            $options,
+            $filterService->single($field['field'] . '_option'), [
+                'class' => 'form-control entity-list-option',
+        ]) !!}
     </div>
 </div>
