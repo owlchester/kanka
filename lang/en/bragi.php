36fe765df398f2e10309509ec2bcce279c7f3f31
ilestis <jay.payne.ch@gmail.com>
2023-01-18 10:19:24 -0600

    FR translations 1.39

--- lang/en/bragi.php
+++ lang/en/bragi.php
@@ -1,20 +1,20 @@
 <?php
 
 return [
-    'intro' => 'Hi! I\'m :name, an AI here to help you generate backstories for your characters in Kanka. You can learn more about me :here.',
-    'here' => 'here',
-    'errors' => [
-        'invalid-sub' => 'To access this feature, you need to have an Wyvern or Elemental subscription.',
-        'out-of-tokens' => 'You are out of tokens! You\'ll automatically get more on :date',
+    'actions'       => [
+        'generate'  => 'Generate',
+        'insert'    => 'Use',
     ],
-    'actions' => [
-        'generate' => 'Generate',
-        'insert' => 'Use',
+    'errors'        => [
+        'invalid-sub'   => 'To access this feature, you need to have an Wyvern or Elemental subscription.',
+        'out-of-tokens' => 'You are out of tokens! You\'ll automatically get more on :date.',
     ],
-    'placeholders' => [
-        'prompt' => 'Provide a prompt that will be transformed into a backstory.',
+    'here'          => 'here',
+    'intro'         => 'Hi! I\'m :name, an AI here to help you generate backstories for your characters in Kanka. You can learn more about me :here.',
+    'kankappy'      => 'They are secretly a disciple of Kankappy.',
+    'loading'       => 'Please hang tight, I\'m thinking hard and it can take up to a minute!',
+    'placeholders'  => [
+        'prompt'    => 'Provide a prompt that will be transformed into a backstory.',
     ],
-    'token-limit' => 'You currently have :amount tokens. Each time I generate a backstory, that uses a token, so use them wisely!',
-    'loading' => 'Please hang tight, I\'m thinking hard and it can take up to a minute!',
-    'kankappy' => 'They are secretly a disciple of Kankappy.',
+    'token-limit'   => 'You currently have :amount tokens. Each time I generate a backstory, that uses a token, so use them wisely!',
 ];
