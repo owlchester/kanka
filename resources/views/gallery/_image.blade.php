7b993c440261e21b80cdf496b48a7d1f193c7f06
ilestis <jay.payne.ch@gmail.com>
2023-01-24 14:41:06 -0600

    Gallery: replace old plugin

--- resources/views/gallery/_image.blade.php
+++ resources/views/gallery/_image.blade.php
@@ -4,11 +4,11 @@
 */
 ?>
 
-<li tabindex="0" class="pull-left h-36 w-32 p-2 m-2 rounded drop-shadow cursor flex flex-col items-center justify-center text-center" role="checkbox" aria-label="{{ $image->name }}" aria-checked="false" data-id="{{ $image->id }}"
+<li tabindex="0" class="pull-left h-36 w-32 p-2 m-2 rounded drop-shadow cursor flex flex-col items-center justify-center text-center select-none" role="checkbox" aria-label="{{ $image->name }}" aria-checked="false" data-id="{{ $image->id }}"
     data-url="{{ route('images.edit', $image) }}" @if ($image->is_folder) data-folder="{{ route('campaign.gallery.index', ['folder_id' => $image->id]) }}" @endif title="{{ $image->name }}">
     @if ($image->is_folder)
         @if ($image->visibility_id == \App\Models\Visibility::VISIBILITY_ALL)
-            <i class="fa-solid fa-folder text-3xl mb-3"></i>
+            <i class="fa-solid fa-folder text-3xl mb-3" aria-hidden="true"></i>
         @else
             {!! $image->visibilityIcon('fa-2x') !!}
         @endif
@@ -17,7 +17,7 @@
         </div>
     @else
     <div class="w-full image-preview ">
-        <div class="gallery-thumbnail inline-block rounded cover-background"
+        <div class="w-25 h-25 inline-block rounded cover-background"
             style="background-image: url('{{ Img::crop(100, 100)->url($image->path) }}')">
         </div>
         <div class="w-full thumbnail-text text-left inline-block truncate">
