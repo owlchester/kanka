ba58fee971886ef5b9496936a3be61cd3d738819
ilestis <jay.payne.ch@gmail.com>
2023-01-30 07:03:01 -0600

    Prod assets

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,28 +1,2 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!****************************************!*\
-  !*** ./resources/assets/js/profile.js ***!
-  \****************************************/
-var api;
-$(document).ready(function () {
-  if ($('#newsletter-api').length === 1) {
-    init();
-  }
-});
-function init() {
-  api = $('#newsletter-api').val();
-  handle($('input[name="mail_release"]'));
-}
-function handle(element) {
-  $(element).change(function () {
-    var name = this.name;
-    var data = {};
-    data[name] = this.checked ? 1 : 0;
-    $.post(api, data).done(function (res) {
-      window.showToast(res.message);
-    });
-  });
-}
-/******/ })()
-;
+(()=>{var e;$(document).ready((function(){var n;1===$("#newsletter-api").length&&(e=$("#newsletter-api").val(),n=$('input[name="mail_release"]'),$(n).change((function(){var n={};n[this.name]=this.checked?1:0,$.post(e,n).done((function(e){window.showToast(e.message)}))})))}))})();
 //# sourceMappingURL=profile.js.map
