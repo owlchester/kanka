36ef4a8fc962037bb910846e18574396744844fb
spitfire305 <itza305@gmail.com>
2023-01-31 15:51:07 -0600

    Minor code improvements

--- app/Services/MentionsService.php
+++ app/Services/MentionsService.php
@@ -701,17 +701,14 @@ class MentionsService
         $this->text = preg_replace_callback('`\{attribute:(.*?)\}`i', function ($matches) {
             $id = (int) $matches[1];
             $attribute = $this->attribute($id);
-            $fallback = '';
-            if (str_contains($matches[1], '|')) {
-                $fallback = substr($matches[1], strpos($matches[1], "|") + 1);
-            }
+            $fallback = Str::after($matches[1], '|');
 
             // No entity found, the user might not be allowed to see it, if theres a fallback, apply it
             if (empty($attribute)) {
                 if (!$fallback) {
                     $replace = '<i class="unknown-mention unknown-attribute">' . __('crud.history.unknown') . '</i>';
                 } else {
-                    $replace = '<span class="attribute attribute-mention">' . $fallback . '</span>';
+                    $replace = '<i class="unknown-mention unknown-attribute">' . $fallback . '</i>';
                 }
             } else {
                 $replace = '<span class="attribute attribute-mention" title="' . e($attribute->name)
