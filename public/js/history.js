6ac538fca15190269c1d820f31b20982d92c8206
ilestis <jay.payne.ch@gmail.com>
2023-03-28 07:35:08 -0600

    1.40 assets

--- public/js/history.js
+++ public/js/history.js
@@ -1,21 +1,2 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!****************************************!*\
-  !*** ./resources/assets/js/history.js ***!
-  \****************************************/
-$(document).ready(function () {
-  initHistoryFilters();
-});
-function initHistoryFilters() {
-  var form = $('form.history-filters');
-  var filters = $('.history-filters select');
-  filters.on('change', function () {
-    $('.filters-loading').show();
-    console.log('changed');
-    form.submit();
-    filters.prop('disabled', true);
-  });
-}
-/******/ })()
-;
+$(document).ready((function(){var o,e;o=$("form.history-filters"),(e=$(".history-filters select")).on("change",(function(){$(".filters-loading").show(),console.log("changed"),o.submit(),e.prop("disabled",!0)}))}));
 //# sourceMappingURL=history.js.map
