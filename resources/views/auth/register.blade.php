1ebdf8ffc3e90c42b7cfedb9a0d745bdf24fd3c1
ilestis <jay.payne.ch@gmail.com>
2023-04-03 18:05:31 -0600

    Auth: remove bootstrap

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,105 +3,103 @@
 ])
 
 @section('content')
-    <h1>{{ __('auth.register.title') }}</h1>
+    <h1 class="text-2xl leading-tight mb-3">{{ __('auth.register.title') }}
+    </h1>
 
     @include('partials.errors')
 
-    <form method="POST" action="{{ route('register') }}" class="submit-lock">
+    <form method="POST" action="{{ route('register') }}" class="submit-lock w-full">
         {{ csrf_field() }}
 
-        <div class="form-group has-feedback{{ $errors->has('name') ? ' has-error' : '' }}">
-            <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" placeholder="{{ __('auth.register.fields.name') }}" required autofocus>
-            <span class="glyphicon glyphicon-user form-control-feedback"></span>
+        <div class="mb-3 has-feedback{{ $errors->has('name') ? ' has-error' : '' }}">
+            <div class="flex items-stretch w-full">
+                <input id="name" type="text" class="rounded border p-2 w-full" name="name" value="{{ old('name') }}" placeholder="{{ __('auth.register.fields.name') }}" required autofocus>
+            </div>
 
             @if ($errors->has('name'))
-                <span class="help-block">
-                    <strong>{{ $errors->first('name') }}</strong>
-                </span>
+                <span class="text-red-500">{{ $errors->first('name') }}</span>
             @endif
         </div>
 
-        <div class="form-group has-feedback{{ $errors->has('email') ? ' has-error' : '' }}">
-            <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" placeholder="{{ __('auth.register.fields.email') }}" required>
-            <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+        <div class="mb-3 has-feedback{{ $errors->has('email') ? ' has-error' : '' }}">
+            <input id="email" type="email" class="rounded border p-2 w-full" name="email" value="{{ old('email') }}" placeholder="{{ __('auth.register.fields.email') }}" required>
 
             @if ($errors->has('email'))
-                <span class="help-block">
-                    <strong>{{ $errors->first('email') }}</strong>
-                </span>
+                <span class="text-red-500">{{ $errors->first('email') }}</span>
             @endif
         </div>
 
-        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-            <div class="input-group">
-                <input id="password" type="password" class="form-control" name="password" required placeholder="{{ __('auth.register.fields.password') }}">
-                <a href="#" class="toggle-password input-group-addon" tabindex="-1" title="{{ __('auth.helpers.password') }}">
+        <div class="mb-3{{ $errors->has('password') ? ' has-error' : '' }}">
+            <div class="flex items-stretch w-full">
+                <input id="password" type="password" class="rounded border p-2 w-full" name="password" required placeholder="{{ __('auth.register.fields.password') }}">
+                <a href="#" class="toggle-password input-group-addon p-2" tabindex="-1" title="{{ __('auth.helpers.password') }}">
                     <i class="toggle-password-icon fa-solid fa-eye"></i>
                 </a>
             </div>
 
             @if ($errors->has('password'))
-                <span class="help-block">
-                    <strong>{{ $errors->first('password') }}</strong>
-                </span>
+                <span class="text-red-500">{{ $errors->first('password') }}</span>
             @endif
         </div>
-        <div class="form-group">
-            <div class="checkbox">
+        <div class="mb-3">
+            <div class="text-gray-500 text-sm">
                 <label for="newsletter" class="">
                     <input id="newsletter" type="checkbox" name="newsletter" value="1" />
                     {!! __('front/newsletter.groups.all') !!}
                 </label>
             </div>
         </div>
-        <div class="">
-            <div class="pull-right pl-2">
-                <button type="submit" class="btn btn-primary btn-save">
-                    {{ __('auth.register.submit') }}
-                </button>
-                <div class="btn btn-primary btn-wait disabled" style="display: none;">
+        <div class="flex gap-2">
+            <div class="grow text-xs text-gray-500">
+                {!! __('auth.register.tos', [
+    'terms' => link_to_route('front.terms', __('front.terms.title'), null, ['class' => 'text-blue-500 hover:text-blue-800']),
+    'privacy' => link_to_route('front.privacy', __('front.menu.privacy'), null, ['class' => 'text-blue-500 hover:text-blue-800']),
+    ]) !!}
+            </div>
+            <div class="flex-0">
+                <div class="rounded border border-gray btn-wait disabled" style="display: none;">
                     <i class="fa-solid fa-spinner fa-spin"></i>
                 </div>
+
+                <button type="submit" class="rounded border border-blue-500 text-blue-500 uppercase px-6 py-2 transition-all bg-white hover:shadow-xs hover:bg-blue-500 hover:text-white btn-save">
+                    {{ __('auth.register.submit') }}
+                </button>
             </div>
-            <p class="small">
-                {!! __('auth.register.tos', [
-    'terms' => link_to_route('front.terms', __('front.terms.title')),
-    'privacy' => link_to_route('front.privacy', __('front.menu.privacy')),
-    ]) !!}
-            </p>
         </div>
+
     </form>
 
-    <div class="row">
-        <div class="col-md-12">
-            <div class="social-auth-links text-center">
-                <p>- {{ __('auth.login.or') }} -</p>
+    <div class="social-auth-links text-center mb-3">
+        <p class="mb-2 text-gray-500">- {{ __('auth.login.or') }} -</p>
 
-                @if(config('services.facebook.client_id'))
-                <a href="{{ route('auth.provider', ['provider' => 'facebook']) }}" class="btn btn-app btn-facebook" title="{{ __('auth.register.register_with_facebook') }}">
-                    <i class="fab fa-facebook-f"></i> Facebook
+        <div class="flex flex-col gap-2">
+            @if(config('services.facebook.client_id'))
+                <a href="{{ route('auth.provider', ['provider' => 'facebook']) }}" class="rounded border border-blue-500 text-blue-500 hover:text-white hover:bg-blue-500 px-6 py-2 uppercase transition-all duration-150" title="{{ __('auth.register.register_with_facebook') }}">
+                    <i class="fab fa-facebook-f mb-1" aria-hidden="true"></i>
+                    Facebook
                 </a>
-                @endif
+            @endif
 
-                @if(config('services.google.client_id'))
-                <a href="{{ route('auth.provider', ['provider' => 'google']) }}" class="btn btn-app btn-google" title="{{ __('auth.register.register_with_google') }}">
-                    <i class="fab fa-google"></i> Google
+            @if(config('services.google.client_id'))
+                <a href="{{ route('auth.provider', ['provider' => 'google']) }}" class="rounded border border-red-400 text-red-400 hover:text-white hover:bg-red-400 px-6 py-2 uppercase transition-all duration-150" title="{{ __('auth.register.register_with_google') }}">
+                    <i class="fab fa-google" aria-hidden="true"></i>
+                    Google
                 </a>
-                @endif
+            @endif
 
-                @if(config('services.twitter.client_id'))
-                <a href="{{ route('auth.provider', ['provider' => 'twitter']) }}" class="btn btn-app btn-twitter" title="{{ __('auth.register.register_with_twitter') }}">
-                    <i class="fab fa-twitter"></i> Twitter
+            @if(config('services.twitter.client_id'))
+                <a href="{{ route('auth.provider', ['provider' => 'twitter']) }}" class="rounded border border-blue-300 text-blue-300 hover:text-white hover:bg-blue-300 px-6 py-2 uppercase transition-all duration-150" title="{{ __('auth.register.register_with_twitter') }}">
+                    <i class="fab fa-twitter" aria-hidden="true"></i>
+                    Twitter
                 </a>
-                @endif
-            </div>
-
-            <p>
-                {!! __('auth.register.already', ['login' => link_to('login', __('auth.register.log-in'))]) !!}
-            </p>
-
+            @endif
         </div>
     </div>
+
+    <p class="text-sm text-gray-500">
+        {!! __('auth.register.already', ['login' => link_to('login', __('auth.register.log-in'), ['class' => 'text-blue-500 hover:text-blue-800'])]) !!}
+    </p>
+
 
 </div>
 @endsection
--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -12,22 +12,36 @@
     <meta property="og:title" content="{{ $title ?? '' }} - {{ config('app.name') }}" />
     <meta property="og:site_name" content="{{ config('app.site_name') }}" />
 
-    <link rel="icon" type="image/png" href="/favicon.ico">
+    <link rel="shortcut icon" href="/images/favicon/favicon.ico" type="image/x-icon" />
+    <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon/favicon-16x16.png">
+    <link rel="apple-touch-icon" href="/images/favicon/apple-touch-icon.png" />
+    <link rel="apple-touch-icon" sizes="57x57" href="/images/favicon/apple-touch-icon-57x57.png" />
+    <link rel="apple-touch-icon" sizes="72x72" href="/images/favicon/apple-touch-icon-72x72.png" />
+    <link rel="apple-touch-icon" sizes="76x76" href="/images/favicon/apple-touch-icon-76x76.png" />
+    <link rel="apple-touch-icon" sizes="114x114" href="/images/favicon/apple-touch-icon-114x114.png" />
+    <link rel="apple-touch-icon" sizes="120x120" href="/images/favicon/apple-touch-icon-120x120.png" />
+    <link rel="apple-touch-icon" sizes="144x144" href="/images/favicon/apple-touch-icon-144x144.png" />
+    <link rel="apple-touch-icon" sizes="152x152" href="/images/favicon/apple-touch-icon-152x152.png" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/images/favicon/apple-touch-icon-180x180.png" />
 
+    @if (request()->has('bs'))
     <link href="/css/bootstrap.css?v={{ config('app.version') }}" rel="stylesheet">
+    @endif
 
     @vite('resources/sass/auth.scss')
     @if (!config('fontawesome.kit'))<link href="/vendor/fontawesome/6.0.0/css/all.min.css" rel="stylesheet">@endif
 </head>
-<body  class="hold-transition register-page">
+<body class="hold-transition register-page">
 @include('layouts._tracking-fallback')
-    <div class="login-box">
+    <div class="login-box mx-auto">
 
         <!-- Content Header (Page header) -->
-        <div class="login-box-body">
-            <div class="text-center login-logo">
+        <div class="bg-white md:rounded-xl p-5 md:mt-28">
+            <div class="text-center w-full login-logo mb-5">
                 <a href="{{ route('home') }}" tabindex="-1">
-                    <img src="https://kanka-app-assets.s3.amazonaws.com/images/logos/logo-small.png" alt="{{ config('app.name') }}" title="{{ config('app.name') }}"/></a>
+                    <img src="https://kanka-app-assets.s3.amazonaws.com/images/logos/logo-small.png" alt="{{ config('app.name') }}" title="{{ config('app.name') }}" class="w-20 inline" />
+                </a>
             </div>
             @yield('content')
         </div>
