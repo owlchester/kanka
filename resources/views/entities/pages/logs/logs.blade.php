b5e52942ba76fb793a0c0a4921d0ddab5e798369
ilestis <jay.payne.ch@gmail.com>
2023-04-14 12:03:11 -0600

    Entity logs: compatibility with tailwind

--- resources/views/entities/pages/logs/logs.blade.php
+++ resources/views/entities/pages/logs/logs.blade.php
@@ -72,7 +72,7 @@
                             </tr>
                         @endif
                         @if ($campaign->superboosted() && !empty($log->changes))
-                        <tr id="log-{{ $log->id }}" class="collapse">
+                        <tr id="log-{{ $log->id }}" class="collapse !visible">
                             <td colspan="4">
                                 <dl class="dl-horizontal">
                                     @foreach ($log->changes as $attribute => $value)
