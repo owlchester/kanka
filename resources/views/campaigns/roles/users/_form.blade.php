4a3e842fd0cdbb3663ac6ed58153767f5ae9b291
ilestis <jay.payne.ch@gmail.com>
2023-04-18 08:45:15 -0600

    Admin role: better ux

--- resources/views/campaigns/roles/users/_form.blade.php
+++ resources/views/campaigns/roles/users/_form.blade.php
@@ -1,10 +1,13 @@
 {{ csrf_field() }}
-<div class="row">
-    <div class="col-md-12">
-        <div class="form-group required">
-            <label>{{ trans('campaigns.members.fields.name') }}</label>
-            {!! Form::select('user_id', $campaign->membersList($role->users->pluck('user_id')->toArray()), null, ['class' => 'form-control']) !!}
-        </div>
-    </div>
+<div class="form-group required">
+    <label>{{ __('campaigns.members.fields.name') }}</label>
+    {!! Form::select('user_id', $campaign->membersList($role->users->pluck('user_id')->toArray()), null, ['class' => 'form-control']) !!}
 </div>
+
+@if($role->isAdmin())
+    <div class="alert alert-warning">
+        <i class="fa-solid fa-exclamation-triangle" aria-hidden="true"></i>
+        {!! __('campaigns/roles.warnings.adding-to-admin', ['name' => $role->name, 'amount' => '<strong>15</strong>']) !!}
+    </div>
+@endif
 
