cfe0c2d8fbaafd4945fb496ef7fd8a5626978f17
spitfire305 <itza305@gmail.com>
2023-04-26 13:56:15 -0600

    Removed useless code

--- resources/views/races/members/_form.blade.php
+++ resources/views/races/members/_form.blade.php
@@ -1,4 +1,4 @@
 <div class="form-group">
     <label>{{ __('races.fields.members') }}</label>
-    <select multiple="multiple" name="members[]" id="members" class="form-control form-members" style="width: 100%" data-url="{{ route('characters.find', ['with_race' => '1']) }}"></select>
+    <select multiple="multiple" name="members[]" id="members" class="form-control form-members" style="width: 100%" data-url="{{ route('characters.find') }}"></select>
 </div>
