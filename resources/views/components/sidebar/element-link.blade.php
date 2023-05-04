71ad5cdb7214ca280ceeaca10b41e51a2988bc7b
ilestis <jay.payne.ch@gmail.com>
2023-05-04 15:14:28 -0600

    Sidebar: quick links same component

--- resources/views/components/sidebar/element-link.blade.php
+++ resources/views/components/sidebar/element-link.blade.php
@@ -1,4 +1,4 @@
-<a href="{{ $url }}" class="px-2 py-1.5 flex items-center gap-2 my-0.5 rounded">
+<a href="{{ $url }}" class="px-2 py-2 flex items-center gap-2 my-0.5 rounded">
     <i class="w-6 flex-shrink-0 text-base  {{ $icon }}"></i>
     <span class="truncate">{!! $text !!}</span>
 </a>
--- resources/views/layouts/sidebars/_quick-link.blade.php
+++ resources/views/layouts/sidebars/_quick-link.blade.php
@@ -1,30 +1,34 @@
 <?php /** @var \App\Models\MenuLink $menuLink */ ?>
 @if ($menuLink->dashboard && $currentCampaign->boosted() && $menuLink->isValidDashboard())
     <li class="p-0 m-0 subsection sidebar-quick-link sidebar-quick-link-{{ $menuLink->position }} {{ $sidebar->activeMenuLink($menuLink) }} {{ $menuLink->customClass($currentCampaign) }}">
-        <a href="{{ $menuLink->getRoute() }}" class="px-2 py-1.5 flex items-center gap-2 my-0.5 rounded">
-            <i class="w-6 flex-shrink-0 text-base {{ $menuLink->icon() }}" aria-hidden="true"></i>
-            {{ $menuLink->name }}
-        </a>
+        <x-sidebar.element
+            :url="$menuLink->getRoute()"
+            :icon="$menuLink->icon()"
+            :text="$menuLink->name"
+        ></x-sidebar.element>
     </li>
 @elseif ($menuLink->target)
     <li class="p-0 m-0 subsection sidebar-quick-link sidebar-quick-link-{{ $menuLink->position }} {{ $sidebar->activeMenuLink($menuLink) }} {{ $menuLink->customClass($currentCampaign) }}">
-        <a href="{{ $menuLink->getRoute() }}" class="px-2 py-1.5 flex items-center gap-2 my-0.5 rounded">
-            <i class="w-6 flex-shrink-0 text-base {{ $menuLink->icon() }}" aria-hidden="true"></i>
-            {{ $menuLink->name }}
-        </a>
+        <x-sidebar.element
+            :url="$menuLink->getRoute()"
+            :icon="$menuLink->icon()"
+            :text="$menuLink->name"
+        ></x-sidebar.element>
     </li>
 @elseif ($menuLink->type)
     <li class="p-0 m-0 subsection sidebar-quick-link sidebar-quick-link-{{ $menuLink->position }} {{ $sidebar->activeMenuLink($menuLink) }} {{ $menuLink->customClass($currentCampaign) }}">
-        <a href="{{ $menuLink->getRoute() }}" class="px-2 py-1.5 flex items-center gap-2 my-0.5 rounded">
-            <i class="w-6 flex-shrink-0 text-base {{ $menuLink->icon() }}" aria-hidden="true"></i>
-            {{ $menuLink->name }}
-        </a>
+        <x-sidebar.element
+            :url="$menuLink->getRoute()"
+            :icon="$menuLink->icon()"
+            :text="$menuLink->name"
+        ></x-sidebar.element>
     </li>
 @elseif ($menuLink->isRandom())
     <li class="p-0 m-0 subsection sidebar-quick-link sidebar-quick-link-{{ $menuLink->position }} {{ $sidebar->activeMenuLink($menuLink) }} {{ $menuLink->customClass($currentCampaign) }}">
-        <a href="{{ route('menu_links.random', $menuLink) }}" class="px-2 py-1.5 flex items-center gap-2 my-0.5 rounded">
-            <i class="w-6 flex-shrink-0 text-base {{ $menuLink->icon() }}" aria-hidden="true"></i>
-            {{ $menuLink->name }}
-        </a>
+        <x-sidebar.element
+            :url="$menuLink->getRoute()"
+            :icon="$menuLink->icon()"
+            :text="$menuLink->name"
+        ></x-sidebar.element>
     </li>
 @endif
