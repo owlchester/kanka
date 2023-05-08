2577066dce4119d8e7cfbdfc248bba275cb7bcd8
ilestis <jay.payne.ch@gmail.com>
2023-05-08 10:10:49 -0600

    Paypal polish up

--- config/paypal.php
+++ config/paypal.php
@@ -9,7 +9,7 @@ return [
     'sandbox' => [
         'client_id'         => env('PAYPAL_SANDBOX_CLIENT_ID', ''),
         'client_secret'     => env('PAYPAL_SANDBOX_CLIENT_SECRET', ''),
-        'app_id'            => 'APP-80W284485P519543T',
+        'app_id'            => env('PAYPAL_SANDBOX_APP_ID', ''),
     ],
     'live' => [
         'client_id'         => env('PAYPAL_LIVE_CLIENT_ID', ''),
