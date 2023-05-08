30df1b3e5e45e26beaa0a64e34adea13f2331414
ilestis <jay.payne.ch@gmail.com>
2023-05-08 11:02:15 -0600

    Sidebar smaller vertical padding

--- resources/views/components/sidebar/element-text.blade.php
+++ resources/views/components/sidebar/element-text.blade.php
@@ -1,4 +1,4 @@
-<div class="px-2 py-2 flex items-center gap-2 my-0.5 rounded">
+<div class="px-2 py-1.5 flex items-center gap-2 my-0.5 rounded">
     <i class="w-6 flex-shrink-0 text-base  {{ $icon }}"></i>
     <span>{!! $text !!}</span>
 </div>
