b3921364dedef3c7709fbfdada1d6e485ba7f58c
ilestis <jay.payne.ch@gmail.com>
2023-06-11 18:43:51 -0600

    Fix tree/index switch buttons

--- resources/views/cruds/lists/_actions.blade.php
+++ resources/views/cruds/lists/_actions.blade.php
@@ -1,6 +1,6 @@
 @foreach ($actions as $action)
     @if (empty($action['policy']) || (auth()->check() && auth()->user()->can($action['policy'], $model)))
-        <a href="{{ $action['route'] }}" class="btn2 btn-{{ $action['class'] }} mr-2" @if ($action['blank'])target="_blank"@endif>
+        <a href="{{ $action['route'] }}" class="btn2 btn-{{ $action['class'] }}" @if ($action['blank'])target="_blank"@endif>
             {!! $action['label'] !!}
         </a>
     @endif
