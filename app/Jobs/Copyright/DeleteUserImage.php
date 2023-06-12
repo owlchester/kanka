116373c11bad00e6c13a0b5cfd40f40e8758cd09
spitfire305 <itza305@gmail.com>
2023-06-12 15:15:12 -0600

    Admin: User Image

---
+++ app/Jobs/Copyright/DeleteUserImage.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Jobs\Copyright;
+
+use App\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+class DeleteUserImage implements ShouldQueue
+{
+    use Dispatchable;
+    use InteractsWithQueue;
+    use Queueable;
+    use SerializesModels;
+
+    protected $userId;
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $this->deleteImage();
+
+        Log::info('Removed image from user #' . $this->userId . ' for copyright reasons');
+    }
+
+    private function deleteImage()
+    {        
+        $user = User::where('id', $this->userId)->first();
+
+        if (empty($user) || !(Storage::exists($user->avatar))) {
+            // Image was deleted
+            return;
+        }
+        Storage::delete($user->avatar);
+        $user->avatar = null;
+        $user->saveQuietly();
+    }
+}
