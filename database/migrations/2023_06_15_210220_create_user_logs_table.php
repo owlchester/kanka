8459a83f2d517ad97b91f2040b20da2c317da8bb
ilestis <jay.payne.ch@gmail.com>
2023-06-16 15:52:13 -0600

    User logs new connection

--- database/migrations/2023_06_15_210220_create_user_logs_table.php
+++ database/migrations/2023_06_15_210220_create_user_logs_table.php
@@ -1,37 +1,32 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
-class CreateUserLog extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
-     *
-     * @return void
      */
-    public function up()
+    public function up(): void
     {
-        Schema::create('user_logs', function (Blueprint $table) {
-            $table->increments('id');
+        Schema::connection('logs')->create('user_logs', function (Blueprint $table) {
+            $table->id();
             $table->integer('user_id')->unsigned();
             $table->unsignedTinyInteger('type_id')
                 ->default(\App\Models\UserLog::TYPE_LOGIN);
             $table->string('ip', 255)->nullable();
+            $table->char('country', 6)->nullable();
             $table->timestamps();
-
-            $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete();
         });
     }
 
     /**
      * Reverse the migrations.
-     *
-     * @return void
      */
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('user_logs');
     }
-}
+};
