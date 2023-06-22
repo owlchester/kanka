65c0ac60705c09ed4ca7644e6c6bec4659681879
spitfire305 <itza305@gmail.com>
2023-06-22 11:08:19 -0600

    Summernote: Remove Summary

--- resources/views/editors/summernote.blade.php
+++ resources/views/editors/summernote.blade.php
@@ -9,7 +9,6 @@
     <script src="/vendor/summernote/plugin/summernote-toc-kanka/summernote-toc.js" defer></script>
     <script src="/vendor/summernote/plugin/summernote-aroba-kanka/summernote-aroba.js" defer></script>
     <script src="/vendor/summernote/plugin/summernote-table-ext.js" defer></script>
-    <script src="/vendor/summernote/plugin/spoiler/summernote-spoiler.js" defer></script>
     <script src="/vendor/summernote/plugin/summernote-image-attribute.js" defer></script>
     <script src="/vendor/summernote/plugin/kanka/summernote-bragi-kanka.min.js" defer></script>
     <script src="/vendor/summernote/plugin/kanka/summernote-prettify-kanka.min.js" defer></script>
