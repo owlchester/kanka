2e94990244658707473e32cddb5c8568ab25a90e
ilestis <jay.payne.ch@gmail.com>
2023-06-25 12:09:04 -0600

    Fix dashboard select2 not selectable on edit

--- resources/views/dashboard/widgets/forms/edit.blade.php
+++ resources/views/dashboard/widgets/forms/edit.blade.php
@@ -15,7 +15,8 @@
     'title' => __('dashboard.setup.widgets.' . $model->widget),
     'titleIcon' => $model->widgetIcon(),
     'content' => 'dashboard.widgets.forms._' . $widget,
-    'deleteID' => '#delete-form-widget-' . $model->id
+    'deleteID' => '#delete-form-widget-' . $model->id,
+    'dropdownParent' => '#edit-widget',
 ])
 <input type="hidden" name="widget" value="{{ $widget }}">
 @if(empty($dashboards) && !empty($dashboard))
