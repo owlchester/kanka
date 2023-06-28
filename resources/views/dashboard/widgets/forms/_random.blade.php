f1e85db7c1c828d866d3ac3a8acc193ed30bff8d
spitfire305 <itza305@gmail.com>
2023-06-28 15:52:22 -0600

    Random Widget: Option to use header

--- resources/views/dashboard/widgets/forms/_random.blade.php
+++ resources/views/dashboard/widgets/forms/_random.blade.php
@@ -53,6 +53,16 @@
             @includeWhen(!$boosted, 'dashboard.widgets.forms._boosted')
 
             <x-grid>
+                {!! Form::hidden('config[entity-header]', 0) !!}
+                <div class="field-header checkbox">
+                    <label>
+                        {!! Form::checkbox('config[entity-header]', 1, (!empty($model) ? $model->conf('entity-header') : null), ['id' => 'config-entity-header', 'disabled' => !$boosted ? 'disabled' : null]) !!}
+                        {{ __('dashboard.widgets.recent.entity-header') }}
+
+                        <i class="fa-solid fa-question-circle hidden-xs hidden-sm" data-toggle="tooltip" title="{{ __('dashboard.widgets.recent.helpers.entity-header') }}" aria-hidden="true"></i>
+                    </label>
+                    <p class="help-block visible-xs visible-sm">{{ __('dashboard.widgets.recent.helpers.entity-header') }}</p>
+                </div>
                 @include('dashboard.widgets.forms._related')
                 @include('dashboard.widgets.forms._class')
             </x-grid>
