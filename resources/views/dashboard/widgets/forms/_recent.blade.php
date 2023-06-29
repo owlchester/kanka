d72853263d0204ef2092a94a8a3c2b020d795748
spitfire305 <itza305@gmail.com>
2023-06-29 11:52:29 -0600

    Reduced Copypasted code

--- resources/views/dashboard/widgets/forms/_recent.blade.php
+++ resources/views/dashboard/widgets/forms/_recent.blade.php
@@ -70,17 +70,7 @@
 
                 <div class="col-span-2 collapse !visible {{ isset($model) && $model->conf('singular') ? 'in' : null }}" id="widget-advanced">
                     @if($campaignService->campaign()->boosted())
-                        {!! Form::hidden('config[entity-header]', 0) !!}
-                        <div class="field-header checkbox">
-                            <label>
-                                {!! Form::checkbox('config[entity-header]', 1, (!empty($model) ? $model->conf('entity-header') : null), ['id' => 'config-entity-header']) !!}
-                                {{ __('dashboard.widgets.recent.entity-header') }}
-
-                                <i class="fa-solid fa-question-circle hidden-xs hidden-sm" data-toggle="tooltip" title="{{ __('dashboard.widgets.recent.helpers.entity-header') }}" aria-hidden="true"></i>
-                            </label>
-                        </div>
-                        <p class="help-block visible-xs visible-sm">{{ __('dashboard.widgets.recent.helpers.entity-header') }}</p>
-
+                        @include('dashboard.widgets.forms._header_select')
                         @include('dashboard.widgets.forms._related')
                     @else
                         <p class="help-block">{!! __('dashboard.widgets.advanced_options_boosted', [
