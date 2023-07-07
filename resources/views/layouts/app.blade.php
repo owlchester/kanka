9a41fd2d85f8d74c8c4c51b0b4571e2dafe3c4eb
ilestis <jay.payne.ch@gmail.com>
2023-07-06 18:50:55 -0600

    Implement changes to ads from partner

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -102,7 +102,7 @@ $showSidebar = (!empty($sidebar) && $sidebar === 'settings') || !empty($campaign
 
             @yield('content-header')
 
-            <section class="content" role="main">
+            <section class="content" role="main" id="content-container">
                 @if (isset($sidebar) && $sidebar === 'settings') <div class="max-w-4xl"> @endif
                 @if (auth()->check() && \App\Facades\Identity::isImpersonating())
                     <div class=" alert p-4 rounded alert-warning border-0 shadow-xs flex flex-col lg:flex-row items-center gap-2 lg:gap-5 mb-5">
