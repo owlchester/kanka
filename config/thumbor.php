05da076980cb222259c98d9940d79011d5479d43
ilestis <jay.payne.ch@gmail.com>
2023-07-18 07:32:56 -0600

    Disable thumbor by default on new projects, as it only work with localhost

--- config/thumbor.php
+++ config/thumbor.php
@@ -7,7 +7,7 @@
  */
 
 return [
-    'key' => env('THUMBOR_KEY', 'local'),
+    'key' => env('THUMBOR_KEY'),
     'bases' => [
         'user' => 'https://' . env('AWS_BUCKET') . '.s3.eu-central-1.amazonaws.com/',
         'app' => 'https://' . env('AWS_BUCKET_APP') . '.s3.eu-central-1.amazonaws.com/',
