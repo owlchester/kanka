007bb2136957b531cae20de64c62afa8c7664c3c
spitfire305 <itza305@gmail.com>
2023-07-18 16:37:14 -0600

    Dashboard Widgets: Show members sort

--- resources/views/dashboard/widgets/previews/_members.blade.php
+++ resources/views/dashboard/widgets/previews/_members.blade.php
@@ -18,8 +18,10 @@ if (isset($model)) {
 }
 $members = $entity->isFamily()
     ? $child->members()->orderBy('name')->get()
-    : $child->members()->with(['character', 'character.entity'])->get()
-;
+    : $child->members()->with(['character', 'character.entity'])
+        ->orderBy(
+            App\Models\Character::select('name')->whereColumn('organisation_member.character_id', 'characters.id'), 'asc'
+        )->get();
 @endphp
 
 <div class="widget-advanced-members">
