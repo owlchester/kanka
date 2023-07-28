5deae748bc8badeb5058550b410a084d343561f8
spitfire305 <itza305@gmail.com>
2023-07-28 15:22:15 -0600

    Calendars: Custom date formatting

---
+++ resources/views/cruds/fields/format.blade.php
@@ -0,0 +1,10 @@
+<div class="field-format">
+    <label>
+        {{ __('calendars.fields.format') }}
+        <i class="fa-solid fa-question-circle hidden-xs hidden-sm" aria-hidden="true" data-toggle="tooltip" title="{{ __('calendars.helpers.format', ['format' => __('calendars.helpers.custom_format')]) }}" href="https://docs.kanka.io/en/latest/entities/calendars.html#date-format"></i>
+    </label>
+    {!! Form::text('format', $model->format, ['class' => 'form-control']) !!}
+    <p class="help-block visible-xs visible-sm">
+        {!! __('calendars.helpers.format', ['format' => link_to('https://docs.kanka.io/en/latest/entities/calendars.html#date-format', __('calendars.helpers.custom_format'), null, ['target' => '_blank'])]) !!}
+    </p>
+</div>
