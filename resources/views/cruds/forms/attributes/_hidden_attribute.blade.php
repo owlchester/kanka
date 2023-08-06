9165ec9486204cc794956b721c011b56d7989f54
ilestis <jay.payne.ch@gmail.com>
2023-08-06 15:04:21 -0600

    Fix hidden attributes modal

--- resources/views/cruds/forms/attributes/_hidden_attribute.blade.php
+++ resources/views/cruds/forms/attributes/_hidden_attribute.blade.php
@@ -1,12 +1,10 @@
-<div class="field-hidden-attribute">
-    <div class="row attribute_row">
-        <div class="col-xs-12 col-sm-4">
-            <dt>{{ $attribute->name }}</dt>
-        </div>
-        @if ($attribute->isCheckbox())
-            <i class="@if($attribute->value == 1)fa-solid fa-check-square @else fa-solid fa-square @endif mr-2 fa-2x" data-tab="{{ __('entities/attributes.visibility.tab') }}" data-entry="{{ __('entities/attributes.visibility.entry') }}" title="@if($attribute->isPinned()) {{ __('entities/attributes.visibility.entry') }} @else  {{ __('entities/attributes.visibility.tab') }} @endif"></i>
-        @else
-            {{ $attribute->value }}
-        @endif
-    </div>
+<div class="field-hidden-attribute w-full grid grid-cols-2">
+    <span>{{ $attribute->name }}</span>
+    <span>
+    @if ($attribute->isCheckbox())
+        <i class="@if($attribute->value == 1)fa-solid fa-check-square @else fa-solid fa-square @endif mr-2 fa-2x" data-tab="{{ __('entities/attributes.visibility.tab') }}" data-entry="{{ __('entities/attributes.visibility.entry') }}" title="@if($attribute->isPinned()) {{ __('entities/attributes.visibility.entry') }} @else  {{ __('entities/attributes.visibility.tab') }} @endif"></i>
+    @else
+        {{ $attribute->value }}
+    @endif
+    </span>
 </div>
