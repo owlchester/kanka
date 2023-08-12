acc5881473d9c9384a9ebbe51db3b37837e5b071
ilestis <ilestis@users.noreply.github.com>
2023-08-12 14:53:06 +0000

    Fix styling

--- app/Policies/CampaignPolicy.php
+++ app/Policies/CampaignPolicy.php
@@ -3,7 +3,6 @@
 namespace App\Policies;
 
 use App\Facades\CampaignCache;
-use App\Facades\CampaignLocalization;
 use App\Facades\EntityPermission;
 use App\Facades\Identity;
 use App\Facades\UserCache;
