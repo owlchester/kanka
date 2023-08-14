02c3f44163def31d760d98a08f370ed2509ebea9
ilestis <jay.payne.ch@gmail.com>
2023-08-14 16:52:54 -0600

    Split slug migration in two for qa/prod to work

--- database/migrations/2023_08_14_225219_add_new_slug.php
+++ database/migrations/2023_08_14_225219_add_new_slug.php
@@ -4,15 +4,17 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class () extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::table('campaigns', function (Blueprint $table) {
-            $table->dropColumn('slug');
+            $table->string('slug', 45)->nullable()->unique()->after('name');
         });
+        \Illuminate\Support\Facades\DB::statement("UPDATE campaigns SET slug = id");
     }
 
     /**
@@ -20,8 +22,5 @@ return new class () extends Migration {
      */
     public function down(): void
     {
-        Schema::table('campaigns', function (Blueprint $table) {
-            $table->dropColumn('slug');
-        });
     }
 };
