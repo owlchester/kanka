91d21ba6cee2206b4814ff020e9f73eea6c951e4
ilestis <jay.payne.ch@gmail.com>
2023-08-15 10:31:42 -0600

    Cleanup

--- app/Models/Visibility.php
+++ app/Models/Visibility.php
@@ -13,15 +13,6 @@ use Illuminate\Database\Eloquent\Model;
  */
 class Visibility extends Model
 {
-    /**
-     * Visibility strings for old tables that haven't been migrated
-     */
-    public const VISIBILITY_ALL_STR = 'all';
-    public const VISIBILITY_ADMIN_STR = 'admin';
-    public const VISIBILITY_SELF_STR = 'self';
-    public const VISIBILITY_MEMBERS_STR = 'members';
-    public const VISIBILITY_ADMIN_SELF_STR = 'admin-self';
-
     public $fillable = [
         'code'
     ];
