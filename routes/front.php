2111b6a2dcc1b9e3b163591d817922a09f8c4a71
ilestis <ilestis@users.noreply.github.com>
2023-08-18 17:44:41 +0000

    Fix styling

--- routes/front.php
+++ routes/front.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Http\Controllers\User\ProfileController;
 use Illuminate\Support\Facades\Route;
-
 
 //Route::get('/', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
