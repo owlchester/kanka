aa0b4471ea3ec3f5693dfdec85a425d0e7719228
ilestis <jay.payne.ch@gmail.com>
2023-11-01 09:46:52 -0600

    Missing calendar format migration

---
+++ database/migrations/2023_07_28_181821_update_calendars_add_format.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('calendars', function (Blueprint $table) {
+            $table->string('format', 20)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('calendars', function (Blueprint $table) {
+            $table->dropColumn('format');
+        });
+    }
+};
