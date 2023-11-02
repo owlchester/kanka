76dec8064a4d5fb9247a10dd7e654b1ae8149a38
ilestis <jay.payne.ch@gmail.com>
2023-11-01 20:43:35 -0600

    Fix repeating pinned members

--- resources/views/entities/components/members.blade.php
+++ resources/views/entities/components/members.blade.php
@@ -9,7 +9,7 @@ $previousRelation = null;
 @foreach ($models as $member)
     @if(!empty($previousRelation) && $previousRelation == $member->role)
     <div class="pinned-member flex gap-2" data-character="{{ $member->character_id }}" data-organisation="{{ $member->organisation_id }}" data-role="{{ $member->role }}" data-private="{{ $member->is_private }}">
-        <div class="text-right">
+        <div class="grow text-right">
             @if ($model instanceof \App\Models\Character)
                 {!! $member->organisation->tooltipedLink() !!}
             @else
