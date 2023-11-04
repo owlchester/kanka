049759048a2e331bc46ce1e5c424eda4679f1241
ilestis <jay.payne.ch@gmail.com>
2023-11-04 05:56:25 -0600

    Fix default

--- config/domains.php
+++ config/domains.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'api' => env('API_DOMAIN', 'https://api.kanka.io'),
-    'app' => env('APP_DOMAIN', 'https://app.kanka.io'),
-    'front' => env('FRONT_DOMAIN', 'https://kanka.io'),
+    'api' => env('API_DOMAIN', 'api.kanka.io'),
+    'app' => env('APP_DOMAIN', 'app.kanka.io'),
+    'front' => env('FRONT_DOMAIN', 'kanka.io'),
 ];
