f405a14e16373e1b16d112b360c4a663fac7d868
ilestis <jay.payne.ch@gmail.com>
2023-11-12 09:51:45 -0600

    Perf improvements for grid thumbnails

--- resources/views/cruds/datagrids/_grid.blade.php
+++ resources/views/cruds/datagrids/_grid.blade.php
@@ -18,7 +18,7 @@
 @if ($stacked > 0)
     <div class="stack inline-grid items-center align-items-end w-[47%] xs:w-[25%] sm:w-48 " data-stack="{{ $stacked }}">
         <div class="entity overflow-hidden rounded shadow-sm hover:shadow-md aspect-square w-full flex flex-col bg-box" title="{{ $model->name }}" @foreach ($dataAttributes as $att) data-{{ $att }}="true" @endforeach data-entity="{{ $model->entity->id }}" data-entity-type="{{ $model->getEntityType() }}" @if (!empty($model->type)) data-type="{{ \Illuminate\Support\Str::slug($model->type) }}" @endif>
-            <a href="{{ route($route . '.' . $sub, [$campaign, 'm' => $mode ?? 'grid', 'parent_id' => $model->id]) }}"  class="block avatar grow relative cover-background overflow-hidden text-center" style="background-image: url('{{ Avatar::entity($model->entity)->child($model)->fallback()->size(192, 144)->thumbnail() }}')">
+            <a href="{{ route($route . '.' . $sub, [$campaign, 'm' => $mode ?? 'grid', 'parent_id' => $model->id]) }}"  class="block avatar grow relative cover-background overflow-hidden text-center" style="background-image: url('{{ Avatar::entity($model->entity)->fallback()->size(192, 144)->thumbnail() }}')">
 
                 @if ($model->is_private)
                     <div class="bubble-private absolute left-1.5 top-1.5 text-base shadow-xs flex justify-center align-items-center items-center aspect-square rounded-full w-6 h-6 bg-box opacity-80">
@@ -52,7 +52,7 @@
     </div>
 @else
     <div class="entity block overflow-hidden rounded shadow-sm hover:shadow-md w-[47%] xs:w-[25%] sm:w-48 aspect-square flex flex-col bg-box @if (isset($isParent)) shadow-lg stacking-parent font-bold @endif" title="{{ $model->name }}" @foreach ($dataAttributes as $att) data-{{ $att }}="true" @endforeach data-entity="{{ $model->entity->id }}" data-entity-type="{{ $model->getEntityType() }}" @if (!empty($model->type)) data-type="{{ \Illuminate\Support\Str::slug($model->type) }}" @endif>
-        <a href="{{ $model->getLink() }}" class="block avatar grow relative cover-background" style="background-image: url('{{ Avatar::entity($model->entity)->child($model)->fallback()->size(192, 144)->thumbnail() }}')">
+        <a href="{{ $model->getLink() }}" class="block avatar grow relative cover-background" style="background-image: url('{{ Avatar::entity($model->entity)->fallback()->size(192, 144)->thumbnail() }}')">
             @if ($model->is_private)
                 <div class="bubble-private absolute left-1.5 top-1.5 text-base shadow-xs flex justify-center align-items-center items-center inline-block aspect-square rounded-full w-6 h-6 text-xs bg-box text-base opacity-80">
                     <x-icon class="fa-regular fa-lock" :title="__('crud.is_private')"></x-icon>
