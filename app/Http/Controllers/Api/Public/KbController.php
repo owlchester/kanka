4e246e6b8e4eb8ce2317e03bb25638b7cbef5178
ilestis <jay.payne.ch@gmail.com>
2023-11-17 06:04:40 -0600

    Bring back KB handlers

---
+++ app/Http/Controllers/Api/Public/KbController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Api\Public;
+
+use App\Http\Controllers\Controller;
+use App\Services\Api\KbService;
+use Carbon\Carbon;
+
+class KbController extends Controller
+{
+    protected KbService $service;
+
+    public function __construct(KbService $service)
+    {
+        $this->service = $service;
+    }
+    public function index()
+    {
+        return response()->json($this->service->api())
+            ->header('Expires', Carbon::now()->addDays(7)->toDateTimeString())
+            ;
+    }
+}
