4003062f525ebe998f36b6c8e90329c71f67f5f4
spitfire305 <spitfire305@users.noreply.github.com>
2023-11-24 17:09:32 +0000

    Fix styling

--- app/Jobs/Emails/SubscriptionDeletedEmailJob.php
+++ app/Jobs/Emails/SubscriptionDeletedEmailJob.php
@@ -11,7 +11,6 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Mail;
 
 class SubscriptionDeletedEmailJob implements ShouldQueue
 {
