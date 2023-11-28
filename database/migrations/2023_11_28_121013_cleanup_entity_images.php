ccdff430ca356d385c0892fa5bdda540544b1325
ilestis <jay.payne.ch@gmail.com>
2023-11-28 06:13:16 -0600

    Cleanup old image column

---
+++ database/migrations/2023_11_28_121013_cleanup_entity_images.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        $tables = ['abilities', 'calendars', 'characters', 'creatures', 'events', 'families', 'items', 'journals', 'locations', 'maps', 'notes', 'organisations', 'quests', 'races', 'tags', 'timelines'];
+        foreach ($tables as $tablename) {
+            if (!Schema::hasColumn($tablename, 'image')) {
+                continue;
+            }
+            Schema::table($tablename, function (Blueprint $table) {
+                $table->dropColumn('image');
+            });
+        }
+        //
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
