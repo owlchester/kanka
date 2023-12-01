ce23fa33e5e6c92e61112177e84950ab4ade1060
ilestis <jay.payne.ch@gmail.com>
2023-12-01 10:06:19 -0600

    Fix legacy editor to work in Kanka 2.0

--- resources/views/editors/tinymce.blade.php
+++ resources/views/editors/tinymce.blade.php
@@ -1,3 +1,12 @@
+
+@section('styles')
+    @parent
+    @if (config('app.asset_url'))
+        <link href="{{ config('app.asset_url') }}/vendor/bootstrap/bootstrap-summernote.css?v={{ config('app.version') }}" rel="stylesheet">
+    @else
+        <link href="/css/bootstrap-summernote.css?v={{ config('app.version') }}" rel="stylesheet">
+    @endif
+@endsection
 @section('scripts')
     @parent
     <script src="{{ '/js/tinymce/tinymce.min.js' }}"></script>
@@ -12,7 +21,7 @@
                 "searchreplace code fullscreen",
                 "insertdatetime media nonbreaking table directionality",
                 "emoticons paste textcolor colorpicker textpattern",
-                "fullpage mention media"
+                "fullpage @if(!empty($campaign)) mention @endif media"
             ],
             toolbar: "undo redo | styleselect | bold italic underline strikethrough forecolor backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | hr | link image table media | code fullscreen",
             nanospell_server: "php",
