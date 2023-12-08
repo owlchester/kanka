1d7ef04ca3d154fa665353078c40725f6017650e
ilestis <jay.payne.ch@gmail.com>
2023-12-08 12:04:44 -0600

    Maps: validate min/max zooms in PHP

--- app/Http/Requests/StoreMap.php
+++ app/Http/Requests/StoreMap.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use App\Facades\Limit;
+use App\Models\Map;
 use App\Traits\ApiRequest;
 use Illuminate\Foundation\Http\FormRequest;
 
@@ -38,6 +39,9 @@ class StoreMap extends FormRequest
             'template_id' => 'nullable',
             'center_x' => 'nullable|numeric',
             'center_y' => 'nullable|numeric',
+            'max_zoom' => 'nullable|numeric|min:1|max:' . Map::MAX_ZOOM,
+            'min_zoom' => 'nullable|numeric|min:' . Map::MIN_ZOOM . '|max:' . Map::MAX_ZOOM_REAL,
+            'initial_zoom' => 'nullable|numeric|min:' . MAP::MIN_ZOOM . '|max:' . Map::MAX_ZOOM_REAL,
         ];
 
         $self = request()->route('map');
