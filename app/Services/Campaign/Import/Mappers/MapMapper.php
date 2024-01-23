186b63a308f433d769843d3968bd71248a7aa278
ilestis <jay.payne.ch@gmail.com>
2024-01-22 18:11:24 -0600

    Fix map mapper

--- app/Services/Campaign/Import/Mappers/MapMapper.php
+++ app/Services/Campaign/Import/Mappers/MapMapper.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Str;
 
 class MapMapper extends MiscMapper
 {
-    protected array $ignore = ['id', 'campaign_id', 'slug', 'image', '_lft', '_rgt', 'map_id', 'created_at', 'updated_at'];
+    protected array $ignore = ['id', 'campaign_id', 'slug', 'image', '_lft', '_rgt', 'map_id', 'created_at', 'updated_at', 'location_id'];
 
     protected string $className = Map::class;
     protected string $mappingName = 'maps';
@@ -32,6 +32,7 @@ class MapMapper extends MiscMapper
     {
         // @phpstan-ignore-next-line
         $this->loadModel()
+            ->foreign('locations', 'location_id')
             ->groups()
             ->layers()
             ->markers()
