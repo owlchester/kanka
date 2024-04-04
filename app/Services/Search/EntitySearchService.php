389978dbf592798f44619c1046e1ff51d1091f9d
ilestis <jay.payne.ch@gmail.com>
2024-04-04 12:06:39 -0600

    Fix performance and formatting

--- app/Services/Search/EntitySearchService.php
+++ app/Services/Search/EntitySearchService.php
@@ -40,7 +40,7 @@ class EntitySearchService
         ];
 
         if ($term2) {
-            $queries[] =             
+            $queries[] =
                 (new SearchQuery())
                     ->setIndexUid('entities')
                     ->setQuery($term2)
@@ -99,30 +99,67 @@ class EntitySearchService
      */
     protected function fetch(): array
     {
-        $posts = Post::whereIn('id', $this->postIds)->get();
-        $attributes = Attribute::with('entity')->has('entity')->whereIn('id', $this->attributeIds)->get();
-        $questElements = QuestElement::with(['quest', 'quest.entity'])->has('quest')->whereIn('id', $this->questElementIds)->get();
-        $timelineElements = TimelineElement::with(['timeline', 'timeline.entity'])->has('timeline')->whereIn('id', $this->timelineElementIds)->get();
+        $posts = Post::select(['id', 'entity_id'])
+            ->with('entity')
+            ->has('entity')
+            ->whereIn('id', $this->postIds)
+            ->get();
+        $attributes = Attribute::select(['id', 'entity_id'])
+            ->with('entity')
+            ->has('entity')->whereIn('id', $this->attributeIds)
+            ->get();
+        $questElements = QuestElement::select(['id', 'quest_id'])
+            ->with(['quest', 'quest.entity'])
+            ->has('quest')->whereIn('id', $this->questElementIds)
+            ->get();
+        $timelineElements = TimelineElement::select(['id', 'timeline_id'])
+            ->with(['timeline', 'timeline.entity'])
+            ->has('timeline')
+            ->whereIn('id', $this->timelineElementIds)
+            ->get();
 
         //Get entities from db
-        $entities = Entity::whereIn('id', $this->ids)->orderBy('name')->get();
+        $entities = Entity::select('id', 'name')
+            ->whereIn('id', $this->ids)
+            ->orderBy('name')
+            ->get();
 
         //Process entities for output
         $output = [];
         foreach ($entities as $entity) {
-            $output[$entity->id] = ['id' => $entity->id, 'entity' => $entity->name, 'url' => $entity->url()];
+            $output[$entity->id] = [
+                'id' => $entity->id,
+                'entity' => $entity->name,
+                'url' => $entity->url()
+            ];
         }
         foreach ($attributes as $attribute) {
-            $output[$attribute->entity->id] = ['id' => $attribute->entity->id, 'entity' => $attribute->entity->name, 'url' => $attribute->entity->url()];
+            $output[$attribute->entity->id] = [
+                'id' => $attribute->entity->id,
+                'entity' => $attribute->entity->name,
+                'url' => $attribute->entity->url()
+            ];
         }
         foreach ($posts as $post) {
-            $output[$post->entity->id] = ['id' => $post->entity->id, 'entity' => $post->entity->name, 'url' => $post->entity->url()];
+            $output[$post->entity->id] = [
+                'id' => $post->entity->id,
+                'entity' => $post->entity->name,
+                'url' => $post->entity->url()
+            ];
         }
         foreach ($questElements as $questElement) {
-            $output[$questElement->quest->entity->id] = ['id' => $questElement->quest->entity->id, 'entity' => $questElement->quest->name, 'url' => $questElement->quest->entity->url()];
+            $output[$questElement->quest->entity->id] = [
+                'id' => $questElement->quest->entity->id,
+                'entity' => $questElement->quest->name,
+                'url' => $questElement->quest->entity->url()
+            ];
         }
         foreach ($timelineElements as $timelineElement) {
-            $output[$timelineElement->timeline->entity->id] = ['id' => $timelineElement->timeline->entity->id, 'entity' => $$timelineElement->timeline->entity->name, 'url' => $timelineElement->timeline->entity->url()];
+            $output[$timelineElement->timeline->entity->id] = [
+                'id' => $timelineElement->timeline->entity->id,
+                'entity' => $timelineElement->timeline->entity->name,
+                'url' => $timelineElement->timeline->entity->url()
+            ];
         }
 
         return $output;
