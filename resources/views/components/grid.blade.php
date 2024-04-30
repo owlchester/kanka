331a8beb3997c68adeb55474c5cd2314cac0308a
ilestis <jay.payne.ch@gmail.com>
2024-04-29 18:44:04 -0600

    Grid always 1/1 on mobile and small screens

--- resources/views/components/grid.blade.php
+++ resources/views/components/grid.blade.php
@@ -1,4 +1,4 @@
-<div class="@if ($type === '1/1') flex flex-col @else grid @endif gap-4 w-full
+<div class="flex flex-col @if ($type !== '1/1') md:grid @endif gap-4 w-full
 @if ($type === '3/4') grid-cols-4
 @elseif ($type === '3/3') md:grid-cols-3
 @elseif ($type !== '1/1') grid-cols-1 md:grid-cols-2 @endif
