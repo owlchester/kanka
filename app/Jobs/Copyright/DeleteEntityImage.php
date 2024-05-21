35384daba04973de44db5865c046f18ba68684e0
spitfire305 <spitfire305@users.noreply.github.com>
2024-05-21 22:01:17 +0000

    Fix styling

--- app/Jobs/Copyright/DeleteEntityImage.php
+++ app/Jobs/Copyright/DeleteEntityImage.php
@@ -11,7 +11,6 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Log;
-use Illuminate\Http\Request;
 use App\Services\ImageService;
 use App\Models\Entity;
 
