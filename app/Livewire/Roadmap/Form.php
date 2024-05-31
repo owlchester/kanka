6e175d6bbb5fba37426798d4d8ca21c1f4c1ab46
Spitfire <itza305@gmail.com>
2024-05-31 11:40:34 -0600

    Auto upvote own feature

--- app/Livewire/Roadmap/Form.php
+++ app/Livewire/Roadmap/Form.php
@@ -4,6 +4,7 @@ namespace App\Livewire\Roadmap;
 
 use App\Enums\FeatureStatus;
 use App\Models\Feature;
+use App\Models\FeatureVote;
 use App\Models\FeatureFile;
 use Livewire\Attributes\Validate;
 use Livewire\Component;
@@ -41,6 +42,16 @@ class Form extends Component
         $feat->status_id = FeatureStatus::Draft;
         $feat->save();
 
+        if (!auth()->user()->can('vote', $feat)) {
+            /** @var FeatureVote $vote */
+            $vote = new FeatureVote;
+            $vote->feature_id = $feat->id;
+            $vote->user_id = auth()->user()->id;
+            $vote->save();
+            $feat->upvote_count++;
+            $feat->updateQuietly();
+        }
+        
         NewFeatureEmailJob::dispatch($feat);
 
         if ($this->file) {
