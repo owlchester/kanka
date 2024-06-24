ec5ddedf0211ecefdad5701d6c76a49669a1d660
ilestis <jay.payne.ch@gmail.com>
2024-06-24 08:25:48 -0600

    Newsletter migration script

---
+++ app/Console/Commands/Migrations/NewsletterSubCommand.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Console\Commands\Migrations;
+
+use App\Services\NewsletterService;
+use App\User;
+use Illuminate\Console\Command;
+
+class NewsletterSubCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'migrate:newsletter-sub';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Update users who are subbed and want the newsletter to be in the correct mailerlite group';
+
+    protected NewsletterService $service;
+
+    protected int $count = 0;
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $this->service = app()->make(NewsletterService::class);
+        User::whereNotNull('pledge')
+            ->where('settings', 'like', '%mail_release%')
+            ->chunk(500, function ($users) {
+            foreach ($users as $user) {
+                if (!$user->mail_release) {
+                    continue;
+                }
+                $options = [
+                    'releases' => (bool) $user->mail_release
+                ];
+                $this->service->user($user)->update($options);
+                $this->count++;
+            }
+        });
+
+        $this->info('Processes ' . $this->count . ' users.');
+    }
+}
