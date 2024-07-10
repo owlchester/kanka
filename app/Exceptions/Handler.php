c37008f5e64661c8a4aef8741bc27a6c66c50e75
ilestis <jay.payne.ch@gmail.com>
2024-07-10 15:43:39 -0600

    Better form support for backend errors

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -9,6 +9,8 @@ use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Exceptions\ThrottleRequestsException;
 use Illuminate\Support\Str;
 use Illuminate\Validation\ValidationException;
+use Illuminate\View\ViewException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\HttpKernel\Exception\HttpException as SymHttpException;
--- resources/js/utility/formError.js
+++ resources/js/utility/formError.js
@@ -30,6 +30,11 @@ window.formErrorHandler = function(err, form) {
         return;
     }
 
+    //
+    if (err.data) {
+        window.showToast('Backend error', 'error');
+        return;
+    }
     // Loop through the errors to add the class and error message
     const errors = err.data.errors;
     let logs = [];
