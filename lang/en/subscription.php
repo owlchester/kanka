41463583ea944233374a9265b55a42707e46a17f
ilestis <jay.payne.ch@gmail.com>
2024-07-13 09:57:17 -0600

    Fix currency switching not caught

--- lang/en/subscription.php
+++ lang/en/subscription.php
@@ -9,5 +9,6 @@ return [
         'invalid_card_country'  => [
             'brl'   => 'We\'re sorry but we currently only accept BRL payments for customers with Brazilian credit cards. If you think this is a mistake, contact us at :email.',
         ],
+        'invalid_currency' => 'You previously had a subscription in :old, preventing you from having a new subscription in :new. Please switch your currency to :old, or contact us at :email if you wish to switch currencies.',
     ],
 ];
