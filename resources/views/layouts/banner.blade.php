3cc9be437adafb2726271ccbf480190a8192a315
Spitfire <itza305@gmail.com>
2024-07-15 16:11:42 -0600

    Banners can be implemented using Tutorials

--- resources/views/layouts/banner.blade.php
+++ resources/views/layouts/banner.blade.php
@@ -1,3 +1,9 @@
+<x-tutorial code="ft" type="warning">
+    <p>
+        Kanka will be undergoing scheduled maintenance on Tuesday 21st of May 2024. As a result, Kanka will be unavailable from <a href="https://everytimezone.com/s/bff36b39" target="_blank" style="text-decoration: underline"><i class="fa-solid fa-external-link"></i> 14:30 UTC</a> to 16:00 UTC. Join us on <a href="{{ config('social.discord') }}" target="_blank"  style="text-decoration: underline">Discord</a> to get updates.
+    </p>
+</x-tutorial>
+
 @if ((auth()->guest() || (auth()->check() && !auth()->user()->settings()->get('banner_bf2023'))) && \Carbon\Carbon::create(2023, 11, 28)->isFuture())
     <div class="alert alert-warning p-2 flex gap-2 banner-notification items-center" id="banner-notification">
         <div class="grow">
