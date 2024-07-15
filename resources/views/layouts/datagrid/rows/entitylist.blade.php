632c2b463c47288a23dd8a505f83f62fd5de5cf8
ilestis <jay.payne.ch@gmail.com>
2024-07-15 10:17:59 -0600

    Fix private races showing up

--- resources/views/layouts/datagrid/rows/entitylist.blade.php
+++ resources/views/layouts/datagrid/rows/entitylist.blade.php
@@ -3,6 +3,9 @@ $relation = $with;
 @endphp
 <div class="flex flex-wrap gap-1">
 @foreach ($model->$relation as $rel)
+    @if (!$rel->entity)
+        @continue
+    @endif
     <x-entity-link
         :entity="$rel->entity"
         :campaign="$campaign" />
