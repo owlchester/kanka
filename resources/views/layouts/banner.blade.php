632b24f4bbcb5f293db83066ad87adc1766bdd4b
ilestis <jay.payne.ch@gmail.com>
2024-07-15 20:34:19 -0600

    Cleanup

--- resources/views/layouts/banner.blade.php
+++ resources/views/layouts/banner.blade.php
@@ -1,8 +1,10 @@
-<x-tutorial code="ft" type="warning">
+@if ((auth()->guest() || (auth()->check() && !auth()->user()->settings()->get('banner_bf2023'))) && \Carbon\Carbon::create(2023, 11, 28)->isFuture())
+    <x-tutorial code="ft" type="warning">
     <p>
         Kanka will be undergoing scheduled maintenance on Tuesday 21st of May 2024. As a result, Kanka will be unavailable from <a href="https://everytimezone.com/s/bff36b39" target="_blank" style="text-decoration: underline"><i class="fa-solid fa-external-link"></i> 14:30 UTC</a> to 16:00 UTC. Join us on <a href="{{ config('social.discord') }}" target="_blank"  style="text-decoration: underline">Discord</a> to get updates.
     </p>
 </x-tutorial>
+@endif
 
 @if ((auth()->guest() || (auth()->check() && !auth()->user()->settings()->get('banner_bf2023'))) && \Carbon\Carbon::create(2023, 11, 28)->isFuture())
     <div class="alert alert-warning p-2 flex gap-2 banner-notification items-center" id="banner-notification">
@@ -15,17 +17,6 @@
             <button type="button" class="close p-2 banner-notification-dismiss text-2xl" data-dismiss="#banner-notification" aria-hidden="true" data-url="{{ route('settings.banner', ['code' => 'bf2023']) }}">×</button>
         @endif
     </div>
-@endif
-
-@if (false && auth()->check() && !auth()->user()->settings()->get('banner_ft'))
-<div class="alert alert-warning p-2 flex gap-2 banner-notification " id="banner-notification">
-    <div class="grow">
-        Kanka will be undergoing scheduled maintenance on Tuesday 21st of May 2024. As a result, Kanka will be unavailable from <a href="https://everytimezone.com/s/bff36b39" target="_blank" style="text-decoration: underline"><i class="fa-solid fa-external-link"></i> 14:30 UTC</a> to 16:00 UTC. Join us on <a href="{{ config('social.discord') }}" target="_blank"  style="text-decoration: underline">Discord</a> to get updates.
-    </div>
-    @if (auth()->check())
-        <button type="button" class="close p-2 banner-notification-dismiss text-2xl" data-dismiss="#banner-notification" aria-hidden="true" data-url="{{ route('settings.banner', ['code' => 'ft']) }}">×</button>
-    @endif
-</div>
 @endif
 
 @if (auth()->check() && in_array(app()->currentLocale(), ['nl']) && !auth()->user()->settings()->get('banner_translators_2024'))
