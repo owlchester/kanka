1d620d7e5ae829699463069a13461ffdf8cd89e0
spitfire305 <spitfire305@users.noreply.github.com>
2024-07-23 19:08:38 +0000

    Fix styling

--- app/Models/TimelineElement.php
+++ app/Models/TimelineElement.php
@@ -12,7 +12,6 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Support\Str;
 use Laravel\Scout\Searchable;
 
 /**
