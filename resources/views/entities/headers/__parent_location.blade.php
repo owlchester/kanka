797241143c08e46721b21b57b5937defce35a5e2
ilestis <jay.payne.ch@gmail.com>
2024-08-08 14:51:14 -0600

    Parent locations

--- resources/views/entities/headers/__parent_location.blade.php
+++ resources/views/entities/headers/__parent_location.blade.php
@@ -1,24 +1,24 @@
 <?php /**
  * @var \App\Models\MiscModel $model
  */
-if (!$campaign->enabled('locations') || !$model->location || !$model->location->entity) {
+if (!$campaign->enabled('locations') || !$model->parent || !$model->parent->entity) {
     return;
 }
 ?>
 <div class="entity-header-sub-element">
     <x-icon :class="\App\Facades\Module::duoIcon('location')" :title="__('crud.fields.parent')" />
-    @if ($model->location->parent && $model->location->parent->entity)
+    @if ($model->parent->parent && $model->parent->parent->entity)
         {!! __('crud.fields.locations', [
             'first' => \Illuminate\Support\Facades\Blade::renderComponent(
-                new \App\View\Components\EntityLink($model->location->entity, $campaign)
+                new \App\View\Components\EntityLink($model->parent->entity, $campaign)
                 ),
             'second' => \Illuminate\Support\Facades\Blade::renderComponent(
-                new \App\View\Components\EntityLink($model->location->parent->entity, $campaign)
+                new \App\View\Components\EntityLink($model->parent->parent->entity, $campaign)
                 ),
         ]) !!}
     @else
         <x-entity-link
-            :entity="$model->location->entity"
+            :entity="$model->parent->entity"
             :campaign="$campaign" />
     @endif
 </div>
