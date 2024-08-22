481dba51c1163138a3b42253ed77ab5e626aad3d
Spitfire <itza305@gmail.com>
2024-08-22 10:44:18 -0600

    PostRecove

--- lang/en/campaigns/recovery.php
+++ lang/en/campaigns/recovery.php
@@ -6,18 +6,10 @@ return [
     ],
     'error'         => 'An error occurred trying to recover entities.',
     'fields'        => [
-        'deleted'   => 'Deleted',
+        'deleted'    => 'Deleted',
+        'deleted_at' => 'Deleted :date'
     ],
     'helper'        => 'Deleted entities of the campaign can be recovered for up to :count days. Entities deleted while the campaign isn\'t upgraded to premium status are still recoverable once the campaign is upgraded.',
-    'post-title'    => 'Post Recovery',
-    'posts'         => [
-        'error'     => 'An error occurred trying to recover posts.',
-        'success'   => '{1} :count post was recovered.|[2,*] :count posts were recovered.',
-    ],
-    'success'       => '{1} :count entity was recovered.|[2,*] :count entities were recovered.',
+    'success_v2'    => '{1} :count post or entity was recovered.|[2,*] :count posts and/or entities were recovered.',
     'title'         => 'Entity Recovery',
-    'toggle'        => [
-        'entity'    => 'Switch to entity recovery',
-        'post'      => 'Switch to post recovery',
-    ],
 ];
