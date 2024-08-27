f4c4e7c378c0ffcf777ce88a2a9985ecd8b3805a
ilestis <jay.payne.ch@gmail.com>
2024-08-27 09:09:31 -0600

    Lang

--- lang/en/gallery.php
+++ lang/en/gallery.php
@@ -18,7 +18,7 @@ return [
     ],
     'download'  => [
         'errors'    => [
-            'copy_failed'           => 'Our servers couldn\'t download the given\ image.',
+            'copy_failed'           => 'Our servers couldn\'t download the given image.',
             'gallery_full_free'     => 'The gallery storage space is full. Enable premium features for more storage.',
             'gallery_full_premium'  => 'The gallery storage space is full. Remove unused files first.',
             'invalid_format'        => 'The file isn\'t a valid file format.',
@@ -27,12 +27,19 @@ return [
         ],
     ],
     'filters' => [
-        'only_unused' => 'Only show unused images.',
+        'only_unused' => 'Only show unused files',
+    ],
+    'move' => [
+        'success' => '[0] Moved 0 elements|[1] Moved one element|{2,*} Moved :count elements'
+    ],
+    'update' => [
+        'home' => 'Home folder',
+        'success' => '[0] Updated 0 elements|[1] Updated one element|{2,*} Updated :count elements'
+    ],
+    'delete' => [
+        'success' => '[0] Deleted 0 elements|[1] Deleted one element|{2,*} Deleted :count elements'
     ],
     'file' => [
         'saved' => 'Saved',
     ],
-    'delete' => [
-        'success' => '[0] Deleted 0 elements|[1] Deleted one element|{2,*} Deleted :count elements'
-    ]
 ];
