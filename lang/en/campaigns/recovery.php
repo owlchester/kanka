6d5527a08c18bac07ff32073c419f842093e6880
Spitfire <itza305@gmail.com>
2024-08-28 07:46:56 -0600

    Fixed translation

--- lang/en/campaigns/recovery.php
+++ lang/en/campaigns/recovery.php
@@ -10,6 +10,6 @@ return [
         'deleted_at' => 'Deleted :date'
     ],
     'helper'        => 'Deleted entities of the campaign can be recovered for up to :count days. Entities deleted while the campaign isn\'t upgraded to premium status are still recoverable once the campaign is upgraded.',
-    'success_v2'    => '{1} :count post or entity was recovered.|[2,*] :count posts and/or entities were recovered.',
+    'success_v2'    => '{1} :count element was recovered.|[2,*] :count elements were recovered.',
     'title'         => 'Entity Recovery',
 ];
