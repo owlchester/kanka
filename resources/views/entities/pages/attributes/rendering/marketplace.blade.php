0660cf17a58e5b6b8d2d6a4b244c8d6e768fb85f
ilestis <jay.payne.ch@gmail.com>
2024-09-04 14:34:41 -0600

    Add more js data for character sheets

--- resources/views/entities/pages/attributes/rendering/marketplace.blade.php
+++ resources/views/entities/pages/attributes/rendering/marketplace.blade.php
@@ -41,6 +41,7 @@ if (!isset($entity)) {
     <script>
         const entityData = {
             name: `{{ $entity->name }}`,
+            type_field: `{{ $entity->child->type }}`,
             is_private: {{ $entity->is_private ? 'true' : 'false' }},
             type: {
                 id: {{ $entity->type_id }},
@@ -51,7 +52,52 @@ if (!isset($entity)) {
 @foreach ($entity->allAttributes as $attr)
 "{{ $attr->exposedName() }}": `{!! $attr->value !!}`,
 @endforeach
-            }
+            },
+@if ($entity->child instanceof \App\Models\Character)
+            gender: `{{ $entity->child->sex }}`,
+            pronouns: `{{ $entity->child->pronouns }}`,
+            is_dead: {{ $entity->child->isDead() ? 'true' : 'false' }},
+            title: `{{ $entity->child->title }}`,
+            age: `{{ $entity->child->age }}`,
+            traits: [@foreach ($entity->child->characterTraits as $trait)
+            {
+                name: `{{ $trait->name }}`,
+                entry: `{{ $trait->entry }}`,
+                section_id: {{ $trait->section_id }},
+                section: '{{ $trait->section_id === 1 ? 'appearance' : 'personality' }}',
+            },
+            @endforeach ],
+            races: [@foreach ($entity->child->characterRaces as $race)
+            {
+                id: {{ $race->race->id }},
+                name: `{{ $race->race->name }}`,
+                url: `{{ $race->race->getLink() }}`
+            },
+            @endforeach ],
+            families: [@foreach ($entity->child->characterFamilies as $family)
+            {
+                id: {{ $family->family->id }},
+                name: `{{ $family->family->name }}`,
+                url: `{{ $family->family->getLink() }}`
+            },
+            @endforeach ],
+            @endif
+            @if ($entity->child->location)
+            location: {
+                id: {{ $entity->child->location->id }},
+                name: `{{ $entity->child->location->name }}`,
+                url: `{{ $entity->child->location->getLink() }}`
+            },
+            @endif
+
+            tags: [@foreach ($entity->tags as $tag)
+            {
+                id: {{ $tag->id }},
+                name: `{{ $tag->name }}`,
+                slug: `{{ $tag->slug }}`,
+                url: `{{ $tag->getLink() }}`
+            },
+            @endforeach ],
         }
         const attributeApis = {
             all: {
@@ -61,6 +107,12 @@ if (!isset($entity)) {
             create: {
                 method: 'POST',
                 url: '{{ route('entities.attributes.live-api.create', [$campaign, $entity]) }}'
+            },
+        }
+        const abilityApis = {
+            all: {
+                method: 'GET',
+                url: '{{ route('entities.entity_abilities.api', [$campaign, $entity]) }}'
             },
         }
     </script>
