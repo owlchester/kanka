21fef4bd03cb3a4a9d97c597890a56036bae76b5
ilestis <jay.payne.ch@gmail.com>
2024-09-06 10:14:30 -0600

    Typo

--- lang/en/attribute_templates.php
+++ lang/en/attribute_templates.php
@@ -9,7 +9,7 @@ return [
         'auto_apply'    => 'Auto-apply',
     ],
     'hints'         => [
-        'automatic'                 => 'The following :count attributes where automatically applied from :link.',
+        'automatic'                 => 'The following :count attributes were automatically applied from :link.',
         'automatic_apply'           => '{1} The following :count attribute was automatically applied from :link | [2,] The following :count attributes were automatically applied from :link.',
         'entity_type'               => 'Automatically apply this template\'s attributes to new entities of the selected type.',
         'parent_attribute_template' => 'This attribute template can be a child of another attribute template. When applying this attribute template, it and all of its parents will be applied.',
