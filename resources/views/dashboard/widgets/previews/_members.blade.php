21b9cafc9996c66cd3d964516d9cf35515277a5e
ilestis <jay.payne.ch@gmail.com>
2024-09-09 10:37:36 -0600

    Fix family members on dashboard

--- resources/views/dashboard/widgets/previews/_members.blade.php
+++ resources/views/dashboard/widgets/previews/_members.blade.php
@@ -17,33 +17,32 @@ if (isset($model)) {
     $child = $entity->child;
 }
 $members = $entity->isFamily()
-    ? $child->members()->orderBy('name')->get()
+    ? $child->members()->with('entity')->orderBy('name')->get()
     : $child->members()->with(['character', 'character.entity'])
         ->leftJoin('characters', 'characters.id', '=', 'organisation_member.character_id')
         ->orderBy('characters.name')
         ->get();
 @endphp
 
-<div class="widget-advanced-members">
+<div class="widget-advanced-members mt-5">
 
 @if($entity->isFamily())
-    <div class="grid grid-cols-1 gap-2 members">
-            <?php /** @var \App\Models\CharacterFamily $member */?>
+    <div class="flex flex-col gap-2 members">
+<?php /** @var \App\Models\CharacterFamily $member */?>
         @foreach ($members as $member)
+            @if (empty($member->entity)) @continue @endif
             <div class="">
                 <x-entity-link
-                    :entity="$model->entity"
+                    :entity="$member->entity"
                     :campaign="$campaign" />
             </div>
         @endforeach
     </div>
 @else
     <div class="flex flex-col gap-2 members">
-        <?php /** @var \App\Models\OrganisationMember $member */?>
+<?php /** @var \App\Models\OrganisationMember $member */?>
         @foreach ($members as $member)
-            @if (empty($member->character))
-                @continue
-            @endif
+            @if (empty($member->character)) @continue @endif
             <div class="grid grid-cols-2 gap-2 members" data-role="{{ Illuminate\Support\Str::slug($member->role) }}"  data-status="{{ $member->status_id }}">
                 <div class="font-extrabold">{{ $member->role }}</div>
                 <div>
