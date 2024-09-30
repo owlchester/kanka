810cbd7f63e86fd25f12279c7d1e56cb43ee5fcc
ilestis <jay.payne.ch@gmail.com>
2024-09-30 09:01:33 -0600

    Freestar

--- resources/views/entities/components/menu_v2.blade.php
+++ resources/views/entities/components/menu_v2.blade.php
@@ -1,5 +1,4 @@
-<div class="entity-submenu md:w-52 md:flex-none">
+<div class="entity-submenu md:w-48 md:flex-none">
     @includeWhen(isset($withPins), 'entities.components.pins')
     @include('entities.components.menu')
-    @includeWhen(!isset($withPins), 'ads.siderail_left')
 </div>
