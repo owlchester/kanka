8ffc10189fc40b2a06e23bf58d5546cc2ca5d5d0
ilestis <jay.payne.ch@gmail.com>
2024-11-27 11:24:47 -0600

    BF 2024 banner

--- resources/views/layouts/banner.blade.php
+++ resources/views/layouts/banner.blade.php
@@ -1,4 +1,4 @@
-{{--@if (\Carbon\Carbon::create(2023, 11, 28)->isFuture())--}}
+{{--@if (\Carbon\Carbon::create()->between(\Carbon\Carbon::create('2024-11-27'), \Carbon\Carbon::create('2024-12-02')))--}}
 {{--    <x-tutorial code="ft" type="warning" :auth="false">--}}
 {{--        <p>--}}
 {{--            Kanka will be undergoing scheduled maintenance on Tuesday 21st of May 2024. As a result, Kanka will be unavailable from <a href="https://everytimezone.com/s/bff36b39" target="_blank" style="text-decoration: underline"><i class="fa-solid fa-external-link"></i> 14:30 UTC</a> to 16:00 UTC. Join us on <a href="{{ config('social.discord') }}" target="_blank"  style="text-decoration: underline">Discord</a> to get updates.--}}
@@ -6,15 +6,15 @@
 {{--    </x-tutorial>--}}
 {{--@endif--}}
 
-{{--@if (\Carbon\Carbon::create(2023, 11, 28)->isFuture())--}}
-{{--    <x-tutorial code="banner_bf2024" type="warning">--}}
-{{--        <p>--}}
-{{--            <a href="{{ route('settings.subscription') }}" class="block">--}}
-{{--                {!! __('banners.blackfriday24', ['code' => '<code>BF2024</code>']) !!}--}}
-{{--            </a>--}}
-{{--        </p>--}}
-{{--    </x-tutorial>--}}
-{{--@endif--}}
+@if (auth()->check() && auth()->user()->subscriptions()->count() === 0 && \Carbon\Carbon::now()->between(\Carbon\Carbon::create('2024-11-29'), \Carbon\Carbon::create('2024-12-02')))
+    <x-tutorial code="bf2024" type="warning">
+        <p>
+            <a href="{{ route('settings.subscription') }}" class="block text-warning-content">
+                {!! __('banners.blackfriday24', ['code' => '<code>BF2024</code>']) !!}
+            </a>
+        </p>
+    </x-tutorial>
+@endif
 
 @inject('countryService', 'App\Services\CountryService')
 @if (auth()->check() && $countryService->getCountry() === 'BR')
