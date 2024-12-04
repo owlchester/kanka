1af1bdf63469b9234e245d3162dcd4711e1368a3
ilestis <jay.payne.ch@gmail.com>
2024-12-04 12:39:05 -0600

    Fix timeline element colour

--- resources/views/timelines/elements/_icon.blade.php
+++ resources/views/timelines/elements/_icon.blade.php
@@ -5,12 +5,12 @@ if (!isset($absolute)) {
 $min = $absolute ? 'absolute top-0 text-center w-8 h-8 rounded-full' : 'rounded-full';
 ?>
 
-@if (!empty($element->icon)) 
-    @if (Illuminate\Support\Str::startsWith($element->icon, '<i class=')) 
+@if (!empty($element->icon))
+    @if (Illuminate\Support\Str::startsWith($element->icon, '<i class='))
         {!! str_replace('<i class="', '<i class="bg-' . $element->colour . ' ', $element->icon) !!}
     @else
         <i class="bg-{{ $element->colour }} {{ $element->icon }} {{ $min }}" aria-hidden="true"></i>
-    @endif    
+    @endif
 @else
-    <i class="fa fa-solid fa-hourglass-half bg-' {{ $element->colour }} ' ' {{ $min }} '" aria-hidden="true"></i>
+    <i class="fa fa-solid fa-hourglass-half bg-{{ $element->colour }} {{ $min }}" aria-hidden="true"></i>
 @endif
