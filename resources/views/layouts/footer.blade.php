a552311b83d0ada5bec48cda33fcbb04eeab4dba
ilestis <jay.payne.ch@gmail.com>
2024-12-17 12:48:09 -0600

    End of year footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,10 +2,11 @@
     <div class="lg:max-w-7xl lg:mx-auto">
         <div class="flex flex-col gap-10">
             <div class="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-5">
-                <div class="flex-col gap-8 hidden lg:flex col-span-2 ">
+                <div class="flex-col gap-4 hidden lg:flex col-span-2 ">
                     <a href="{{ route('home') }}" class="logo">
                         @include('icons.kanka-svg')
                     </a>
+
 
                     <div class="flex items-center gap-5 text-3xl flex-wrap">
                         @include('layouts._socials')
@@ -14,7 +15,21 @@
                     @include('layouts._lang-switcher')
 
                     <div class="text-xs">
-                        Kanka v{{ config('app.version') }} - {!! __('footer.copyright', ['copy' => '&copy;', 'year' => date('Y'), 'company' => 'Owlchester SNC'])!!}  - {{ __('footer.server_time', ['time' => \Carbon\Carbon::now()->isoFormat('MMMM Do YYYY, h:mm a')]) }} ({{ gethostname() }})
+                        <p>
+                            {{ __('footer.made') }}
+                        </p>
+                        <p>
+                            {{ __('footer.thanks') }}
+                        </p>
+                        <p>
+                            {!! __('footer.copyright', ['copy' => '&copy;', 'year' => date('Y'), 'company' => 'Owlchester SNC'])!!}
+                        </p>
+                    </div>
+
+                    <div class="text-xs">
+                        <p>
+                            Kanka v{{ config('app.version') }} - <span data-toggle="tooltip" data-title="{{ __('footer.server-time', ['server' => gethostname()]) }}">{{ \Carbon\Carbon::now()->isoFormat('MMMM Do YYYY, h:mm A') }}</span>
+                        </p>
                     </div>
                 </div>
                 <div class="flex flex-col gap-3 text-sm">
@@ -30,7 +45,6 @@
                     <a href="{{ Domain::toFront('kb') }}">{{ __('footer.kb') }}</a>
                     <a href="https://docs.kanka.io/en/latest/index.html" target="_blank">{{ __('footer.documentation') }}</a>
                     <a href="{{ route('larecipe.index') }}" target="_blank">{{ __('front.features.api.link') }}</a>
-                    <a href="https://blog.kanka.io/category/news/" target="_blank">{{ __('footer.whats-new') }}</a>
                     <a href="https://blog.kanka.io" target="_blank">{{ __('footer.blog') }}</a>
                     <a href="https://status.kanka.io" target="_blank">{{ __('footer.status') }}</a>
                     <a href="{{ Domain::toFront('newsletter') }}">{{ __('footer.newsletter') }}</a>
@@ -38,6 +52,7 @@
 
                 <div class="flex flex-col gap-3 text-sm">
                     <span class="block text-nav uppercase">{{ __('footer.community') }}</span>
+                    <a href="https://blog.kanka.io/category/news/" target="_blank">{{ __('footer.whats-new') }}</a>
                     <a href="{{ Domain::toFront('campaigns') }}">{{ __('footer.public-campaigns') }}</a>
                     <a href="{{ route('roadmap') }}">{{ __('footer.roadmap') }}</a>
                     <a href="{{ Domain::toFront('hall-of-fame') }}">{{ __('front/hall-of-fame.title') }}</a>
@@ -47,10 +62,10 @@
                     <span class="block text-nav uppercase">{{ __('footer.company') }}</span>
                     <a href="{{ Domain::toFront('about') }}">{{ __('footer.about') }}</a>
                     <a href="{{ Domain::toFront('contact') }}">{{ __('footer.contact') }}</a>
-                    <a href="{{ Domain::toFront('press-kit') }}">{{ __('footer.press-kit') }}</a>
-                    <a href="{{ Domain::toFront('security') }}">{{ __('footer.security') }}</a>
                     <a href="{{ Domain::toFront('privacy-policy') }}">{{ __('footer.privacy') }}</a>
                     <a href="{{ Domain::toFront('terms-and-conditions') }}">{{ __('footer.terms') }}</a>
+                    <a href="{{ Domain::toFront('security') }}">{{ __('footer.security') }}</a>
+                    <a href="{{ Domain::toFront('press-kit') }}">{{ __('footer.press-kit') }}</a>
                 </div>
             </div>
             <div class="lg:hidden flex flex-col gap-5 text-center">
@@ -92,7 +107,7 @@
             </li>
             <li class="py-2">
                 <a rel="alternate" hreflang="en" href="{{ $currentUrl . 'en' }}">
-                    English
+                    UK English
                 </a>
             </li>
             <li class="py-2">
