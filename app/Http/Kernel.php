49361f47298b225ca150e0e59d71c84b6d3454c7
ilestis <jay.payne.ch@gmail.com>
2024-12-19 11:15:57 -0600

    Remove AuthenticateSession

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -34,7 +34,7 @@ class Kernel extends HttpKernel
             Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
-            \Illuminate\Session\Middleware\AuthenticateSession::class,
+            //\Illuminate\Session\Middleware\AuthenticateSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
--- config/session.php
+++ config/session.php
@@ -192,6 +192,6 @@ return [
     |
     */
 
-    'same_site' => env('SESSION_SAME_SITE', 'strict'),
+    'same_site' => env('SESSION_SAME_SITE', 'lax'),
 
 ];
