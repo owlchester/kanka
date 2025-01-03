8772d24e84f39fb37606421c1707ff1b38b78549
ilestis <jay.payne.ch@gmail.com>
2025-01-03 10:16:31 -0600

    Map tweaks

--- resources/js/gallery/Selection.vue
+++ resources/js/gallery/Selection.vue
@@ -313,7 +313,7 @@ const clickOutside = (event) => {
     let isInDialog = (rect.top <= event.clientY && event.clientY <= rect.top + rect.height &&
         rect.left <= event.clientX && event.clientX <= rect.left + rect.width)
     if (!isInDialog && event.target.tagName === 'DIALOG') {
-        closeModal(event.target)
+        closeDialog(event.target)
     }
 }
 
