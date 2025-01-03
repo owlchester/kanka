d369a0fd16474879b22be1ab51d6015947a012b9
ilestis <jay.payne.ch@gmail.com>
2025-01-02 18:23:16 -0600

    Move actions cog to button

--- resources/views/entities/headers/cog.blade.php
+++ resources/views/entities/headers/cog.blade.php
@@ -3,6 +3,10 @@
  * @var \App\Models\MiscModel $model
  * @var \App\Models\Campaign $campaign
  * */?>
+@if (auth()->guest() || auth()->user()->created_at->isAfter(\Carbon\Carbon::create('2025-01-01')))
+    <?php return; ?>
+@endif
+
 <div class="dropdown entity-actions flex items-center">
     <div role="button" tabindex="0" aria-expanded="false" class="cursor-pointer" data-pulse=".entity-actions-button" data-content="{{ __('entities/actions.new-action-button') }}" data-placement="top">
         <span class="sr-only">{{ __('entities/permissions.quick.screen-reader') }}</span>
