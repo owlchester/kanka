0121296c1d86225ef50ff551f51a6465cf75a0b0
ilestis <jay.payne.ch@gmail.com>
2025-01-05 13:01:40 -0600

    Quick creator missing locations field

--- resources/views/entities/creator/forms/organisation.blade.php
+++ resources/views/entities/creator/forms/organisation.blade.php
@@ -1,7 +1,5 @@
 <x-grid>
     @include('cruds.fields.type', ['base' => \App\Models\Organisation::class, 'trans' => 'organisations'])
-
     @include('cruds.fields.organisation', ['isParent' => true])
-
-    @include('cruds.fields.location')
+    @include('cruds.fields.locations')
 </x-grid>
