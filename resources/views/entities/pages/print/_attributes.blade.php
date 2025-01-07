8d8520dcc90b6a0de6160ee4aef900d7b29ac9a2
ilestis <jay.payne.ch@gmail.com>
2025-01-07 08:39:03 -0600

    Fix issues in export/print

--- resources/views/entities/pages/print/_attributes.blade.php
+++ resources/views/entities/pages/print/_attributes.blade.php
@@ -1,4 +1,4 @@
-<div class="print-box-attributes p-4">
+<div class="print-box-attributes">
 
     <h2>{{ __('crud.tabs.attributes') }}</h2>
     @include('entities.pages.attributes.render', [
--- resources/views/entities/pages/print/_relations.blade.php
+++ resources/views/entities/pages/print/_relations.blade.php
@@ -8,7 +8,7 @@ $rows = $entity
     ->paginate()
     ->withPath(route('entities.relations_table', [$campaign, $entity, 'mode' => 'table']));
 @endphp
-<div class="print-box-relations p-4">
+<div class="print-box-relations">
     <h2>{{ __('crud.tabs.connections') }}</h2>
 
     <div id="datagrid-parent" class="table-responsive">
