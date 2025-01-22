6cdca39908a141d77050ed078ed4a264aeff2a26
ilestis <jay.payne.ch@gmail.com>
2025-01-22 15:41:10 -0600

    Purify

--- config/purify.php
+++ config/purify.php
@@ -64,7 +64,7 @@ return [
                 . 'div[class|style|id|align|role|title],'
                 . 'p[class|style|id|dir|align],'
                 . 'span[class|style|id|dir],'
-                . 'a[href|class|style|target|rel|title|data-toggle|data-html|id|role],'
+                . 'a[href|class|style|target|rel|title|data-toggle|data-html|id|role|data-dropdown|data-pulse|data-animate],'
                 . 'br[class|style],'
                 . 'i[class],u[class],'
                 . 'img[src|style|alt|width|height|class|title|id|data-gallery-id],'
