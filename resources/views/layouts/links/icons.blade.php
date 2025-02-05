50ee9cf5815d5ee6b696cf5ece1b94cf3a4c37bd
Spitfire <itza305@gmail.com>
2025-02-05 05:32:45 -0600

    Updated apple icons

--- resources/views/layouts/links/icons.blade.php
+++ resources/views/layouts/links/icons.blade.php
@@ -1,6 +1,8 @@
 <link rel="shortcut icon" href="/images/favicon/favicon.ico" type="image/x-icon" />
 <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon/favicon-32x32.png">
 <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon/favicon-16x16.png">
+<link rel="icon" type="image/png" href="/images/favicon/favicon-96x96.png" sizes="96x96" />
+<link rel="icon" type="image/svg+xml" href="/images/favicon/favicon.svg" />
 
 <link rel="apple-touch-icon" href="/images/favicon/apple-touch-icon.png" />
 <link rel="apple-touch-icon" sizes="57x57" href="/images/favicon/apple-touch-icon-57x57.png" />
@@ -10,6 +12,11 @@
 <link rel="apple-touch-icon" sizes="120x120" href="/images/favicon/apple-touch-icon-120x120.png" />
 <link rel="apple-touch-icon" sizes="144x144" href="/images/favicon/apple-touch-icon-144x144.png" />
 <link rel="apple-touch-icon" sizes="152x152" href="/images/favicon/apple-touch-icon-152x152.png" />
-<link rel="apple-touch-icon" sizes="180x180" href="/images/favicon/apple-touch-icon-180x180.png" />
+<link rel="apple-touch-icon" sizes="180x180" href="/images/favicon/apple-touch-icon.png" />
+<link rel="shortcut icon" href="/images/favicon/favicon.ico" />
+<link rel="manifest" href="/images/favicon/site.webmanifest" />
 
-@if (isset($campaign))<meta name="apple-mobile-web-app-title" content="{{ $campaign->name }}">@endif<meta name="apple-mobile-web-app-status-bar-style" content="#40479e">
+
+
+@if (isset($campaign))<meta name="apple-mobile-web-app-title" content="{{ $campaign->name }}">@else<meta name="apple-mobile-web-app-title" content="Kanka" />@endif
+<meta name="apple-mobile-web-app-status-bar-style" content="#40479e">
