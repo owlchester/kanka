413f0e6cc7cce986d552424380be1ae93f8099a6
ilestis <jay.payne.ch@gmail.com>
2025-02-10 16:32:20 -0600

    Fix and UI tweak

--- resources/views/entities/creator/forms/race.blade.php
+++ resources/views/entities/creator/forms/race.blade.php
@@ -1,5 +1,5 @@
 <x-grid>
     @include('cruds.fields.type', ['base' => \App\Models\Race::class, 'trans' => 'races'])
     @include('cruds.fields.race', ['isParent' => true, 'dynamicNew' => true])
-    @include('cruds.fields.locations', ['dynamicNew' => auth()->user()->can('create', [$campaign->getEntityTypes()->where('id', config('entities.ids.location'))->first(), $campaign])]])
+    @include('cruds.fields.locations', ['dynamicNew' => auth()->user()->can('create', [$campaign->getEntityTypes()->where('id', config('entities.ids.location'))->first(), $campaign])])
 </x-grid>
