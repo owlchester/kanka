dce340bcd76891a6775452509378bbc3a698aa68
ilestis <jay.payne.ch@gmail.com>
2025-03-03 16:52:36 -0600

    Backup config tweaks

--- config/backup.php
+++ config/backup.php
@@ -259,7 +259,7 @@ return [
              * After cleaning up the backups remove the oldest backup until
              * this amount of megabytes has been reached.
              */
-            'delete_oldest_backups_when_using_more_megabytes_than' => 10000,
+            'delete_oldest_backups_when_using_more_megabytes_than' => 50000,
         ],
     ],
 
