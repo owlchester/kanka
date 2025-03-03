f8867c463e02f4d15ef3c8f37c4afb992905cfe9
ilestis <jay.payne.ch@gmail.com>
2025-03-03 12:17:12 -0600

    New backup config

--- config/backup.php
+++ config/backup.php
@@ -197,7 +197,7 @@ return [
     'monitor_backups' => [
         [
             'name' => env('APP_NAME', 'laravel-backup'),
-            'disks' => ['local'],
+            'disks' => [ env('BACKUP_DISK', 'local')],
             'health_checks' => [
                 Spatie\Backup\Tasks\Monitor\HealthChecks\MaximumAgeInDays::class => 1,
                 Spatie\Backup\Tasks\Monitor\HealthChecks\MaximumStorageInMegabytes::class => 5000,
