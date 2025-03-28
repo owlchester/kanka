21f4d9b0a0bd164714fb50b7044f5219297f4b96
Spitfire <itza305@gmail.com>
2025-03-28 08:37:35 -0600

    Removed ugly gray line

--- resources/views/vendor/mail/html/layout.blade.php
+++ resources/views/vendor/mail/html/layout.blade.php
@@ -40,8 +40,6 @@ h1, h2, h3, h4, h5, p { margin:0;}
 <td class="" background="" bgcolor="#F4F7FA" align="center" valign="top" style="padding: 0 8px;">
 <table width="640" class="wrapper" align="center" border="0" cellpadding="0" cellspacing="0" style="
 max-width: 640px;
-border:1px solid #EAECED;
-border-radius:8px; border-collapse: separate!important; overflow: hidden;
 ">
 <tbody>
 <tr>
