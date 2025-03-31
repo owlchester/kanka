a460b364292a4b590f974e201ca2a0e0b0cad08f
ilestis <jay.payne.ch@gmail.com>
2025-03-31 17:35:59 -0600

    Pricing wip

--- resources/views/settings/subscription/tiers/actions/_kobold.blade.php
+++ resources/views/settings/subscription/tiers/actions/_kobold.blade.php
@@ -1,11 +1,11 @@
 <?php /** @var \App\Models\TierPrice $current */?>
 @if (!$user->hasPayPal())
     @if(empty($current))
-        <span class="btn2 btn-block btn-sm">
+        <span class="btn2 btn-block">
             {{ __('tiers.current') }}
         </span>
     @else
-        <a class="btn2 btn-block btn-sm btn-error " data-toggle="dialog" data-target="subscribe-confirm" data-url="{{ route('settings.subscription.change', ['tier' => 1]) }}">
+        <a class="btn2 btn-block btn-error " data-toggle="dialog" data-target="subscribe-confirm" data-url="{{ route('settings.subscription.change', ['tier' => 1]) }}">
             {{ __('settings.subscription.subscription.actions.cancel') }}
         </a>
     @endif
--- resources/views/settings/subscription/tiers/actions/_wyvern.blade.php
+++ resources/views/settings/subscription/tiers/actions/_wyvern.blade.php
@@ -6,32 +6,32 @@
 @endphp
 @if ($user->hasPayPal())
     @if (!$user->isWyvern() && !$user->isElemental())
-        <a class="btn2 btn-block btn-sm btn-primary" data-toggle="dialog" data-target="subscribe-confirm" data-url="{{ route('settings.subscription.change', ['tier' => $tier, 'period' => 'yearly']) }}">
+        <a class="btn2 btn-block btn-primary" data-toggle="dialog" data-target="subscribe-confirm" data-url="{{ route('settings.subscription.change', ['tier' => $tier, 'period' => 'yearly']) }}">
             {{ __('tiers.actions.subscribe.choose', ['tier' => $tier->name]) }}
         </a>
     @endif
 @else
     @if($user->subscribedToPrice($tier->monthlyPlans(), 'kanka'))
-        <a class="btn2 btn-block btn-sm disabled">
+        <a class="btn2 btn-block disabled">
             {{ __('tiers.current') }}
         </a>
     @elseif ($user->subscribedToPrice($tier->plans(), 'kanka'))
-        <a class="btn2 btn-block btn-sm disabled">
+        <a class="btn2 btn-block disabled">
             {{ __('settings.subscription.subscription.actions.downgrading') }}
         </a>
     @else
-        <a class="btn2 btn-block btn-sm btn-primary price-monthly" data-toggle="dialog" data-target="subscribe-confirm" data-url="{{ route('settings.subscription.change', ['tier' => $tier, 'period' => 'monthly']) }}">
+        <a class="btn2 btn-block btn-primary price-monthly" data-toggle="dialog" data-target="subscribe-confirm" data-url="{{ route('settings.subscription.change', ['tier' => $tier, 'period' => 'monthly']) }}">
             {{ __('tiers.actions.subscribe.choose', ['tier' => $tier->name]) }}
         </a>
     @endif
 
 
     @if($user->subscribedToPrice($tier->yearlyPlans(), 'kanka'))
-        <a class="btn2 btn-block btn-sm disabled">
+        <a class="btn2 btn-block disabled">
             {{ __('tiers.current') }}
         </a>
     @else
-        <a class="btn2 btn-block btn-sm btn-primary price-yearly" data-toggle="dialog" data-target="subscribe-confirm" data-url="{{ route('settings.subscription.change', ['tier' => $tier, 'period' => 'yearly']) }}">
+        <a class="btn2 btn-block btn-primary price-yearly" data-toggle="dialog" data-target="subscribe-confirm" data-url="{{ route('settings.subscription.change', ['tier' => $tier, 'period' => 'yearly']) }}">
             {{ __('tiers.actions.subscribe.choose', ['tier' => $tier->name]) }}
         </a>
     @endif
