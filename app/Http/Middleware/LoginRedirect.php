f5f0a3e8fc679c65266d62b00176d574f5ef029b
spitfire305 <72818019+spitfire305@users.noreply.github.com>
2025-03-31 20:41:51 +0000

    Apply automatic changes

--- app/Http/Middleware/LoginRedirect.php
+++ app/Http/Middleware/LoginRedirect.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Middleware;
 
-use Symfony\Component\HttpFoundation\Response;
-use Illuminate\Http\Request;
 use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class LoginRedirect
 {
@@ -12,14 +12,14 @@ class LoginRedirect
     {
         // Store the previous URL in session only if it's not the login route itself and is a valid redirect url
         if ($request->is('login') || $request->is('register')) {
-            //If to check where the request is coming from and set the variable if its a valid route.
+            // If to check where the request is coming from and set the variable if its a valid route.
             if (url()->previous() === route('roadmap')) {
                 session(['login_redirect' => url()->previous()]);
-            } elseif (!(url()->previous() === route('login')) && !(url()->previous() === route('register'))) {
-                //If the user moves outside the login page, reset the redirect variable.
+            } elseif (! (url()->previous() === route('login')) && ! (url()->previous() === route('register'))) {
+                // If the user moves outside the login page, reset the redirect variable.
                 session(['login_redirect' => null]);
             }
-        } 
+        }
 
         return $next($request);
     }
