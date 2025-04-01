ed4016ef058e2a384189d09beeca3f05f86fc5d5
Spitfire <itza305@gmail.com>
2025-03-31 19:17:21 -0600

    Moved to param

--- app/Http/Middleware/LoginRedirect.php
+++ app/Http/Middleware/LoginRedirect.php
@@ -10,12 +10,15 @@ class LoginRedirect
 {
     public function handle(Request $request, Closure $next): Response
     {
+        $whitelist = ['roadmap'];
+
         // Store the previous URL in session only if it's not the login route itself and is a valid redirect url
         if ($request->is('login') || $request->is('register')) {
             // If to check where the request is coming from and set the variable if its a valid route.
-            if (url()->previous() === route('roadmap')) {
-                session(['login_redirect' => url()->previous()]);
-            } elseif (! (url()->previous() === route('login')) && ! (url()->previous() === route('register'))) {
+            if ($request->has('next') && in_array($request->get('next'), $whitelist)) {
+                session(['login_redirect' => route($request->get('next'))]);
+
+            } elseif (! (strtok(url()->previous(), '?') === route('login')) && ! (strtok(url()->previous(), '?') === route('register'))) {
                 // If the user moves outside the login page, reset the redirect variable.
                 session(['login_redirect' => null]);
             }
