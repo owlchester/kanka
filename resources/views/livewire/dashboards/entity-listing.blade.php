21ff56c8d4a3b9c71db4080a821c7b46c48e7d5b
ilestis <jay.payne.ch@gmail.com>
2025-04-10 15:46:03 -0600

    Add completed quest info

--- resources/views/livewire/dashboards/entity-listing.blade.php
+++ resources/views/livewire/dashboards/entity-listing.blade.php
@@ -2,23 +2,30 @@
 <?php /** @var \App\Models\Entity[]|\Illuminate\Pagination\LengthAwarePaginator $entities */?>
 <div class="flex flex-col gap-2">
 @foreach ($entities as $entity)
-    <div class="flex items-center gap-2" data-entity-type="{{ $entity->entityType->pluralCode() }}">
-        <a class="entity-picture inline-block rounded-full cover-background w-9 h-9 flex-shrink-0" style="background-image: url('{{ Avatar::entity($entity)->fallback()->size(80)->thumbnail() }}');"
-            title="{{ $entity->name }}"
-            href="{{ $entity->url() }}">
-        </a>
+    <div class="flex items-center gap-2 justify-between" data-entity-type="{{ $entity->entityType->pluralCode() }}">
+        <div class="flex items-center gap-2">
+            <a class="entity-picture inline-block rounded-full cover-background w-9 h-9 flex-shrink-0" style="background-image: url('{{ Avatar::entity($entity)->fallback()->size(80)->thumbnail() }}');"
+                title="{{ $entity->name }}"
+                href="{{ $entity->url() }}">
+            </a>
 
-        <div class="grow break-all truncate">
-            <x-entity-link
-                :entity="$entity"
-                :campaign="$campaign" />
+            <div class="break-all truncate">
+                <x-entity-link
+                    :entity="$entity"
+                    :campaign="$campaign" />
 
-            @if ($entity->is_private)
-                <x-icon class="lock" tooltip title="{{ __('crud.is_private') }}" />
-            @endif
+                @if ($widget->entityType?->id === config('entities.ids.quest'))
+                    @if ($entity->quest->isCompleted())
+                        <x-icon class="fa-regular fa-check-circle" tooltip :title="__('quests.fields.is_completed')" />
+                    @endif
+                @endif
+                @if ($entity->is_private)
+                    <x-icon class="lock" tooltip title="{{ __('crud.is_private') }}" />
+                @endif
+            </div>
         </div>
 
-        <div class="blame flex-none text-right text-xs">
+        <div class="blame flex flex-col gap-0.5 text-xs items-end">
             <span class="author block">
                 @if (empty($entity->updated_by) && !empty($entity->created_by))
                     {{ \App\Facades\UserCache::name($entity->created_by) }}
