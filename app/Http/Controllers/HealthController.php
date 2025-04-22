7ed1ee049503a836d71a5c35ed503a993a282276
ilestis <jay.payne.ch@gmail.com>
2025-04-22 09:26:51 -0600

    Add health page

---
+++ app/Http/Controllers/HealthController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Events\DiagnosingHealth;
+use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\View;
+
+class HealthController extends Controller
+{
+    public function index()
+    {
+        $exception = null;
+
+        try {
+            Event::dispatch(new DiagnosingHealth);
+        } catch (\Throwable $e) {
+            if (app()->hasDebugModeEnabled()) {
+                throw $e;
+            }
+
+            report($e);
+
+            $exception = $e->getMessage();
+        }
+
+        return response()->view('health', [
+            'exception' => $exception,
+        ], status: $exception ? 500 : 200);
+    }
+}
