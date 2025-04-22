0cb1abbe37259d5034447aa2e74498c9b74cf82b
ilestis <jay.payne.ch@gmail.com>
2025-04-22 17:04:40 -0600

    Premium icon

--- lang/en/campaigns/logs.php
+++ lang/en/campaigns/logs.php
@@ -5,8 +5,8 @@ return [
         'helper' => 'Premium features are needed to view logs older than :amount days old.',
     ],
     'helpers' => [
-        'list' => 'Kanka keeps a log of all main changes made to the campaign for up to :amount days. These logs don\'t detail individual changes to values, but rather the overall state of the campaign.',
+        'list' => 'We keeps a log of all main changes made to the campaign for up to :amount days. These logs don\'t detail individual changes to values, but rather the overall state of the campaign.',
         'title' => 'No logs',
-        'nothing' => 'There are no logs to show. Please keep in mind that logs are only available for the last :amount days.',
+        'nothing' => 'There are no logs to show. Please keep in mind that logs are only kept for up to :amount days.',
     ],
 ];
