4769bc3d8f7c719196d52ee4ac78875033333df8
Spitfire <itza305@gmail.com>
2025-04-23 13:43:50 -0600

    Implemented requested changes

--- lang/en/attribute_templates.php
+++ lang/en/attribute_templates.php
@@ -15,6 +15,7 @@ return [
         'entity_type'               => 'Automatically apply this template\'s attributes to new entities of the selected type.',
         'parent_attribute_template' => 'This attribute template can be a child of another attribute template. When applying this attribute template, it and all of its parents will be applied.',
         'is_enabled'                => 'Enable this template for use when creating an entity.',
+        'is_disabled'               => 'This template is disabled.'
     ],
     'placeholders'  => [
         'name'  => 'Name of the Attribute Template',
