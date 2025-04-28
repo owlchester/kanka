457dad2e141dc44349ecca5ef58da294d2b64cee
ilestis <jay.payne.ch@gmail.com>
2025-04-28 17:00:08 -0600

    Refactor UserEventSubscriber to be cleaner

--- app/Listeners/UserEventSubscriber.php
+++ app/Listeners/UserEventSubscriber.php
@@ -4,17 +4,18 @@ namespace App\Listeners;
 
 use App\Jobs\Emails\MailSettingsChangeJob;
 use App\Models\User;
-use App\Models\UserFlag;
 use App\Models\UserLog;
+use App\Services\Auth\LoginService;
+use App\Services\Auth\SessionService;
 use App\Services\InviteService;
 use App\Services\StarterService;
 use App\Services\Users\CampaignService;
-use Carbon\Carbon;
 use Exception;
 use Illuminate\Auth\Events\Login;
 use Illuminate\Auth\Events\Logout;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Events\Dispatcher;
+use Illuminate\Support\Facades\Log;
 
 /**
  * @property User $user
@@ -33,14 +34,10 @@ class UserEventSubscriber
      * @return void
      */
     public function __construct(
-        InviteService $inviteService,
-        StarterService $starterService,
-        CampaignService $campaignService
-    ) {
-        $this->inviteService = $inviteService;
-        $this->starterService = $starterService;
-        $this->campaignService = $campaignService;
-    }
+        protected LoginService $loginService,
+        protected SessionService $sessionService,
+    )
+    {}
 
     /**
      * Handle user login events.
@@ -51,60 +48,30 @@ class UserEventSubscriber
         $user = $event->user;
         // Log the user's login
         if (! $user) {
-            dd('Error OSL-010');
+            Log::error('Missing user in login event');
+            return false;
         }
 
-        $action = auth()->viaRemember() ? UserLog::TYPE_AUTOLOGIN : UserLog::TYPE_LOGIN;
-        $userLogType = session()->get('kanka.userLog', $action);
-        if ($user->isBanned()) {
-            $userLogType = session()->get('kanka.userLog', UserLog::TYPE_BANNED_LOGIN);
-        }
-        $user->log($userLogType);
-
-        session()->remove('kanka.userLog');
-        $user->updateQuietly(['last_login_at' => Carbon::now()]);
-
-        // Delete any flags to auto-delete the account based on inactivity
-        UserFlag::where('user_id', $user->id)
-            ->whereIn('flag', [UserFlag::FLAG_INACTIVE_1, UserFlag::FLAG_INACTIVE_2])
-            ->delete();
+        $this->loginService
+            ->user($user)
+            ->logUserActivity()
+            ->updateLastLoginTime()
+            ->clearInactivityFlag();
 
         // Update mailerlite for the login stuff
         if (! session()->has('first_login') && $user->hasNewsletter()) {
             MailSettingsChangeJob::dispatch($user);
         }
 
-        // Does the user have a join campaign token?
-        if (session()->has('invite_token')) {
-            try {
-                $campaign = $this->inviteService
-                    ->user($user)
-                    ->useToken(session()->get('invite_token'));
-                $this->campaignService
-                    ->user($user)
-                    ->campaign($campaign)
-                    ->set();
+        // Process invite token or first login
+        $inviteResult = $this->sessionService->user($user)->handleInviteToken();
+        if ($inviteResult !== null) {
+            return $inviteResult;
+        }
 
-                return true;
-            } catch (Exception $e) {
-                // Silence errors here
-            }
-        } elseif (session()->has('first_login')) {
-            // Todo: move this to the controller? Not sure why it should be an event's responsibility
-            // Let's create their first campaign for them
-            try {
-                $campaign = $this->starterService
-                    ->user($user)
-                    ->create();
-                session()->remove('first_login');
-                $this->campaignService
-                    ->user($user)
-                    ->campaign($campaign)
-                    ->set();
-
-                return true;
-            } catch (Exception $e) {
-            }
+        $firstLoginResult = $this->sessionService->user($user)->handleFirstLogin();
+        if ($firstLoginResult !== null) {
+            return $firstLoginResult;
         }
 
         return true;
---
+++ app/Services/Auth/SessionService.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Services\Auth;
+
+use App\Services\Users\CampaignService;
+use App\Services\InviteService;
+use App\Services\StarterService;
+use App\Traits\UserAware;
+use Exception;
+
+class SessionService
+{
+    use UserAware;
+
+    public function __construct(
+        protected InviteService $inviteService,
+        protected StarterService $starterService,
+        protected CampaignService $campaignService
+    ) {}
+
+    public function handleInviteToken(): ?bool
+    {
+        // Does the user have a join campaign token?
+        if (!session()->has('invite_token')) {
+            return null;
+        }
+        try {
+            $campaign = $this->inviteService
+                ->user($this->user)
+                ->useToken(session()->get('invite_token'));
+            $this->campaignService
+                ->user($this->user)
+                ->campaign($campaign)
+                ->set();
+
+            return true;
+        } catch (Exception $e) {
+            // Silence errors here
+            return false;
+        }
+    }
+
+    public function handleFirstLogin(): ?bool
+    {
+        if (!session()->has('first_login')) {
+            return null;
+        }
+        try {
+            $campaign = $this->starterService
+                ->user($this->user)
+                ->create();
+            session()->remove('first_login');
+            $this->campaignService
+                ->user($this->user)
+                ->campaign($campaign)
+                ->set();
+
+            return true;
+        } catch (Exception $e) {
+            // Log exception or handle it properly
+            return false;
+        }
+
+    }
+}
