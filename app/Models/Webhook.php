5fcbd8c63e8bfbce0d9325101dd83af24854282f
Spitfire <itza305@gmail.com>
2025-05-29 10:37:18 -0600

    Webhooks now properly fill updated_at and created_at

--- app/Models/Webhook.php
+++ app/Models/Webhook.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use App\Enums\WebhookAction;
 use App\Facades\CampaignLocalization;
+use App\Models\Concerns\Blameable;
 use App\Models\Concerns\HasCampaign;
 use App\Models\Concerns\Paginatable;
 use App\Models\Concerns\SortableTrait;
@@ -23,6 +24,7 @@ use Illuminate\Support\Arr;
  */
 class Webhook extends Model
 {
+    use Blameable;
     use HasCampaign;
     use Paginatable;
     use SortableTrait;
