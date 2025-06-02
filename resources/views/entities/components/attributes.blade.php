85ee46a33ef8759c146407c3e05607fd3be51cdc
ilestis <jay.payne.ch@gmail.com>
2025-06-02 16:03:54 -0600

    Fix live-edit on pinned attributes with links inside

--- resources/views/entities/components/attributes.blade.php
+++ resources/views/entities/components/attributes.blade.php
@@ -1,6 +1,7 @@
 <?php
 /**
  * @var \App\Models\Attribute $attribute
+ * @var \App\Models\Entity $entity
  */
 $attributes = $entity->starredAttributes();
 ?>
@@ -24,7 +25,7 @@ $attributes = $entity->starredAttributes();
                 <p class="m-0 grow w-full live-edit @if (trim($attribute->value) === '') empty-value @endif" data-id="{{ $attribute->id }}">
                     {!! nl2br($attribute->mappedValue()) !!}
                 </p>
-            @elseif (!$attribute->isCheckbox() && !$attribute->isSection())
+            @elseif (!$attribute->isSection())
                 <p class="live-edit @if (trim($attribute->value) === '') empty-value @endif text-right grow m-0 inline-block" data-id="{{ $attribute->id }}">
                     {!! $attribute->mappedValue() !!}
                 </p>
