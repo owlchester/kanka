f473bd5ee5c45931463024e40250957787404931
ilestis <jay.payne.ch@gmail.com>
2025-06-02 12:03:26 -0600

    Translation cleanup

--- lang/fr/attribute_templates.php
+++ lang/fr/attribute_templates.php
@@ -1,15 +1,15 @@
 <?php
 
 return [
-    'create'                => [
+    'create'        => [
         'title' => 'Créer un nouveau Modèle d\'attribut',
     ],
-    'fields'                => [
+    'fields'        => [
         'attributes'    => 'Attributs',
         'auto_apply'    => 'Auto-appliquer',
         'is_enabled'    => 'Activé',
     ],
-    'hints'                 => [
+    'hints'         => [
         'automatic'                 => 'Attributs automatiquement appliqués depuis le modèle :link.',
         'automatic_apply'           => '1} L\'attribut suivant a été automatiquement appliqué pour :link | [2,] Les :count attributs suivants ont été automatiquement appliqués pour :link.',
         'entity_type'               => 'Si défini, lors de la création d\'une nouvelle entité de ce type, ce modèle d\'attribut ainsi que ses parents seront automatiquement appliqués.',
@@ -17,10 +17,10 @@ return [
         'is_enabled'                => 'Activer ce modèle pour l\'utiliser dans la campagne.',
         'parent_attribute_template' => 'Ce modèle d\'attribut peut être l\'enfant d\'un autre modèle d\'attribut. Lorsqu\'un modèle d\'attribut est appliqué, celui-ci ainsi que tous ses descendants seront aussi appliqués.',
     ],
-    'placeholders'          => [
+    'placeholders'  => [
         'name'  => 'Nom du modèle d\'attribut',
     ],
-    'show'                  => [
+    'show'          => [
         'tabs'  => [
             'attributes'    => 'Attributs',
         ],
