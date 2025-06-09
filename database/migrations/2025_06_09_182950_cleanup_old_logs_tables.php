3908ba0fe79109863257dc5af23509d82c1d1731
ilestis <jay.payne.ch@gmail.com>
2025-06-09 12:42:10 -0600

    Fix counts sometimes ignoring permissions

---
+++ database/migrations/2025_06_09_182950_cleanup_old_logs_tables.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        $tables = ['admin_logs', 'job_logs', 'user_logs'];
+        foreach ($tables as $tablename) {
+            Schema::dropIfExists($tablename);
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
