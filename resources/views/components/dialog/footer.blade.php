37574bed373ea6f7ef6a81ac1d8472086e18d01d
ilestis <jay.payne.ch@gmail.com>
2025-06-13 09:10:34 -0600

    Fix some modals have multiple close buttons

--- resources/views/components/dialog/footer.blade.php
+++ resources/views/components/dialog/footer.blade.php
@@ -1,20 +1,8 @@
 <div class="mt-5 flex gap-2 md:gap-5 text-left w-full justify-between p-4 md:p-6">
-    <div class="">
-        @if (request()->ajax() || $modal || $dialog)
-            @if ($modal)
-                <button type="button" class="btn2 btn-ghost btn-full" data-dismiss="modal">
-                    {{ __('crud.cancel') }}
-                </button>
-            @else
-                <button type="button" class="btn2 btn-ghost btn-full" onclick="this.closest('dialog').close('close')">
-                    {{ __('crud.cancel') }}
-                </button>
-            @endif
-        @else
-            <a href="{{ (!empty($cancel) ? $cancel : url()->previous()) }}" class="btn2 btn-ghost">
-                {{ __('crud.cancel') }}
-            </a>
-        @endif
-    </div>
+    <menu class="flex flex-wrap gap-3 ps-0 ms-0">
+        <button type="button" class="btn2 btn-outline" onclick="this.closest('dialog').close('close')">
+            {{ __('crud.cancel') }}
+        </button>
+    </menu>
     {!! $slot !!}
 </div>
--- resources/views/cruds/datagrids/bulks/modals/_batch-footer.blade.php
+++ resources/views/cruds/datagrids/bulks/modals/_batch-footer.blade.php
@@ -1,8 +1,3 @@
-<menu class="flex flex-wrap gap-3 ps-0 ms-0">
-    <button autofocus type="button" class="btn2 btn-ghost btn-full" onclick="this.closest('dialog').close('close')">
-        {{ __('crud.cancel') }}
-    </button>
-</menu>
 <menu class="flex flex-wrap gap-3 ps-0">
     <button class="btn2 btn-primary" type="submit">
         <x-icon class="save" />
