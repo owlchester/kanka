a4e054ac360ecd41f5b638992457b5a99adbdd1c
ilestis <jay.payne.ch@gmail.com>
2025-06-15 13:39:43 -0600

    Allow removing custom module parent and other fixes

--- resources/views/entities/components/og.blade.php
+++ resources/views/entities/components/og.blade.php
@@ -1,3 +1,4 @@
+@php /** @var \App\Models\Entity $entity */ @endphp
 @section('og')
     @if ($tooltip = $entity->mappedPreview())<meta property="og:description" content="{{ $tooltip }}" />@endif
     @if ($entity->hasImage())<meta property="og:image" content="{{ \App\Facades\Avatar::entity($entity)->size(276)->thumbnail()  }}" />@endif
