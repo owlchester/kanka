89c3b53bcc09b65233b12e0734cba34b7b0cf3a3
spitfire305 <72818019+spitfire305@users.noreply.github.com>
2025-06-25 21:27:07 +0000

    Fix styling

--- app/Events/Campaigns/EntityTypes/EntityTypeToggled.php
+++ app/Events/Campaigns/EntityTypes/EntityTypeToggled.php
@@ -2,8 +2,8 @@
 
 namespace App\Events\Campaigns\EntityTypes;
 
-use App\Models\User;
 use App\Models\EntityType;
+use App\Models\User;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Foundation\Events\Dispatchable;
