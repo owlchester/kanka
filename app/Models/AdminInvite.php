9837d5ddfa58f62f0b72528a8714cec63082cc49
Spitfire <itza305@gmail.com>
2025-06-27 16:45:05 -0600

    PHPSTAN

--- app/Models/AdminInvite.php
+++ app/Models/AdminInvite.php
@@ -8,7 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 /**
- * @property int id
  * @property string $token
  * @property int $campaign_id
  * @property int $created_by
