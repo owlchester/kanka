1bf3bdc8cc805a99e77b51356daf9e7f01b7931a
ilestis <jay.payne.ch@gmail.com>
2025-07-09 16:54:04 -0600

    Filesystem config for new marketplace storage on prod

--- config/filesystems.php
+++ config/filesystems.php
@@ -94,15 +94,13 @@ return [
         ],
 
         's3-marketplace' => [
-            'driver' => 's3',
-            'key' => env('AWS_ACCESS_KEY_ID'),
-            'secret' => env('AWS_SECRET_ACCESS_KEY'),
-            'region' => env('AWS_DEFAULT_REGION'),
+            'driver' => 's3','key' => env('HETZNER_S3_ACCESS_KEY_ID'),
+            'secret' => env('HETZNER_S3_ACCESS_KEY_SECRET'),
+            'region' => env('HETZNER_S3_REGION'),
             'bucket' => env('AWS_BUCKET_MARKETPLACE'),
-            'visibility' => 'public',
-            'url' => env('AWS_URL_MP', env('AWS_URL')),
-            'endpoint' => env('AWS_ENDPOINT'),
-            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
+            'root' => env('APP_ENV'),
+            'endpoint' => env('HETZNER_S3_ENDPOINT'),
+            'use_path_style_endpoint' => true,
         ],
 
         /**
