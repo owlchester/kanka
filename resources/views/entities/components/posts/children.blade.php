ebf64a421635acd47f68fd4aa8f099dbe5d4c872
Spitfire <itza305@gmail.com>
2025-07-09 20:30:29 -0600

    Implemented requested changes

--- resources/views/entities/components/posts/children.blade.php
+++ resources/views/entities/components/posts/children.blade.php
@@ -1,62 +1,33 @@
+<?php
 
+$route = 'entities.children';
+if ($entity->hasChild()) {
+    // Get table name from the model
+    $table = $entity->child->getTable();
 
-@if($entity->hasChild())
-    @php
-        // Get the name
-        $type = class_basename($entity->child);
+    // Make the route name.
+    $route = $table . '.' . $table;
+}
 
-        // Construct the layout class path.
-        $layoutClass = "\\App\\Renderers\\Layouts\\{$type}\\{$type}";
+$datagridOptions = [];
 
-        // Get table name from the model
-        $table = $entity->child->getTable();
+$routeOptions = [
+    $campaign,
+    $entity->child,
+    'init' => 1,
+];
+if (request()->get('m') == \App\Enums\Descendants::All->value || (!request()->has('m') && $campaign->defaultDescendantsMode() === \App\Enums\Descendants::All)) {
+    $routeOptions['m'] = \App\Enums\Descendants::All;
+}
+$routeOptions = Datagrid::initOptions($routeOptions);
+$datagridOptions =
+    ['datagridUrl' => route($route, $routeOptions)]
+;
 
-        // Make the route name.
-        $route = $table . '.' . $table;
+?>
 
-        Datagrid::layout($layoutClass)
-            ->route($route, ['campaign' => $campaign, strtolower($type) => $entity->child]);
-
-        $rows = $entity->child
-            ->descendants()
-            ->sort(request()->only(['o', 'k']), ['name' => 'asc'])
-            ->with([
-                'entity', 'entity.image', 'entity.entityType', 'entity.visibleTags',
-                'parent', 'parent.entity',
-            ])
-            ->paginate(config('limits.pagination'));
-        $rows->withPath(route($route, ['campaign' => $campaign, strtolower($type) => $entity->child]));
-
-    @endphp
-@else
-
-    @php
-        $layout = app()->make(App\Renderers\Layouts\Entity\Children::class);
-        $layout->entityType($entity->entityType);
-        Datagrid::layout($layout)
-            ->route('entities.children', ['campaign' => $campaign, 'entity' =>  $entity]);
-
-        $rows = $entity
-            ->descendants()
-            ->sort(request()->only(['o', 'k']), ['name' => 'asc'])
-            ->with([
-                'image', 'entityType',
-                'visibleTags',
-                'children',
-                'parent',
-            ])
-            ->paginate(config('limits.pagination'));
-        $rows->withPath(route('entities.children', ['campaign' => $campaign, 'entity' =>  $entity]));
-    @endphp
-@endif
 <div class="overflow-x-auto" id="entity-children">
     <div id="datagrid-parent" class="table-responsive">
-        @include('layouts.datagrid._table')
+        @include('layouts.datagrid._table', $datagridOptions)
     </div>
 </div>
-
-@section('modals')
-    @parent
-    @include('layouts.datagrid.delete-forms', ['models' => Datagrid::deleteForms(), 'params' => []])
-    <x-dialog id="edit-dialog" :loading="true" />
-@endsection
