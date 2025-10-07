8970d4a16ad849abfd860dd95ccd3e8203e20d78
ilestis <jay.payne.ch@gmail.com>
2025-10-07 12:11:57 -0600

    Tweak

--- resources/views/whiteboards/show.blade.php
+++ resources/views/whiteboards/show.blade.php
@@ -14,11 +14,9 @@
         @include('entities.components.menu_v2', ['active' => 'story'])
 
         <div class="entity-main-block grow flex flex-col gap-5 min-w-0">
-            <p>
-                <a href="{{ route('whiteboards.draw', [$campaign, $entity->child]) }}" class="btn2 btn-block btn-primary" target="_blank">
-                    <x-icon class="fa-duotone fa-chalkboard" /> {{ __('whiteboards.actions.draw') }}
-                </a>
-            </p>
+            <a href="{{ route('whiteboards.draw', [$campaign, $entity->child]) }}" class="btn2 btn-block btn-primary draw-link" target="_blank">
+                <x-icon class="fa-duotone fa-chalkboard" /> {{ __('whiteboards.actions.draw') }}
+            </a>
 
             @include('entities.components.posts', ['withEntry' => true])
         </div>
