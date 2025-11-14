8ebc6366fce8f80845325d9b2bacdd185f5be0ac
ilestis <jay.payne.ch@gmail.com>
2025-11-13 18:11:36 -0600

    Fix copying a custom entity that has locations

--- resources/views/entities/forms/entry.blade.php
+++ resources/views/entities/forms/entry.blade.php
@@ -2,7 +2,7 @@
     @include('cruds.fields.name', ['trans' => $entityType->pluralCode()])
     @include('cruds.fields.type', ['trans' => 'crud'])
     @include('cruds.fields.parent', ['trans' => 'crud', 'is_parent' => true])
-    @include('cruds.fields.locations', ['from' => $entity ?? null, 'quickCreator' => true])
+    @include('cruds.fields.locations', ['from' => $entity ?? null, 'quickCreator' => true, 'model' => $entity ?? $source ?? null])
     @include('cruds.fields.entry2')
 
     @include('cruds.fields.tags')
