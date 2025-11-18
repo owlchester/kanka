ee98ade496cf0f411c34cf99379e8168714c574d
Spitfire <itza305@gmail.com>
2025-11-18 09:16:49 -0600

    Updated subscription email for admins

--- resources/views/emails/subscriptions/new/md.blade.php
+++ resources/views/emails/subscriptions/new/md.blade.php
@@ -5,41 +5,46 @@
 # New subscription
 @endif
 
-User [{{ $user->email }}](mailto:{{ $user->email }}) [{{ $user->name }}](https://admin.kanka.io/users/{{ $user->id }})
-Currency: {{ $user->currencySymbol() }}
-
-Account created {{ $user->created_at->diffForHumans() }} ({{ $user->created_at->format('d.m.Y') }}).
+- **User ID:** {{ $user->id }}
+- **Username:** [{{ $user->name }}](https://admin.kanka.io/users/{{ $user->id }})
+- **Email:** {{ $user->email }}
+- **Account age:** {{ $user->created_at->diffForHumans() }} ({{ $user->created_at->format('d.m.Y') }})
+- **Subscription tier:** {{ $user->tier ?? 'N/A' }}
+- **Subscription currency:** {{ $user->currencySymbol() }}
+- **User country:** {{ $user->country ?? 'N/A' }}
 
 @if ($trial)
-Trial started on {{ $trial->created_at->format('d.m.Y') }}.
+- **Trial started:** {{ $trial->created_at->format('d.m.Y') }}
 @endif
 
 @if ($lastCancel)
-Previously cancelled {{ $lastCancel->tier }} subscription {{ $lastCancel->created_at->diffForHumans() }} ({{ $lastCancel->created_at->format('d.m.Y') }}).
+---
 
-**Reason provided:**
+### Previous subscription info
 
-{{ $lastCancel->reason }}
+- **Previously cancelled:** {{ $lastCancel->tier }}  
+  {{ $lastCancel->created_at->diffForHumans() }} ({{ $lastCancel->created_at->format('d.m.Y') }})
+
+- **Reason provided:**  
+  {{ $lastCancel->reason }}
 
 @if (!empty($lastCancel->custom))
-**Custom message:**
-
-{!! nl2br(e($lastCancel->custom)) !!}
-
+- **Custom message:**  
+  {!! nl2br(e($lastCancel->custom)) !!}
 @endif
 @endif
+
 @if ($discord = $user->apps->where('app', 'discord')->first())
-Discord: {{ $discord->settings['username'] }}
-
+- **Discord:** {{ $discord->settings['username'] }}
 @endif
+
 @if ($user->referral)
-Referral: {{ $user->referrer->code }}
-
+- **Referral:** {{ $user->referrer->code }}
 @endif
-@if (!empty($user->settings['tracking']))
-Ad campaign
-{{ is_array($user->settings['tracking']) ? collect($user->settings['tracking'])->implode(' ') : $user->settings['tracking'] }}
 
+@if (!empty($user->settings['tracking']))
+- **Ad campaign:**  
+  {{ is_array($user->settings['tracking']) ? collect($user->settings['tracking'])->implode(' ') : $user->settings['tracking'] }}
 @endif
 
 </x-mail::message>
