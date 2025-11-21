6a941185b3393b2c225dc3bcdb09defd59d99e58
ilestis <jay.payne.ch@gmail.com>
2025-11-21 10:33:54 -0600

    Tweaks

--- resources/views/campaigns/modules/edit.blade.php
+++ resources/views/campaigns/modules/edit.blade.php
@@ -1,6 +1,6 @@
 <x-form :action="['modules.update', $campaign, $entityType->id]" method="PATCH" class="w-full max-w-lg">
     @include('partials.forms._dialog', [
-        'title' => __('campaigns/modules.rename.title', ['module' => __('entities.' . $entityType->code)]),
+        'title' => __('campaigns/modules.rename.title', ['module' => __('entities.' . $entityType->pluralCode())]),
         'content' => 'campaigns.modules._form',
         'submit' => __('crud.actions.save-changes')
     ])
