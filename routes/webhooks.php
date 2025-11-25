029f0bbe4076d3f333eeb56ae821def200fd7dda
spitfire305 <72818019+spitfire305@users.noreply.github.com>
2025-11-25 16:28:58 +0000

    Fix styling

--- routes/webhooks.php
+++ routes/webhooks.php
@@ -1,12 +1,11 @@
 <?php
+
 use App\Http\Controllers\Facebook\DeletionController;
 
 Route::post(
     'stripe/webhook',
     '\App\Http\Controllers\WebhookController@handleWebhook'
 )->name('cashier.webhook');
-
-
 
 Route::post('/facebook/data-deletion', [DeletionController::class, 'handle']);
 Route::get('/facebook/data-deletion/status', [DeletionController::class, 'status']);
