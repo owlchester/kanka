4dc023c69c41ddb9a5e5e42083a8f7861f43709a
spitfire305 <72818019+spitfire305@users.noreply.github.com>
2025-11-25 17:07:00 +0000

    Fix styling

--- app/Services/Campaign/ModuleEditService.php
+++ app/Services/Campaign/ModuleEditService.php
@@ -52,8 +52,8 @@ class ModuleEditService
             $settings['modules'][$key]['i'] = $icon;
         }
 
-        //Bookmarks cant have default images.
-        if (($request->hasFile('default_entity_image') || $request->filled('remove-image')) && !$this->entityType->isBookmark()) {
+        // Bookmarks cant have default images.
+        if (($request->hasFile('default_entity_image') || $request->filled('remove-image')) && ! $this->entityType->isBookmark()) {
 
             $this->defaultImageService->campaign($this->campaign)
                 ->user($this->user)
