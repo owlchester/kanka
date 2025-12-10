cba226fd65d456d9e53ece95b4bd0de227058510
spitfire305 <72818019+spitfire305@users.noreply.github.com>
2025-12-10 21:44:10 +0000

    Fix styling

--- app/Events/WhiteboardUpdated.php
+++ app/Events/WhiteboardUpdated.php
@@ -5,7 +5,6 @@ namespace App\Events;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithBroadcasting;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
