d8ad229952188d6cf3cbb4188d8440f0570f5d9f
Spitfire <itza305@gmail.com>
2025-12-12 13:05:16 -0600

    Added fancy dropdown

--- resources/views/entities/pages/abilities/_buttons.blade.php
+++ resources/views/entities/pages/abilities/_buttons.blade.php
@@ -1,26 +1,46 @@
-<a href="{{ route('entities.entity_abilities.reorder', [$campaign, $entity]) }}" class="btn2 btn-sm">
-    <x-icon class="fa-regular fa-arrow-up-arrow-down" />
-    <span class="hidden md:inline">{{ __('entities/abilities.show.reorder') }}</span>
-    <span class="md:hidden">{{ __('sidebar.campaign_switcher.reorder') }}</span>
-</a>
-<a href="{{ route('entities.entity_abilities.reset', [$campaign, $entity]) }}" class="btn2 btn-sm" data-title="{{ __('entities/abilities.helpers.recharge') }}" data-toggle="tooltip">
-    <x-icon class="fa-regular fa-redo" />
-    <span class="hidden md:inline">{{ __('entities/abilities.actions.reset') }}</span>
-    <span class="md:hidden">{{ __('crud.actions.reset') }}</span>
-</a>
-@if ($entity->isCharacter())
-    @php $raceModule = \App\Models\EntityType::default()->where('code', 'race')->first(); @endphp
-    <a href="{{ route('entities.entity_abilities.import.confirm', [$campaign, $entity]) }}" class="btn2 btn-sm"
-        data-toggle="dialog" data-target="abilities-dialog" data-url="{{ route('entities.entity_abilities.import.confirm', [$campaign, $entity]) }}" data-title="{{ __('entities/abilities.helpers.sync') }}" data-toggle="tooltip">
-        <x-icon :class="$raceModule->icon()" />
-        <span class="hidden md:inline">{{ __('entities/abilities.actions.sync') }}</span>
+<div class="join flex items-center">
+    <!-- Main add button -->
+    <a href="{{ route('entities.entity_abilities.create', [$campaign, $entity]) }}" class="btn2 btn-sm join-item"
+        data-toggle="dialog" data-target="abilities-dialog" data-url="{{ route('entities.entity_abilities.create', [$campaign, $entity]) }}">
+        <x-icon class="plus" />
+        <span class="hidden md:inline">{{ __('entities/abilities.actions.add') }}</span>
         <span class="md:hidden">{{ __('crud.add') }}</span>
     </a>
 
-@endif
-<a href="{{ route('entities.entity_abilities.create', [$campaign, $entity]) }}" class="btn2 btn-sm"
-    data-toggle="dialog" data-target="abilities-dialog" data-url="{{ route('entities.entity_abilities.create', [$campaign, $entity]) }}">
-    <x-icon class="plus" />
-    <span class="hidden md:inline">{{ __('entities/abilities.actions.add') }}</span>
-    <span class="md:hidden">{{ __('crud.add') }}</span>
-</a>
+    <!-- Dropdown using the entity-actions style -->
+    <div class="dropdown entity-actions-dropdown flex items-center join-item">
+
+        <div data-dropdown aria-expanded="false" aria-haspopup="menu" aria-controls="abilities-submenu" class="btn2 btn-sm join-item entity-actions-button">
+            <x-icon class="fa-solid fa-caret-down" />
+        </div>
+
+        <div class="dropdown-menu hidden" role="menu" id="abilities-submenu">
+            @if ($entity->isCharacter())
+                @php $raceModule = \App\Models\EntityType::default()->where('code', 'race')->first(); @endphp
+                <x-dropdowns.item
+                    link="#"
+                    :dialog=" route('entities.entity_abilities.import.confirm', [$campaign, $entity]) "
+                    icon="{{ $raceModule->icon() }}"
+                    data-toggle="dialog" data-target="abilities-dialog" data-url="{{ route('entities.entity_abilities.import.confirm', [$campaign, $entity]) }}" data-title="{{ __('entities/abilities.helpers.sync') }}" data-toggle="tooltip"
+                >
+                    <span class="hidden md:inline">{{ __('entities/abilities.actions.sync') }}</span>
+                </x-dropdowns.item>
+            @endif
+
+            <x-dropdowns.item :link="route('entities.entity_abilities.reset', [$campaign, $entity])" icon="fa-regular fa-redo">
+                <span class="grow">{{ __('entities/abilities.actions.reset') }}</span>
+            </x-dropdowns.item>
+
+            <x-dropdowns.item :link="route('entities.entity_abilities.reorder', [$campaign, $entity])"
+                              icon="fa-regular fa-arrow-up-arrow-down">
+                <span class="grow">{{ __('entities/abilities.show.reorder') }}</span>
+            </x-dropdowns.item>
+            
+            <x-dropdowns.divider />
+
+            <x-dropdowns.item :link="'https://docs.kanka.io/en/latest/entities/abilities.html#entity-abilities'" icon="fa-regular fa-book">
+                <span class="grow">{{ __('general.learn-more') }}</span>
+            </x-dropdowns.item>
+        </div>
+    </div>
+</div>
