5668901e589b1b9cdf03f0bc62c77ca77e31c6d4
ilestis <jay.payne.ch@gmail.com>
2025-12-15 13:20:29 -0600

    Scariest index of my life

---
+++ database/migrations/2025_12_15_191556_add_dashboard_widget_index.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('entities', function (Blueprint $table) {
+            $table->index(
+                [
+                'campaign_id',
+                'type_id',
+                'archived_at',
+                'deleted_at',
+                'updated_at',
+            ], 'dashboard_entity_list_idx');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('entities', function (Blueprint $table) {
+            //
+        });
+    }
+};
