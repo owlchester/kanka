20890385294faee21007eccec09cc081342f0594
ilestis <jay.payne.ch@gmail.com>
2025-12-16 09:37:50 -0600

    Links as links

--- resources/views/layouts/dialogs/languages.blade.php
+++ resources/views/layouts/dialogs/languages.blade.php
@@ -10,34 +10,34 @@
     <div class="grid grid-cols-2 gap-4">
         <ul class="list-none p-0 m-0">
             <li class="py-2">
-                <a rel="nofollow" href="{{ $currentUrl . 'en-US' }}" rel="nofollow">
+                <a rel="nofollow" href="{{ $currentUrl . 'en-US' }}" class="text-link">
                     US English
                 </a>
             </li>
             <li class="py-2">
-                <a rel="nofollow" href="{{ $currentUrl . 'en' }}" >
+                <a rel="nofollow" href="{{ $currentUrl . 'en' }}" class="text-link">
                     UK English
                 </a>
             </li>
             <li class="py-2">
-                <a rel="nofollow" href="{{ $currentUrl . 'pt-BR' }}">
+                <a rel="nofollow" href="{{ $currentUrl . 'pt-BR' }}" class="text-link">
                     Português do Brasil
                 </a>
             </li>
         </ul>
         <ul class="list-none p-0 m-0">
             <li class="py-2">
-                <a rel="nofollow" href="{{ $currentUrl . 'de' }}">
+                <a rel="nofollow" href="{{ $currentUrl . 'de' }}" class="text-link">
                     Deutsch
                 </a>
             </li>
             <li class="py-2">
-                <a rel="nofollow" href="{{ $currentUrl . 'fr' }}">
+                <a rel="nofollow" href="{{ $currentUrl . 'fr' }}" class="text-link">
                     Français
                 </a>
             </li>
             <li class="py-2">
-                <a rel="nofollow" href="{{ $currentUrl . 'es' }}">
+                <a rel="nofollow" href="{{ $currentUrl . 'es' }}" class="text-link">
                     Español
                 </a>
             </li>
@@ -50,24 +50,24 @@
         <div class="grid grid-cols-2 gap-4">
             <ul class="list-none p-0 m-0">
                 <li class="py-2">
-                    <a rel="nofollow" href="{{ $currentUrl . 'it' }}">
+                    <a rel="nofollow" href="{{ $currentUrl . 'it' }}" class="text-link">
                         Italiano
                     </a>
                 </li>
                 <li class="py-2">
-                    <a rel="nofollow" href="{{ $currentUrl . 'pl' }}">
+                    <a rel="nofollow" href="{{ $currentUrl . 'pl' }}" class="text-link">
                         Polska
                     </a>
                 </li>
             </ul>
             <ul class="list-none p-0 m-0">
                 <li class="py-2">
-                    <a rel="nofollow" href="{{ $currentUrl . 'ru' }}">
+                    <a rel="nofollow" href="{{ $currentUrl . 'ru' }}" class="text-link">
                         Pусский
                     </a>
                 </li>
                 <li class="py-2">
-                    <a rel="nofollow" href="{{ $currentUrl . 'sk' }}">
+                    <a rel="nofollow" href="{{ $currentUrl . 'sk' }}" class="text-link">
                         Slovenský
                     </a>
                 </li>
