472ac71721fd49833fb14056d3403c64763134bc
spitfire305 <72818019+spitfire305@users.noreply.github.com>
2026-01-07 18:41:17 +0000

    Fix styling

--- routes/auth.php
+++ routes/auth.php
@@ -16,7 +16,6 @@ Route::get('auth/{provider}', [AuthController::class, 'redirectToProvider'])->na
 // Password Reset Routes...
 Route::post('password/email', [\App\Http\Controllers\Auth\ResetPasswordController::class, 'reset'])->name('password.email');
 
-
 include 'oauth.php';
 /*
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
