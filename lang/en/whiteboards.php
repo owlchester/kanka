13bbb8e1d95f19a6235f449d86d2379bbc7374ad
ilestis <jay.payne.ch@gmail.com>
2026-01-22 06:06:29 -0600

    Missing title

--- lang/en/whiteboards.php
+++ lang/en/whiteboards.php
@@ -4,6 +4,9 @@ return [
     'actions'       => [
         'draw'  => 'Draw',
     ],
+    'create' => [
+        'title' => 'New Whiteboard',
+    ],
     'cta'           => [
         'title' => 'Whiteboards are a special premium feature',
         'text'  => 'To unlock whiteboards, a campaign needs to be made premium by a member at the :wyvern or :elemental tier.',
