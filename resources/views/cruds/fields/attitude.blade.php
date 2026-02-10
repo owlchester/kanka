107362d3dab1640d74cd185e56c3a2d6e64eaf5f
ilestis <jay.payne.ch@gmail.com>
2026-02-10 14:41:33 -0600

    Simpler

--- resources/views/cruds/fields/attitude.blade.php
+++ resources/views/cruds/fields/attitude.blade.php
@@ -5,8 +5,7 @@
     tooltip>
 
     <div x-data="{ value: {{ (int) old('attitude', $model->attitude ?? 0) }} }" class="flex items-center gap-3">
-        <input type="range" name="attitude" x-model="value" min="-100" max="100" step="1" class="w-full range-slider"
-            :style="`--range-fill: ${(+value + 100) / 200 * 100}%`" />
+        <input type="range" name="attitude" x-model="value" min="-100" max="100" step="1" class="w-full"/>
         <span x-text="value" class="text-sm tabular-nums w-10 text-right shrink-0"></span>
     </div>
 </x-forms.field>
