8055611f413e1c673251e1ef2712fd58e0c87c7f
ilestis <jay.payne.ch@gmail.com>
2026-02-16 09:57:35 -0600

    Prepare campaign events table

---
+++ database/migrations/2026_02_16_155432_create_campaign_events_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::dropIfExists('campaign_events');
+        Schema::create('campaign_events', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('campaign_id');
+            $table->unsignedInteger('created_by')->nullable();
+            $table->string('event');
+            $table->json('metadata')->nullable();
+            $table->timestamps();r
+
+            $table->foreign('campaign_id')->references('id')->on('campaigns')->cascadeOnDelete();
+            $table->foreign('created_by')->references('id')->on('users')->cascadeOnDelete();
+
+            $table->index(['campaign_id', 'event']);
+            $table->index(['campaign_id', 'created_at']);
+            $table->index('event');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('campaign_events');
+    }
+};
