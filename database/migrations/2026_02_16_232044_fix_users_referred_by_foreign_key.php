7ea69204287615c32ba81a8f766965b4ff8f0343
ilestis <jay.payne.ch@gmail.com>
2026-02-16 18:04:03 -0600

    Fix migration

--- database/migrations/2026_02_16_232044_fix_users_referred_by_foreign_key.php
+++ database/migrations/2026_02_16_232044_fix_users_referred_by_foreign_key.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //$table->dropForeign(['referred_by']);
+            $table->dropForeign(['referred_by']);
             $table->dropColumn('referred_by');
             $table->unsignedInteger('referred_by')->nullable();
             $table->foreign('referred_by')->references('id')->on('users')->nullOnDelete();
