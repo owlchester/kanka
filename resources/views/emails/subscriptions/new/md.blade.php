2bc75c2220f82ace5027cb6f76a344f3085bf689
Spitfire <itza305@gmail.com>
2026-02-19 12:10:22 -0600

    The fix, Forgot to commit yesterday

--- resources/views/emails/subscriptions/new/md.blade.php
+++ resources/views/emails/subscriptions/new/md.blade.php
@@ -38,8 +38,8 @@
 - **Discord:** {{ $discord->settings['username'] }}
 @endif
 
-@if ($user->referral)
-- **Referred by:** [{{ $user->referrer->name }}](https://admin.kanka.io/users/{{ $user->referrer->id }}) ({{ $user->referrer->code }})
+@if ($user->referrer)
+- **Referred by:** [{{ $user->referrer->name }}](https://admin.kanka.io/users/{{ $user->referrer->id }})
 @endif
 
 @if (!empty($user->settings['tracking']))
