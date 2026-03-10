c525cc4154eb7f2d0afa95d0b3bb8ab5df074f82
ilestis <jay.payne.ch@gmail.com>
2026-03-10 15:02:21 -0600

    Dark/midnight tweaks

--- public/build/js/tinymce/index.js
+++ public/build/js/tinymce/index.js
@@ -1,7 +1,7 @@
-// Exports the "visualchars" plugin for usage with module loaders
+// Exports the "modern" theme for usage with module loaders
 // Usage:
 //   CommonJS:
-//     require('tinymce/plugins/visualchars')
+//     require('tinymce/themes/modern')
 //   ES2015:
-//     import 'tinymce/plugins/visualchars'
-require('./plugin.js');
+//     import 'tinymce/themes/modern'
+require('./theme.js');
