dbe6123dcd405125047bfc5e6ca1b32131028d58
ilestis <3263440+ilestis@users.noreply.github.com>
2026-03-23 14:32:30 +0000

    Fix styling

--- app/Http/Controllers/Api/v1/CharacterApiController.php
+++ app/Http/Controllers/Api/v1/CharacterApiController.php
@@ -20,14 +20,14 @@ class CharacterApiController extends MiscApiController
      */
     public function index(Campaign $campaign)
     {
-        $this->authorize("access", $campaign);
+        $this->authorize('access', $campaign);
 
         return CharacterResource::collection(
             $campaign
                 ->characters()
                 ->withApi()
                 ->filter(request()->all())
-                ->lastSync(request()->get("lastSync"))
+                ->lastSync(request()->get('lastSync'))
                 ->paginate(),
         );
     }
@@ -39,8 +39,8 @@ class CharacterApiController extends MiscApiController
      */
     public function show(Campaign $campaign, Character $character)
     {
-        $this->authorize("access", $campaign);
-        $this->authorize("view", $character->entity);
+        $this->authorize('access', $campaign);
+        $this->authorize('view', $character->entity);
 
         return new CharacterResource($character);
     }
@@ -52,14 +52,14 @@ class CharacterApiController extends MiscApiController
      */
     public function store(Request $request, Campaign $campaign)
     {
-        $this->authorize("access", $campaign);
-        $this->authorize("create", [
-            EntityType::find(config("entities.ids.character")),
+        $this->authorize('access', $campaign);
+        $this->authorize('create', [
+            EntityType::find(config('entities.ids.character')),
             $campaign,
         ]);
 
         $data = $request->all();
-        $data["campaign_id"] = $campaign->id;
+        $data['campaign_id'] = $campaign->id;
         /** @var Character $model */
         $model = Character::create($data);
         $this->crudSave($model, $request->validated());
@@ -77,8 +77,8 @@ class CharacterApiController extends MiscApiController
         Campaign $campaign,
         Character $character,
     ) {
-        $this->authorize("access", $campaign);
-        $this->authorize("update", $character->entity);
+        $this->authorize('access', $campaign);
+        $this->authorize('update', $character->entity);
         $character->update($request->all());
         $this->crudSave($character, $request->validated());
 
@@ -92,8 +92,8 @@ class CharacterApiController extends MiscApiController
      */
     public function destroy(Campaign $campaign, Character $character)
     {
-        $this->authorize("access", $campaign);
-        $this->authorize("delete", $character->entity);
+        $this->authorize('access', $campaign);
+        $this->authorize('delete', $character->entity);
         $character->delete();
 
         return response()->json(null, 204);
