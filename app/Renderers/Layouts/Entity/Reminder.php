04c50e3acfe3c35ca23bc11e55a0b0d0ea9bff16
Spitfire <itza305@gmail.com>
2026-03-26 12:42:51 -0600

    Fixed missing keys

--- app/Renderers/Layouts/Entity/Reminder.php
+++ app/Renderers/Layouts/Entity/Reminder.php
@@ -18,13 +18,13 @@ class Reminder extends Layout
         $columns = [
             'calendar' => [
                 'key' => 'calendar.name',
-                'label' => 'entities.calendar',
+                'label' => __('entities.calendar'),
                 'render' => Standard::ENTITYLINK,
                 'with' => 'calendar',
             ],
             'date' => [
                 'key' => 'date',
-                'label' => 'events.fields.date',
+                'label' => __('events.fields.date'),
                 'render' => function (Model $reminder) {
                     $params = '?year=' . $reminder->year . '&month=' . $reminder->month;
                     $tooltip = $reminder->hasEra() ? ' data-toggle="tooltip" data-title="' . e($reminder->readableRawDate()) . '"' : '';
@@ -34,7 +34,7 @@ class Reminder extends Layout
             ],
             'length' => [
                 'key' => 'length',
-                'label' => 'calendars.fields.length',
+                'label' => __('calendars.fields.length'),
                 'render' => function (Model $reminder) {
                     return trans_choice('calendars.fields.length_days', $reminder->length, ['count' => $reminder->length]);
                 },
