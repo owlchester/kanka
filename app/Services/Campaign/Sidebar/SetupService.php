ecbf73300bf9fe049bfa2aadefadba1fdffec0fc
Jay Payne <jay.payne.ch@gmail.com>
2026-03-26 12:04:42 -0600

    fix: sidebar not loading category info

--- app/Services/Campaign/Sidebar/SetupService.php
+++ app/Services/Campaign/Sidebar/SetupService.php
@@ -447,7 +447,7 @@ class SetupService
 
     protected function loadModules(): void
     {
-        $modules = $this->campaign->getEntityTypes()->where('is_default', 1);
+        $modules = $this->campaign->getEntityTypes();
         /** @var EntityType $module */
         foreach ($modules as $module) {
             $this->modules[$module->code] = $module;
