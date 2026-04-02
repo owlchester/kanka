a15f2079af2daf7f248272ba2ccd706ac184b8ac
Jay Payne <jay.payne.ch@gmail.com>
2026-04-01 19:22:15 -0600

    fix: delete quest element title and text

--- resources/views/quests/elements/_actions.blade.php
+++ resources/views/quests/elements/_actions.blade.php
@@ -5,8 +5,8 @@
 </x-dropdowns.item>
 <x-dropdowns.divider></x-dropdowns.divider>
 @php
-    $url = route('confirm-delete', [$campaign, 'route' => route('quests.quest_elements.destroy', [$campaign, $model, $element]), 'name' => $element->name()]);
+    $url = route('confirm-delete', [$campaign, 'route' => route('quests.quest_elements.destroy', [$campaign, $model, $element]), 'name' => $element->name(), 'permanent' => true]);
 @endphp
 <x-dropdowns.item link="#" css="text-error-content hover:bg-error" :data="['toggle' => 'dialog', 'target' => 'primary-dialog', 'url' => $url]" icon="trash">
-    {{ __('posts.remove.title') }}
+    {{ __('crud.remove') }}
 </x-dropdowns.item>
