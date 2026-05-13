da6602b9f8dce3f3b71fc4f61abf1783a7a8464f
Jay Payne <jay.payne.ch@gmail.com>
2026-05-13 15:45:44 -0600

    feat: remove preview panel logView and delete old Lookup components

--- app/Http/Controllers/Entity/PreviewController.php
+++ app/Http/Controllers/Entity/PreviewController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 use App\Models\Campaign;
 use App\Models\Entity;
 use App\Services\Entity\PreviewService;
-use App\Services\Search\RecentService;
 use App\Traits\CampaignAware;
 use App\Traits\GuestAuthTrait;
 
@@ -15,26 +14,11 @@ class PreviewController extends Controller
     use CampaignAware;
     use GuestAuthTrait;
 
-    protected PreviewService $service;
-
-    protected RecentService $recentService;
-
-    public function __construct(PreviewService $service, RecentService $recentService)
-    {
-        $this->service = $service;
-        $this->recentService = $recentService;
-    }
+    public function __construct(protected PreviewService $service) {}
 
     public function index(Campaign $campaign, Entity $entity)
     {
         $this->campaign($campaign)->authEntityView($entity);
-
-        if (auth()->check()) {
-            $this->recentService
-                ->campaign($campaign)
-                ->user(auth()->user())
-                ->logView($entity);
-        }
 
         return response()->json(
             $this
