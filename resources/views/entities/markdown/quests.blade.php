f132cdd02ca378a14ae41830b9a17b992d95e88d
Jay Payne <jay.payne.ch@gmail.com>
2026-05-19 08:53:29 -0600

    fix: quest markdown export

--- resources/views/entities/markdown/quests.blade.php
+++ resources/views/entities/markdown/quests.blade.php
@@ -1,5 +1,5 @@
 @if ($entity->child->status_id !== \App\Enums\QuestStatus::notStarted)
 ## {!! __('crud.tabs.profile') !!}
 
-* {{ __('quests.fields.status') }}: {{ __('quests.status.' . ['not_started', 'ongoing', 'completed', 'abandoned'][$entity->child->status_id->value]) }}
+* {{ $entity->status->setRelation('entityType', $entity->entityType)->name() }}
 @endif
