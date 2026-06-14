96a90d825c1733063c8a316ebd3fe8d6844076af
ilestis <3263440+ilestis@users.noreply.github.com>
2026-06-14 20:49:31 +0000

    Fix styling

--- app/Services/SearchService.php
+++ app/Services/SearchService.php
@@ -219,7 +219,6 @@ class SearchService
                             ->orWhere('ea.name', 'like', '%' . $this->term . '%');
                     }
                 });
-            
 
             // Exact name match comes first
             // Only do this when the input string is utf8
@@ -228,11 +227,11 @@ class SearchService
                 $escapedTerm = preg_replace('/&/', '\\&', preg_quote($cleanTerm));
                 $query->orderByRaw('FIELD(entities.name, ?) DESC', [$cleanTerm]);
                 $query->orderByRaw('FIELD(ea.name, ?) DESC', [$cleanTerm]);
-                
+
                 // Name word-start match, so when looking for 'Morley', entities named 'Momorley' appear at the end
                 $query->orderByRaw('entities.name RLIKE ? DESC', ["[[:<:]]{$escapedTerm}"]);
                 $query->orderByRaw('ea.name RLIKE ? DESC', ["[[:<:]]{$escapedTerm}"]);
-                
+
                 // Partial name match
                 $query->orderByRaw('entities.name LIKE ? DESC', ["%{$cleanTerm}%"]);
                 $query->orderByRaw('ea.name LIKE ? DESC', ["%{$cleanTerm}%"]);
