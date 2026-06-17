c9ffed6aa219572d280a051b793ffa64245daa9c
Jay Payne <jay.payne.ch@gmail.com>
2026-06-17 13:30:38 -0600

    fix: default status and pin selects to blank in member bulk edit

--- resources/views/layouts/datagrid/bulks/_organisation-member.blade.php
+++ resources/views/layouts/datagrid/bulks/_organisation-member.blade.php
@@ -1,12 +1,10 @@
 @php
 $statuses = [
-    '' => null,
     App\Enums\OrganisationMemberStatus::active->value => __('organisations.members.status.active'),
     App\Enums\OrganisationMemberStatus::inactive->value => __('organisations.members.status.inactive'),
     App\Enums\OrganisationMemberStatus::unknown->value => __('organisations.members.status.unknown'),
 ];
 $pins = [
-    '' => null,
     App\Enums\OrganisationMemberPin::empty->value => __('organisations.members.pinned.none'),
     App\Enums\OrganisationMemberPin::character->value => App\Facades\Module::singular(
         config('entities.ids.character'),
@@ -24,9 +22,19 @@ $pins = [
         <input type="text" name="role" value="" placeholder="{{ __('organisations.members.placeholders.role') }}" maxlength="45" class="w-full" />
     </x-forms.field>
     <x-forms.field field="status_id" :label="__('organisations.members.fields.status')">
-        <x-forms.select name="status_id" :options="$statuses" class="w-full" />
+        <select name="status_id" class="w-full">
+            <option value="" selected="selected"></option>
+            @foreach ($statuses as $value => $label)
+                <option value="{{ $value }}">{{ $label }}</option>
+            @endforeach
+        </select>
     </x-forms.field>
     <x-forms.field field="pin_id" :label="__('organisations.members.fields.pinned')">
-        <x-forms.select name="pin_id" :options="$pins" class="w-full" />
+        <select name="pin_id" class="w-full">
+            <option value="" selected="selected"></option>
+            @foreach ($pins as $value => $label)
+                <option value="{{ $value }}">{{ $label }}</option>
+            @endforeach
+        </select>
     </x-forms.field>
 </x-grid>
