4644dedf5c6ab8fe132d7f644d2d85c7b9cda16c
Jay Payne <jay.payne.ch@gmail.com>
2026-06-23 12:23:03 -0600

    feat: ui tweaks

--- resources/views/partials/helper-modal.blade.php
+++ resources/views/partials/helper-modal.blade.php
@@ -1,5 +1,7 @@
 <x-dialog :id="$id" :title="$title">
-    @foreach ($textes as $text)
-        <p class="text-justify">{!! $text !!}</p>
-    @endforeach
+    <x-grid type="1/1">
+        @foreach ($textes as $text)
+            <p class="text-justify">{!! $text !!}</p>
+        @endforeach
+    </x-grid>
 </x-dialog>
