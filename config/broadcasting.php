8068b276da20477ec771d561f54568b4dd0c8466
Jay Payne <jay.payne.ch@gmail.com>
2026-07-12 16:04:40 -0600

    Revert "fix: configure reverb for tests and add reverb to broadcasting config"
    This reverts commit 425c6cb876906313c9e8ce08fd11ad5767ea69e6.

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -53,18 +53,6 @@ return [
             'driver' => 'null',
         ],
 
-        'reverb' => [
-            'driver' => 'reverb',
-            'key' => env('REVERB_APP_KEY'),
-            'secret' => env('REVERB_APP_SECRET'),
-            'app_id' => env('REVERB_APP_ID'),
-            'options' => [
-                'host' => env('REVERB_HOST', '127.0.0.1'),
-                'port' => env('REVERB_PORT', 8080),
-                'scheme' => env('REVERB_SCHEME', 'http'),
-            ],
-        ],
-
     ],
 
 ];
