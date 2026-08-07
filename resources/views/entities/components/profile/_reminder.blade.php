ac241ca4320aba0c6ff81c2fa3bb42dc52841127
Jay Payne <jay.payne.ch@gmail.com>
2026-08-07 12:33:33 -0600

    fix: calendar date link

--- resources/views/entities/components/profile/_reminder.blade.php
+++ resources/views/entities/components/profile/_reminder.blade.php
@@ -1,7 +1,7 @@
 @if ($entity->calendarReminder())
     <div class="element profile-date">
         <div class="title text-uppercase text-xs">{{ __('crud.fields.calendar_date') }}</div>
-        <a href="{{ route('calendars.show', [$campaign, $entity->calendarDate->calendar_id, 'year' => $entity->calendarReminder()->year, 'month' => $entity->calendarReminder()->month]) }}" class="text-link">
+        <a href="{{ route('entities.show', [$campaign, $entity->calendarDate->calendar->entity, 'year' => $entity->calendarReminder()->year, 'month' => $entity->calendarReminder()->month]) }}" class="text-link">
             {{ $entity->calendarReminder()->readableDate() }}
         </a>
     </div>
