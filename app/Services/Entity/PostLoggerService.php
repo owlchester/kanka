faea87b7903acb4da2376489abf4a4afa5259832
Jay Payne <jay.payne.ch@gmail.com>
2026-08-12 06:22:20 -0600

    feat: log names based on entities, not child

--- app/Services/Entity/PostLoggerService.php
+++ app/Services/Entity/PostLoggerService.php
@@ -2,8 +2,8 @@
 
 namespace App\Services\Entity;
 
+use App\Models\Entity;
 use App\Models\EntityLog;
-use App\Models\Location;
 use App\Models\MiscModel;
 use App\Models\Post;
 use App\Traits\CampaignAware;
@@ -73,9 +73,11 @@ class PostLoggerService
 
         try {
             if ($attribute == 'location_id') {
-                $originalLocation = Location::where('id', $original)->with('entity')->has('entity')->first();
+                $originalLocation = Entity::where('entity_id', $original)
+                    ->where('type_id', config('entities.ids.location'))
+                    ->first();
                 if (! empty($originalLocation)) {
-                    return (string) $originalLocation->entity->name;
+                    return (string) $originalLocation->name;
                 }
 
                 return '';
