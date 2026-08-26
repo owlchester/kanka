0669027d04fa2feeea524b6adeae79af86db5140
Jay Payne <jay.payne.ch@gmail.com>
2026-08-26 11:28:41 -0600

    feat: prevent future me from repeating past me mistakes

--- app/Events/Entities/EntityCreationCompleted.php
+++ app/Events/Entities/EntityCreationCompleted.php
@@ -6,11 +6,13 @@ use App\Models\Entity;
 use App\Models\User;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Events\ShouldDispatchAfterCommit;
 
 class EntityCreationCompleted
 {
     use Dispatchable;
     use SerializesModels;
+    use ShouldDispatchAfterCommit;
 
     public function __construct(public Entity $entity, public ?User $user = null) {}
 }
