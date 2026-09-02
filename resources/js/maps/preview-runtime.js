73fcda764b442f94be288a96cb9c7b9159e03208
Jay Payne <jay.payne.ch@gmail.com>
2026-09-02 09:09:21 -0600

    feat: layers on dashboard

--- resources/js/maps/preview-runtime.js
+++ resources/js/maps/preview-runtime.js
@@ -1,6 +1,8 @@
 import 'leaflet/dist/leaflet.css'
+import '../../../public/vendor/leaflet/leaflet.layerstree.css'
 import 'leaflet.markercluster/dist/MarkerCluster.css'
 import 'leaflet.markercluster/dist/MarkerCluster.Default.css'
+import '../leaflet/layerTreePlugin.js'
 import { createApp } from 'vue'
 import MapPreview from '../components/maps/MapPreview.vue'
 
